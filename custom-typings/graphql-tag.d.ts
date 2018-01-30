1c5a732f315252e70f3fb7e643dd08fc532b9b65
Ken Wheeler <thekenwheeler@gmail.com>
2018-01-30 08:35:16 -0500

    removing unused dep

--- custom-typings/graphql-tag.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'graphql-tag' {
-  import { DocumentNode } from 'graphql';
-  export default function gql(
-    literals: TemplateStringsArray,
-    ...placeholders: Array<string>
-  ): DocumentNode;
-}
