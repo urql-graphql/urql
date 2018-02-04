c7077f5dafdd3184ac00901ed9ca9b90852005f9
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-02-03 20:56:38 -0800

    Enable --declaration emit for TypeScript.

--- src/components/context.tsx
+++ src/components/context.tsx
@@ -1,5 +1,18 @@
-import createReactContext from 'create-react-context';
+import createReactContext, {
+  ProviderProps,
+  ConsumerProps,
+} from 'create-react-context';
+import { ComponentClass } from 'react';
 
 const context = createReactContext({});
 
-export const { Provider, Consumer } = context;
+// TypeScript is very pedantic about re-exporting dependencies when doing
+// --declaration emit, so we need to import ComponentClass. But if we don't
+// explicitly use ComponentClass somewhere in the code, TypeScript *also*
+// ends up issuing an error. This is dumb, but this all gets erased anyway.
+interface Context {
+  Provider: ComponentClass<ProviderProps<{}>>;
+  Consumer: ComponentClass<ConsumerProps<{}>>;
+}
+
+export const { Provider, Consumer }: Context = context;
