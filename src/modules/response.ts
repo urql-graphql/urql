431c2569bdefea6590f0f6b385f3a288d3c55880
Phil Plückthun <phil@kitten.sh>
2018-05-02 10:37:20 +0100

    Add error outlines, interfaces, and stubs

---
+++ src/modules/response.ts
@@ -0,0 +1,5 @@
+import { IResponse, IProcessedResponse } from '../interfaces/response';
+
+const processResponse = (response: IResponse): Promise<IProcessedResponse> => {
+  return Promise.reject(new Error('TODO'));
+};
