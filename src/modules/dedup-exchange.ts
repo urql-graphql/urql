d07e832c731b3c27c8acebdaeaf01c61b42ae44d
Phil Plückthun <phil@kitten.sh>
2018-05-02 18:15:09 +0100

    Define Operations for gql queries and exchanges

---
+++ src/modules/dedup-exchange.ts
@@ -0,0 +1,49 @@
+import Observable from 'zen-observable-ts';
+
+import { IExchange } from '../interfaces/exchange';
+
+// Wraps an exchange and deduplicates in-flight operations by their key
+export const dedupExchange = (forward: IExchange): IExchange => {
+  const inFlight = {};
+
+  return operation => {
+    const { key } = operation;
+
+    // Take existing intermediate observable if it has been created
+    if (inFlight[key] !== undefined) {
+      return inFlight[key];
+    }
+
+    // Keep around one subscription and collect observers for this observable
+    const observers = [];
+    let subscription;
+
+    // Create intermediate observable and only forward to the next exchange once
+    return (inFlight[key] = new Observable(observer => {
+      observers.push(observer);
+
+      if (subscription === undefined) {
+        subscription = forward(operation).subscribe({
+          complete: () => {
+            observers.forEach(x => x.complete());
+            delete inFlight[key];
+          },
+          error: error => {
+            observers.forEach(x => x.error(error));
+          },
+          next: emission => {
+            observers.forEach(x => x.next(emission));
+          },
+        });
+      }
+
+      return () => {
+        if (subscription !== undefined) {
+          subscription.unsubscribe();
+        }
+
+        delete inFlight[key];
+      };
+    }));
+  };
+};
