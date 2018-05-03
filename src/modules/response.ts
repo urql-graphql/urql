c0a67406be6642a882a467477eb758b2e13aa992
Alex Saunders <alexjrsaunders@gmail.com>
2018-05-03 15:59:12 +0100

    fixing removed types errors

--- src/modules/response.ts
+++
@@ -1,5 +0,0 @@
-import { IResponse, IProcessedResponse } from '../interfaces/response';
-
-const processResponse = (response: IResponse): Promise<IProcessedResponse> => {
-  return Promise.reject(new Error('TODO'));
-};
