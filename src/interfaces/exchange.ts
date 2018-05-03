853394914d1f964da71cc7ebec7646ec479a9466
Alex Saunders <alexjrsaunders@gmail.com>
2018-05-03 15:52:25 +0100

    implemented combined error

--- src/interfaces/exchange.ts
+++ src/interfaces/exchange.ts
@@ -2,5 +2,12 @@ import Observable from 'zen-observable-ts';
 
 import { ExecutionResult } from 'graphql';
 import { IOperation } from './operation';
+import { CombinedError } from '../modules/error';
 
-export type IExchange = (operation: IOperation) => Observable<ExecutionResult>;
+export interface IExchangeResult {
+  data: ExecutionResult['data'];
+  error?: CombinedError;
+  typeNames?: string[];
+}
+
+export type IExchange = (operation: IOperation) => Observable<IExchangeResult>;
--- src/modules/error.ts
+++ src/modules/error.ts
@@ -1,22 +1,53 @@
 import { GraphQLError } from 'graphql';
+import { IGraphQLError } from '../interfaces/response';
 
-export class UrqlError extends Error {
+const generateErrorMessage = (
+  networkErr?: Error,
+  graphQlErrs?: IGraphQLError[]
+) => {
+  let error = '';
+  if (networkErr) {
+    error = `[Network] ${networkErr.message}`;
+    return error;
+  }
+
+  for (let err of graphQlErrs) {
+    error += `[GraphQL] ${err.message}\n`;
+  }
+  return error;
+};
+
+const rehydrateGraphQlError = (error: string | IGraphQLError) => {
+  if (typeof error === 'string') {
+    return new GraphQLError(error);
+  } else if (error.message) {
+    return new GraphQLError(error.message);
+  } else {
+    return error;
+  }
+};
+
+export class CombinedError extends Error {
   public message: string;
   public graphQLErrors: GraphQLError[];
   public networkError?: Error;
 
   constructor({
-    message,
     networkError,
     graphQLErrors,
   }: {
-    message?: string;
     networkError?: Error;
-    graphQLErrors?: GraphQLError[];
+    graphQLErrors?: Array<string | IGraphQLError>;
   }) {
-    super(message);
+    const gqlErrors = (graphQLErrors || []).map(rehydrateGraphQlError);
+    const errorMessage = generateErrorMessage(networkError, gqlErrors);
 
-    this.graphQLErrors = graphQLErrors || [];
+    super(errorMessage);
+
+    this.message = errorMessage;
+    this.graphQLErrors = gqlErrors;
     this.networkError = networkError;
+
+    Object.setPrototypeOf(this, CombinedError.prototype);
   }
 }
