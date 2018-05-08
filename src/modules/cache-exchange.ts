0c2c57cc56286f999b42ddf6a6804498543e7b71
Phil Plückthun <phil@kitten.sh>
2018-05-08 21:40:46 +0100

    Add early invalidation to cacheExchange
    Fix #58
    
    Suppose a Client component fetches some data and is then unmounted, its
    response will still remain cached. When a mutation is then performed
    that invalidates its data, the component is not mounted to invalidate
    this response itself. Instead it will render with the stale, cached
    response.
    
    To prevent this change introduces a secondary cache control. This
    control keeps track of typenames as well in the cacheExchange. It maps
    typenames to operation keys (which are also cache keys, woohoo!). When
    a mutation then invalidates a couple of typenames in the cache, the
    cacheExchange can go ahead and look up all cache keys that should be
    invalidated, all while the Client component can stay unmounted.

--- src/modules/cache-exchange.ts
+++ src/modules/cache-exchange.ts
@@ -3,18 +3,41 @@ import Observable from 'zen-observable-ts';
 import { ICache, IExchange, IExchangeResult } from '../interfaces/index';
 import { gankTypeNamesFromResponse } from './typenames';
 
+interface ITypenameInvalidate {
+  [typeName: string]: string[];
+}
+
 // Wraps an exchange and refers/updates the cache according to operations
 export const cacheExchange = (cache: ICache, forward: IExchange): IExchange => {
+  const typenameInvalidate: ITypenameInvalidate = {};
+
   return operation => {
+    const { operationName, key } = operation;
+
     const withTypenames$ = forward(operation).map(
       (response: IExchangeResult) => {
         // Grab typenames from response data
         const typeNames = gankTypeNamesFromResponse(response.data);
+
+        // For mutations, invalidate the cache early so that unmounted Client components don't mount with
+        // stale, cached responses
+        if (operationName === 'mutation') {
+          typeNames.forEach(typename => {
+            const cacheKeys = typenameInvalidate[typename];
+            typenameInvalidate[typename] = [];
+            if (cacheKeys !== undefined) {
+              cacheKeys.forEach(cacheKey => {
+                cache.invalidate(cacheKey);
+              });
+            }
+          });
+        }
+
         return { ...response, typeNames };
       }
     );
 
-    const { operationName, key, context } = operation;
+    const { context } = operation;
     if (operationName !== 'query') {
       return withTypenames$;
     }
@@ -35,6 +58,13 @@ export const cacheExchange = (cache: ICache, forward: IExchange): IExchange => {
           complete: () => observer.complete(),
           error: err => observer.error(err),
           next: (response: IExchangeResult) => {
+            // Mark typenames on typenameInvalidate for early invalidation
+            response.typeNames.forEach(typename => {
+              const cacheKeys =
+                typenameInvalidate[typename] ||
+                (typenameInvalidate[typename] = []);
+              cacheKeys.push(key);
+            });
             // Store data in cache, using serialized query as key
             cache.write(key, response);
             // Return response with typeNames
