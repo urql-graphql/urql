35c8621c76ae942bfe8feec2930f1550ae3811d7
Phil Pluckthun <phil@kitten.sh>
2018-06-28 18:20:47 +0100

    Support additional IGraphQLError fields on rehydration

--- src/modules/error.ts
+++ src/modules/error.ts
@@ -1,4 +1,4 @@
-import { IGraphQLError } from '../interfaces/index';
+import { IGraphQLError, IGraphQLErrorInput } from '../interfaces/index';
 
 const generateErrorMessage = (
   networkErr?: Error,
@@ -17,11 +17,17 @@ const generateErrorMessage = (
   return error.trim();
 };
 
-const rehydrateGraphQlError = (error: string | IGraphQLError): Error => {
+const rehydrateGraphQlError = (
+  error: string | IGraphQLError
+): IGraphQLError => {
   if (typeof error === 'string') {
     return new Error(error);
   } else if (error.message) {
-    return new Error(error.message);
+    const err: IGraphQLError = new Error(error.message);
+    err.path = error.path;
+    err.locations = error.locations;
+    err.extensions = error.extensions;
+    return err;
   } else {
     return error as any;
   }
@@ -34,7 +40,7 @@ export interface CombinedError extends Error {}
 export class CombinedError {
   public name: string;
   public message: string;
-  public graphQLErrors: Error[];
+  public graphQLErrors: IGraphQLError[];
   public networkError?: Error;
   public response?: any;
 
@@ -44,7 +50,7 @@ export class CombinedError {
     response,
   }: {
     networkError?: Error;
-    graphQLErrors?: Array<string | IGraphQLError>;
+    graphQLErrors?: Array<string | IGraphQLErrorInput | IGraphQLError>;
     response?: any;
   }) {
     this.name = 'CombinedError';
