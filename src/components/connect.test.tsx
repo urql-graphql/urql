5edfc5d6120656fcd0f5840bd6de82196a8333dc
Andy Richardson <andy.john.richardson@gmail.com>
2018-11-29 14:34:25 +0000

    add Connect component tests (use enzyme)

--- src/components/connect.test.tsx
+++ src/components/connect.test.tsx
@@ -1,16 +1,60 @@
-/* tslint:disable */
-
+jest.mock('./client');
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import { Connect } from './connect';
-import renderer from 'react-test-renderer';
+import { queryGql, mutationGql } from '../test-utils';
 
-describe('Client Component', () => {
-  it('should provide a context consumer and pass through to client', () => {
-    // @ts-ignore
-    const component = renderer.create(
-      <Connect children={args => <div {...args as any} />} />
+const children = jest.fn().mockReturnValue(() => <h1>Child component</h1>);
+const query = queryGql;
+const mutation = {
+  addUser: mutationGql,
+};
+
+beforeEach(() => {
+  children.mockClear();
+});
+
+it('passes snapshot', () => {
+  const wrapper = shallow(
+    <Connect children={children} mutation={mutation} query={query} />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('provides context consumer', () => {
+  const wrapper = shallow(<Connect children={children} />);
+  expect(wrapper.find('ContextConsumer').length).toBe(1);
+});
+
+describe('UrqlClient', () => {
+  it('is mounted', () => {
+    const wrapper = mount(<Connect children={children} />);
+    expect(wrapper.find('UrqlClient').length).toBe(1);
+  });
+
+  it('is passed children', () => {
+    const wrapper = mount(<Connect children={children} />);
+    expect(wrapper.find('UrqlClient').props()).toHaveProperty(
+      'children',
+      children
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  });
+
+  it('is passed query', () => {
+    const wrapper = mount(<Connect children={children} query={query} />);
+    expect(wrapper.find('UrqlClient').props()).toHaveProperty('query', query);
+  });
+
+  it('is passed mutation', () => {
+    const wrapper = mount(<Connect children={children} mutation={mutation} />);
+    expect(wrapper.find('UrqlClient').props()).toHaveProperty(
+      'mutation',
+      mutation
+    );
+  });
+
+  it('is passed client', () => {
+    const wrapper = mount(<Connect children={children} />);
+    expect(wrapper.find('UrqlClient').props()).toHaveProperty('client', {});
   });
 });
