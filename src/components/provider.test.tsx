0f38d324234e6b35fbbe6c1456496fa0d3a3f764
Andy Richardson <andy.john.richardson@gmail.com>
2018-11-29 14:46:46 +0000

    additional tests for provider

--- src/components/provider.test.tsx
+++ src/components/provider.test.tsx
@@ -1,19 +1,33 @@
-/* tslint:disable */
-
 import React from 'react';
 import { Provider } from './provider';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-describe('Provider Component', () => {
-  it('should provide a context consumer and pass through to client', () => {
+const client = { stub: true };
+const children = <h1>This is a child element</h1>;
+
+it('passes snapshot', () => {
+  const wrapper = shallow(
     // @ts-ignore
-    const component = renderer.create(
-      // @ts-ignore
-      <Provider client="test">
-        <div />
-      </Provider>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    <Provider client={client}>{children}</Provider>
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('mounts ContextProvider', () => {
+  const wrapper = shallow(
+    // @ts-ignore
+    <Provider client={client}>{children}</Provider>
+  );
+
+  expect(wrapper.find('ContextProvider').length).toBe(1);
+});
+
+it('renders children', () => {
+  const wrapper = shallow(
+    // @ts-ignore
+    <Provider client={client}>{children}</Provider>
+  );
+
+  expect(wrapper.find('h1').matchesElement(children)).toBeTruthy();
 });
