4720a00c12bab528df4a7749ca436dd504dd9b11
Andy Richardson <andy.john.richardson@gmail.com>
2018-11-29 14:09:39 +0000

    remove merged type in favour of generic type

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -12,49 +12,6 @@ export interface ConnectProps<T> {
   mutation?: { [type in keyof T]: Mutation };
 }
 
-// This is the type used for the Render Prop. It requires
-// one generic so that TypeScript can properly infer the shape
-// of data and an optional second generic to describe the mutations
-// that will be made available.
-// Note: The following verbose comments are for TypeScript user's autocomplete.
-
-/**
- * Urql's render props.
- */
-export type UrqlProps<Data, Mutations = {}> = {
-  /**
-   * Urql cache
-   */
-  cache: Cache;
-  /**
-   * The data returned by your Urql query.
-   */
-  data: Data | null;
-  /**
-   * Query error, if present.
-   */
-  error: any;
-  /**
-   * Returns true if Urql is fetching data
-   */
-  fetching: boolean;
-  /**
-   * This is like loading but it's false by default, and becomes true after
-   * the first time your query loads. This makes initial loading states easy
-   * and reduces flicker on subsequent fetch/refetches.
-   */
-  loaded: boolean;
-  /**
-   * Manually refetch the query. You can skip the cache, hit the server
-   * and repopulate the cache by calling this like `refetch({ skipCache: true })`.
-   */
-  refetch: (options: { skipFetch?: boolean }, initial?: boolean) => void;
-  /**
-   * Manually refetch all queries from the cache.
-   */
-  refreshAllFromCache: () => void;
-} & Mutations;
-
 export const Connect = function<T>(props: ConnectProps<T>) {
   return (
     <ContextConsumer>
