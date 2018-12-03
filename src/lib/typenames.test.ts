a25cbacf6acf39e2abe8511431c2495fe7aec721
Andy Richardson <andy.john.richardson@gmail.com>
2018-12-03 16:36:35 +0000

    add more tests for typenames and clean up implementation

--- src/lib/typenames.test.ts
+++ src/lib/typenames.test.ts
@@ -14,16 +14,66 @@ describe('formatTypeNames', () => {
 });
 
 describe('gankTypeNamesFromResponse', () => {
-  it('should return all typenames included in a response as an array', () => {
-    let typeNames = gankTypeNamesFromResponse({
+  it('returns all typenames included in a response as an array', () => {
+    const typeNames = gankTypeNamesFromResponse({
       todos: [
         {
           id: 1,
-
           __typename: 'Todo',
         },
       ],
     });
-    expect(typeNames).toMatchObject(['Todo']);
+    expect(typeNames).toEqual(['Todo']);
+  });
+
+  it('does not duplicate typenames', () => {
+    const typeNames = gankTypeNamesFromResponse({
+      todos: [
+        {
+          id: 1,
+          __typename: 'Todo',
+        },
+        {
+          id: 3,
+          __typename: 'Todo',
+        },
+      ],
+    });
+    expect(typeNames).toEqual(['Todo']);
+  });
+
+  it('returns multiple different typenames', () => {
+    const typeNames = gankTypeNamesFromResponse({
+      todos: [
+        {
+          id: 1,
+          __typename: 'Todo',
+        },
+        {
+          id: 3,
+          __typename: 'Avocado',
+        },
+      ],
+    });
+    expect(typeNames).toEqual(['Todo', 'Avocado']);
+  });
+
+  it('works on nested objects', () => {
+    const typeNames = gankTypeNamesFromResponse({
+      todos: [
+        {
+          id: 1,
+          __typename: 'Todo',
+        },
+        {
+          id: 2,
+          subTask: {
+            id: 3,
+            __typename: 'SubTask',
+          },
+        },
+      ],
+    });
+    expect(typeNames).toEqual(['Todo', 'SubTask']);
   });
 });
