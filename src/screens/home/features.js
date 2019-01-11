c63f030a7fd0aedee68e01c8c11157b74941c44b
Sarah Meyer <sarah.meyer@formidable.com>
2019-01-11 12:20:41 -0700

    WIP refactor landing page to styled components

---
+++ src/screens/home/features.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Wrapper = styled.div`
+  display: "flex";
+  flex-direction: "row";
+  justify-content: "space-between";
+`;
+
+class Features extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        {this.props.featureArray.map(feature => {
+          return (
+            <div key={feature.title}>
+              <img src={feature.icon} />
+              <h3>{feature.title}</h3>
+              <p>{feature.description}</p>
+            </div>
+          );
+        })}
+      </Wrapper>
+    );
+  }
+}
+
+Features.propTypes = {
+  featureArray: PropTypes.array
+};
+
+export default Features;
