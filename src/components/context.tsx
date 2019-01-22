6d52cccd84e123f2162a513156c76a98e452a4af
Phil Pluckthun <phil@kitten.sh>
2019-01-22 01:55:07 +0000

    Fix up context types and provide default client

--- src/components/context.tsx
+++ src/components/context.tsx
@@ -1,7 +1,10 @@
-import createReactContext from 'create-react-context';
-import { Client } from '../types';
+import createContext from 'create-react-context';
+import { Client } from '../lib/client';
 
-const context = createReactContext<Client>({} as any); // Requires default value (which is unused)
+// We assume some default options here; mainly not to actually be used
+// but not to error catastrophically if someone is just playing around
+const defaultOptions = { url: '/graphql' };
+const context = createContext<Client>(new Client(defaultOptions));
 
 export const ContextProvider = context.Provider;
 export const ContextConsumer = context.Consumer;
