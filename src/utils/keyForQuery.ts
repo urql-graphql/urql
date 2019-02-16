a519fd976801261ca7849373610d4c4d23ed23b4
Phil Pluckthun <phil@kitten.sh>
2019-02-16 21:55:31 +0000

    Add keyForQuery helper
    This helper is specialised in returning keys for GraphQL documents
    (strings or DocumentNodes) as quickly as possible.

---
+++ src/utils/keyForQuery.ts
@@ -0,0 +1,70 @@
+import { DocumentNode, print } from 'graphql';
+
+interface NameCache {
+  [key: string]: string;
+}
+
+interface WithNameProperty {
+  name?: string;
+  [key: string]: any;
+}
+
+interface WithCacheProperty {
+  __key?: number;
+  [key: string]: any;
+}
+
+/* tslint:disable */
+const hash = (x: string): number => {
+  for (let h = 5381 | 0, i = x.length | 0; i > 0; i++) {
+    h = (h * 33) ^ x.charCodeAt(i);
+  }
+  return h >>> 0;
+};
+/* tslint:enable */
+
+const docNameCache = Object.create(null) as NameCache;
+
+const getKeyForDocNode = (doc: DocumentNode): number => {
+  if ((doc as WithCacheProperty).__key !== undefined) {
+    return (doc as WithCacheProperty).__key as number;
+  }
+
+  // Using print() can be expensive, so we just check the definition nodes' names
+  let name = doc.definitions.reduce((acc, definition) => {
+    const node = definition as WithNameProperty;
+    return acc + (node.name !== undefined ? node.name : '');
+  }, '');
+
+  // This is for inputs that are not using constant references. In such a case
+  // we use the Document names, but we can't use those if they're not unique
+  if (process.env.NODE_ENV !== 'production' && name !== '') {
+    const printed = print(doc);
+    if (!(name in docNameCache)) {
+      docNameCache[name] = printed;
+    } else if (docNameCache[name] !== printed) {
+      console.warn(
+        'Warning: Encountered multiple DocumentNodes with the same name.'
+      );
+    }
+  }
+
+  // If no name is present the stringified document is used
+  if (name === '') {
+    name = print(doc);
+  }
+
+  const key = hash(name);
+  (doc as WithCacheProperty).__key = key;
+  return key;
+};
+
+const getKeyForDocString = (doc: string): number => hash(doc);
+
+export const getKeyForQuery = (doc: string | DocumentNode): number => {
+  if (typeof doc === 'string') {
+    return getKeyForDocString(doc);
+  } else {
+    return getKeyForDocNode(doc);
+  }
+};
