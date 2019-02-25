df1ba24191ca9eb658b29cb7d324d2f959ad10f8
Andy Richardson <andy.john.richardson@gmail.com>
2019-02-25 20:35:23 +0000

    User overloaded types for createRequest

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,22 @@
 import { DocumentNode } from 'graphql';
 import { getKeyForRequest } from './keyForQuery';
 
-export const createRequest = (q: string | DocumentNode, vars?: object) => ({
-  key: getKeyForRequest(q, vars),
-  query: q,
-  variables: vars || {},
-});
+type QueryType = string | DocumentNode;
+
+export function createRequest<Q extends QueryType>(
+  query: Q
+): { key: number; query: Q; variables: {} };
+export function createRequest<Q extends QueryType, T extends object>(
+  query: Q,
+  vars: T
+): { key: number; query: Q; variables: T };
+export function createRequest<Q extends QueryType, T extends object>(
+  q: Q,
+  vars?: T
+) {
+  return {
+    key: getKeyForRequest(q, vars),
+    query: q,
+    variables: vars || {},
+  };
+}
