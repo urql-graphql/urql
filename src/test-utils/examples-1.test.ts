d233db381cecbd5598e512178f75e609c62063e1
Phil Pluckthun <phil@kitten.sh>
2019-03-04 04:17:59 +0000

    Add test suite for "getting started" example

---
+++ src/test-utils/examples-1.test.ts
@@ -0,0 +1,76 @@
+import gql from 'graphql-tag';
+import { query, write } from '../operations';
+import { create, serialize } from '../store';
+
+const Todos = gql`
+  query {
+    todos {
+      __typename
+      id
+      text
+      complete
+    }
+  }
+`;
+
+const ToggleTodo = gql`
+  mutation($id: ID!) {
+    toggleTodo(id: $id) {
+      __typename
+      id
+      text
+      complete
+    }
+  }
+`;
+
+it('passes the "getting-started" example', () => {
+  const store = create();
+  const todosData = {
+    todos: [
+      { id: '0', text: 'Go to the shops', complete: false, __typename: 'Todo' },
+      { id: '1', text: 'Pick up the kids', complete: true, __typename: 'Todo' },
+      { id: '2', text: 'Install urql', complete: false, __typename: 'Todo' },
+    ],
+  };
+
+  const writeRes = write(store, { query: Todos }, todosData);
+
+  expect(writeRes.dependencies).toEqual([
+    'query.todos',
+    'Todo:0',
+    'Todo:1',
+    'Todo:2',
+  ]);
+  expect(serialize(store)).toMatchSnapshot();
+
+  let queryRes = query(store, { query: Todos });
+
+  expect(queryRes.data).toEqual(todosData);
+  expect(queryRes.dependencies).toEqual(writeRes.dependencies);
+  expect(queryRes.isComplete).toBe(true);
+
+  const mutatedTodo = {
+    ...todosData.todos[2],
+    complete: true,
+  };
+
+  const mutationRes = write(
+    store,
+    { query: ToggleTodo, variables: { id: '2' } },
+    {
+      toggleTodo: mutatedTodo,
+    }
+  );
+
+  expect(mutationRes.dependencies).toEqual(['Todo:2']);
+  expect(serialize(store)).toMatchSnapshot();
+
+  queryRes = query(store, { query: Todos });
+
+  expect(queryRes.isComplete).toBe(true);
+  expect(queryRes.data).toEqual({
+    ...todosData,
+    todos: [...todosData.todos.slice(0, 2), mutatedTodo],
+  });
+});
