33257b32ddc46830b63a73600d48a971b54cea8b
boygirl <lauren.eastridge@formidable.com>
2019-03-21 12:48:08 -0700

    simplify webpack config

--- static-config-parts/static-webpack-config.js
+++ static-config-parts/static-webpack-config.js
@@ -1,55 +1,11 @@
 // SURGEON GENERAL'S WARNING: THIS IS NOT A WEBPACK CONFIG, THIS IS A FUNCTION
 // THAT ENHANCES THE BASE REACT-STATIC WEBPACK CONFIG.
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-const staticWebpackConfig = (config, { defaultLoaders, stage }) => {
-  let loaders = [];
-
-  if (stage === "dev") {
-    loaders = [
-      { loader: "style-loader" },
-      { loader: "css-loader" },
-      { loader: "sass-loader" }
-    ];
-  } else {
-    loaders = [
-      {
-        loader: "css-loader",
-        options: {
-          importLoaders: 1,
-          minimize: stage === "prod",
-          sourceMap: false
-        }
-      },
-      {
-        loader: "sass-loader",
-        options: { includePaths: ["src/"] }
-      }
-    ];
-
-    // Don't extract css to file during node build process
-    if (stage !== "node") {
-      loaders = ExtractTextPlugin.extract({
-        fallback: {
-          loader: "style-loader",
-          options: {
-            sourceMap: false,
-            hmr: false
-          }
-        },
-        use: loaders
-      });
-    }
-  }
-
+const staticWebpackConfig = (config, { defaultLoaders }) => {
+  config.devtool = false;
   config.module.rules = [
     {
       oneOf: [
-        {
-          test: /\.s?(a|c)ss$/,
-          use: loaders
-        },
         {
           test: /\.svg$/,
           use: [
