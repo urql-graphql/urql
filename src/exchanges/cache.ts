7299621389f287eec0b018809806a68fa1b87dce
federicobadini <federico@inkofpixel.com>
2019-04-07 01:02:46 +0200

    Cache exchange not forwarding teardown correctly #215

--- src/exchanges/cache.ts
+++ src/exchanges/cache.ts
@@ -73,27 +73,35 @@ export const cacheExchange: Exchange = ({ forward, client }) => {
       })
     );
 
-    const newOps$ = pipe(
-      sharedOps$,
-      filter(op => !shouldSkip(op) && !isOperationCached(op)),
-      map(mapTypeNames),
+    const forwardedOps$ = pipe(
+      merge([
+        pipe(
+          sharedOps$,
+          filter(op => !shouldSkip(op) && !isOperationCached(op)),
+          map(mapTypeNames)
+        ),
+        pipe(
+          sharedOps$,
+          filter(op => shouldSkip(op))
+        ),
+      ]),
       forward,
       tap(response => {
-        if (response.operation.operationName === 'mutation') {
+        if (
+          response.operation &&
+          response.operation.operationName === 'mutation'
+        ) {
           handleAfterMutation(response);
-        } else if (response.operation.operationName === 'query') {
+        } else if (
+          response.operation &&
+          response.operation.operationName === 'query'
+        ) {
           handleAfterQuery(response);
         }
       })
     );
 
-    const skippedOps$ = pipe(
-      sharedOps$,
-      filter(op => shouldSkip(op)),
-      forward
-    );
-
-    return merge([cachedOps$, newOps$, skippedOps$]);
+    return merge([cachedOps$, forwardedOps$]);
   };
 };
 
