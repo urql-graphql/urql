58f971ea984d203c0598ecae6d6a9fefa6dfad34
Kara Stubbs <karaestubbs@gmail.com>
2019-05-21 16:29:56 +0100

    BadgeFixes (#18)
    Fixed mobile view.

--- src/screens/home/more-oss.js
+++ src/screens/home/more-oss.js
@@ -17,24 +17,26 @@ const OuterWrapper = styled.div`
 const OSSCard = styled.div`
   margin: 0 auto 4rem;
   max-width: 43rem;
-  padding-left: 10rem;
   position: relative;
   text-align: left;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   @media (min-width: 768px) {
     width: calc(1 / 2 * 100% - (1 - 1 / 2) * 80px);
-  }
-  @media (min-width: 1024px) {
-    padding-left: 17rem;
+    flex-direction: row;
+    justify-content: space-between;
   }
 `;
 
 const OSSImage = styled.img`
   left: 0;
-  position: absolute;
+  position: relative;
   top: 2rem;
-  width: 8rem;
-  padding: 7px;
+  width: 18rem;
+  max-width: none;
+  padding: 13% 12%;
   @media (min-width: 1024px) {
     padding: 14px;
     width: 14rem;
@@ -52,18 +54,46 @@ const OSSLink = styled.a`
 
 const StyledProjectBadge = styled(ProjectBadge)`
   left: 0;
-  position: absolute;
+  position: relative;
   top: 2rem;
-  width: 8rem;
+  width: 18rem;
   @media (min-width: 1024px) {
     width: 14rem;
   }
 `;
+
+const OSSCopyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    padding-left: 1rem;
+  }
+
+  @media (min-width: 1024px) {
+    padding-left: 2rem;
+  }
+`;
+
+const SecondaryTitleStyled = styled(SecondaryTitle)`
+  text-align: center;
+  @media (min-width: 768px) {
+    text-align: left;
+  }
+`;
+
+const SectionWrapper = styled(Wrapper)`
+  @media (max-width: 768px) {
+    p {
+      text-align: center;
+    }
+  }
+`;
+
 class MoreOSS extends React.Component {
   render() {
     return (
       <OuterWrapper>
-        <Wrapper>
+        <SectionWrapper>
           <SectionTitle>More Open Source from Formidable</SectionTitle>
           {this.props.ossArray.map(card => (
             <OSSCard key={card.title}>
@@ -78,15 +108,19 @@ class MoreOSS extends React.Component {
                     description={card.title}
                   />
                 )}
-                <SecondaryTitle>{card.title}</SecondaryTitle>
               </OSSLink>
-              <BodyCopy>{card.description}</BodyCopy>
+              <OSSCopyContainer>
+                <OSSLink href={card.link}>
+                  <SecondaryTitleStyled>{card.title}</SecondaryTitleStyled>
+                </OSSLink>
+                <BodyCopy>{card.description}</BodyCopy>
+              </OSSCopyContainer>
             </OSSCard>
           ))}
           <Button light="true" to="https://formidable.com/open-source/">
             View All
           </Button>
-        </Wrapper>
+        </SectionWrapper>
       </OuterWrapper>
     );
   }
