8630cac21a9369682669ec6e7b1a1394c37f8d4a
Kara Stubbs <karaestubbs@gmail.com>
2019-05-22 15:16:15 +0100

    GithubButtons (#30)
    * GithubButtons
    
    Implemented single github button as the rest won't work.
    
    * ensure text colour is black e.g. on firefox

--- src/screens/home/get-started.js
+++ src/screens/home/get-started.js
@@ -6,6 +6,7 @@ import { Button } from "../../components/button";
 import { SectionTitle } from "../../components/section-title";
 import { Wrapper } from "../../components/wrapper";
 import constants from "../../constants";
+import octoCat from "../../static/svgs/Octicons-mark-github.svg";
 
 const OuterWrapper = styled.div`
   background: ${constants.color};
@@ -15,13 +16,52 @@ const OuterWrapper = styled.div`
   justify-content: center;
 `;
 
-const GetStartedWrapper = styled(Wrapper)`
+const GetStartedWrapper = styled.div`
   p {
     text-align: center;
   }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 8rem;
 `;
 
-const GetStartedBody = styled(BodyCopy)``;
+const GithubButton = styled.a`
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  height: 3rem;
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  width: 9rem;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 1.6rem;
+  }
+  p {
+    margin: 0;
+    font-size: 14px;
+    font-weight: bold;
+    margin-left: 0.66rem;
+    text-decoration: none;
+    color: black;
+  }
+  &:active {
+    p {
+      color: black;
+    }
+  }
+  &:visited {
+    p {
+      color: black;
+    }
+  }
+`;
+
+const GetStartedTitle = styled(SectionTitle)`
+  margin: 2rem 0 4rem;
+`;
 
 class GetStarted extends React.Component {
   render() {
@@ -30,9 +70,13 @@ class GetStarted extends React.Component {
     return (
       <OuterWrapper>
         <GetStartedWrapper>
-          <SectionTitle>Get Started</SectionTitle>
-          <GetStartedBody>{getStartedObj.description}</GetStartedBody>
+          <GetStartedTitle>Get Started</GetStartedTitle>
+          <BodyCopy>{getStartedObj.description}</BodyCopy>
           <Button to={getStartedObj.link}>Documentation</Button>
+          <GithubButton href="https://github.com/FormidableLabs/urql">
+            <img src={octoCat} />
+            <p>Github</p>
+          </GithubButton>
         </GetStartedWrapper>
       </OuterWrapper>
     );
