45d7f102f1740d96285494f8bd99b0f835fb51ce
Phil Pluckthun <phil@kitten.sh>
2019-06-03 12:22:13 +0100

    Add useRequest hook to simplify logic
    Currently we manually rely on request.key in
    useQuery to ensure that we don't trigger a
    new effect, when it's not necessary, which
    is error-prone.
    
    Instead this hook uses useMemo and checks
    request.key against the last request it generated.
    If the key hasn't changed it returns the previous
    reference.
    
    This ensures reference equality, which removes
    any check of request.key in useQuery

---
+++ src/hooks/useRequest.ts
@@ -0,0 +1,23 @@
+import { DocumentNode } from 'graphql';
+import { useRef, useMemo } from 'react';
+import { GraphQLRequest } from '../types';
+import { createRequest } from '../utils';
+
+/** Creates a request from a query and variables but preserves reference equality if the key isn't changing */
+export const useRequest = (
+  query: string | DocumentNode,
+  variables?: any
+): GraphQLRequest => {
+  const prev = useRef<void | GraphQLRequest>(undefined);
+
+  return useMemo(() => {
+    const request = createRequest(query, variables);
+    // We manually ensure reference equality if the key hasn't changed
+    if (prev.current !== undefined && prev.current.key === request.key) {
+      return prev.current;
+    } else {
+      prev.current = request;
+      return request;
+    }
+  }, [query, variables]);
+};
