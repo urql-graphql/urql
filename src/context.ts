0c6713816390f50aa011a1d86232e75fa17228c6
Phil Pluckthun <phil@kitten.sh>
2019-06-03 16:20:25 +0100

    Remove create-react-context usage

--- src/context.ts
+++ src/context.ts
@@ -1,10 +1,10 @@
-import createContext from 'create-react-context';
+import { createContext } from 'react';
 import { Client, createClient } from './client';
 
 // We assume some default options here; mainly not to actually be used
 // but not to error catastrophically if someone is just playing around
-const defaultOptions = { url: '/graphql' };
+const defaultClient = createClient({ url: '/graphql' });
 
-export const Context = createContext<Client>(createClient(defaultOptions));
+export const Context = createContext<Client>(defaultClient);
 export const Provider = Context.Provider;
 export const Consumer = Context.Consumer;
