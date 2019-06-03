f7e3657c000ce3c513f42a482700a83718ae7377
Phil Pluckthun <phil@kitten.sh>
2019-06-03 17:10:40 +0100

    Adjust useImmediateEffect naming

--- src/hooks/useImmediateEffect.ts
+++ src/hooks/useImmediateEffect.ts
@@ -1,31 +1,34 @@
 import { useRef, useEffect, useCallback } from 'react';
 
-enum EffectState {
-  BeforeMount = 0,
-  AfterMount = 1,
-  Render = 2,
+enum LifecycleState {
+  WillMount = 0,
+  DidMount = 1,
+  Update = 2,
 }
 
 type Effect = () => void | (() => void);
 
 /** This executes an effect immediately on initial render and then treats it as a normal effect */
-export const useImmediateEffect = (effect: Effect, changes?: any[]) => {
-  const state = useRef(EffectState.BeforeMount);
+export const useImmediateEffect = (
+  effect: Effect,
+  changes: ReadonlyArray<any>
+) => {
+  const state = useRef(LifecycleState.WillMount);
   const execute = useCallback(effect, changes);
 
   useEffect(() => {
     // Initially we skip executing the effect since we've already done so on
     // initial render, then we execute it as usual
-    if (state.current === EffectState.Render) {
+    if (state.current === LifecycleState.Update) {
       return execute();
     } else {
-      state.current = EffectState.Render;
+      state.current = LifecycleState.Update;
     }
   }, [execute]);
 
   // On initial render we just execute the effect
-  if (state.current === EffectState.BeforeMount) {
-    state.current = EffectState.AfterMount;
+  if (state.current === LifecycleState.WillMount) {
+    state.current = LifecycleState.DidMount;
     execute();
   }
 };
