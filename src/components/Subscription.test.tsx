6753bb69e3cb7b62002e76047a85cf0e5e5f6915
jdecroock <decroockjovi@gmail.com>
2019-06-07 17:42:49 +0200

    test: sub and mutation

---
+++ src/components/Subscription.test.tsx
@@ -0,0 +1,67 @@
+jest.mock('../client', () => {
+  const d = { data: 1, error: 2 };
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { pipe, interval, map } = require('wonka');
+  const mock = {
+    executeSubscription: jest.fn(() =>
+      pipe(
+        interval(200),
+        map((i: number) => ({ data: i, error: i + 1 }))
+      )
+    ),
+  };
+
+  return {
+    createClient: () => mock,
+    data: d,
+  };
+});
+
+import * as React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { Subscription } from './Subscription';
+import { createClient } from '../client';
+
+// @ts-ignore
+const client = createClient() as { executeSubscription: jest.Mock };
+const query = 'subscription Example { example }';
+
+describe('Subscription', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-console
+    console.log(
+      'supressing console.error output due to react-test-renderer spam (hooks related)'
+    );
+    jest.spyOn(global.console, 'error').mockImplementation();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('Should execute the subscription', done => {
+    let props = {};
+    const Test = () => <p>Hi</p>;
+    const App = () => {
+      return (
+        // @ts-ignore
+        <Subscription query={query}>
+          {({ data, fetching, error }) => {
+            props = { data, fetching, error };
+            return <Test />;
+          }}
+        </Subscription>
+      );
+    };
+    render(<App />);
+    expect(props).toStrictEqual({
+      data: undefined,
+      fetching: true,
+      error: undefined,
+    });
+    setTimeout(() => {
+      expect(props).toStrictEqual({ data: 0, fetching: false, error: 1 });
+      done();
+    }, 200);
+  });
+});
