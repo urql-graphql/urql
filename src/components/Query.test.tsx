f2c21deb7e5b7a2c576f19190beaea30015302e4
jdecroock <decroockjovi@gmail.com>
2019-06-07 17:42:49 +0200

    test: sub and mutation

---
+++ src/components/Query.test.tsx
@@ -0,0 +1,70 @@
+jest.mock('../client', () => {
+  const d = { data: 1234, error: 5678 };
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { map, interval, pipe } = require('wonka');
+  const mock = {
+    executeQuery: jest.fn(() =>
+      pipe(
+        interval(200),
+        map((i: number) => ({ data: i, error: i + 1 }))
+      )
+    ),
+  };
+
+  return {
+    createClient: () => mock,
+    data: d,
+  };
+});
+
+import * as React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { Query } from './Query';
+import { createClient } from '../client';
+
+// @ts-ignore
+const client = createClient();
+const query = '{ example }';
+const variables = {
+  myVar: 1234,
+};
+
+describe('Query', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-console
+    console.log(
+      'supressing console.error output due to react-test-renderer spam (hooks related)'
+    );
+    jest.spyOn(global.console, 'error').mockImplementation();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('Should execute the query', done => {
+    let props = {};
+    const Test = () => <p>Hi</p>;
+    const App = () => {
+      return (
+        // @ts-ignore
+        <Query query={query} variables={variables}>
+          {({ data, fetching, error }) => {
+            props = { data, fetching, error };
+            return <Test />;
+          }}
+        </Query>
+      );
+    };
+    render(<App />);
+    expect(props).toStrictEqual({
+      data: undefined,
+      fetching: true,
+      error: undefined,
+    });
+    setTimeout(() => {
+      expect(props).toStrictEqual({ data: 0, fetching: false, error: 1 });
+      done();
+    }, 200);
+  });
+});
