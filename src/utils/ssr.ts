b3d07ea7a18f405f6a5043f313051f0182c8e569
Andy Richardson <andy.john.richardson@gmail.com>
2019-06-18 13:23:44 +0100

    update SSR check

--- src/utils/ssr.ts
+++ src/utils/ssr.ts
@@ -1,6 +1,1 @@
-export const isSSR =
-  typeof window === 'undefined' ||
-  // @ts-ignore
-  window.document === 'undefined' ||
-  // @ts-ignore
-  window.document.createElement === 'undefined';
+export const isSSR = typeof window !== 'undefined' && 'HTMLElement' in window;
