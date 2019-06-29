823b81fcaf2e7ec3da10913c8d7b4a262d08f66a
Andy Richardson <andy.john.richardson@gmail.com>
2019-06-29 18:11:57 +0100

    fix stack trace index

--- src/hooks/useDevtoolsContext.ts
+++ src/hooks/useDevtoolsContext.ts
@@ -7,7 +7,7 @@ const getHookParent = () => {
    * - named export             " at exports.Home (Home.tsx?9b1b:17)"
    * - default export / inline  " at Home (Home.tsx?9b1b:17)"
    */
-  const stackLine = (new Error().stack as string).split('\n')[4];
+  const stackLine = (new Error().stack as string).split('\n')[3];
   const parsedLine = /.*at (exports\.)?(\w+)/.exec(stackLine);
 
   return parsedLine === null || typeof parsedLine[2] !== 'string'
