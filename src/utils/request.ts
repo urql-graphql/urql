e15d26bfefec74f3b4a475275d7d848df4cfca8d
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-09 10:59:54 +0100

    (feat) - strip whitespace from query

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,11 +2,16 @@ import { DocumentNode, parse } from 'graphql';
 import { getKeyForRequest } from './keyForQuery';
 import { GraphQLRequest, Operation, OperationContext } from '../types';
 
+const parseQuery = (queryString: string) => {
+  const trimmedQuery = queryString.replace(/[\s,]+/g, ' ').trim();
+  return parse(trimmedQuery);
+};
+
 export const createRequest = (
   q: string | DocumentNode,
   vars?: object
 ): GraphQLRequest => {
-  const query = typeof q === 'string' ? parse(q) : q;
+  const query = typeof q === 'string' ? parseQuery(q) : q;
 
   return {
     key: getKeyForRequest(query, vars),
