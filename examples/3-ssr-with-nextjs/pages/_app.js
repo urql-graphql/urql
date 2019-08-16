275fc945f15cbf7850d9d1be732750df64a62067
Ayhan <ayhankuru@yandex.com.tr>
2019-08-16 19:45:29 +0300

    Nextjs Example Update (#386)
    * Update with-urql-client.js
    
    * Update package.json
    
    * Update _app.js

--- examples/3-ssr-with-nextjs/pages/_app.js
+++ examples/3-ssr-with-nextjs/pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import React from 'react';
 
 import withUrqlClient from '../src/with-urql-client';
@@ -8,11 +8,9 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, urqlClient } = this.props;
     return (
-      <Container>
         <Provider value={urqlClient}>
           <Component {...pageProps} />
         </Provider>
-      </Container>
     );
   }
 }
