8a7e3b7dd69b4ab6e007cd5712cccc64b2a88932
Phil Pluckthun <phil@kitten.sh>
2019-08-27 00:22:47 +0100

    Add test for forced subscription teardown

--- src/exchanges/subscription.test.ts
+++ src/exchanges/subscription.test.ts
@@ -1,16 +1,24 @@
 import { print } from 'graphql';
-import { empty, fromValue, pipe, Source, take, toPromise } from 'wonka';
+import {
+  empty,
+  publish,
+  fromValue,
+  pipe,
+  Source,
+  take,
+  toPromise,
+} from 'wonka';
 import { Client } from '../client';
 import { subscriptionOperation, subscriptionResult } from '../test-utils';
 import { OperationResult } from '../types';
 import { subscriptionExchange, SubscriptionForwarder } from './subscription';
 
-const exchangeArgs = {
-  forward: () => empty as Source<OperationResult>,
-  client: {} as Client,
-};
-
 it('should return response data from forwardSubscription observable', async () => {
+  const exchangeArgs = {
+    forward: () => empty as Source<OperationResult>,
+    client: {} as Client,
+  };
+
   const unsubscribe = jest.fn();
   const forwardSubscription: SubscriptionForwarder = operation => {
     expect(operation.query).toBe(print(subscriptionOperation.query));
@@ -37,4 +45,30 @@ it('should return response data from forwardSubscription observable', async () =
 
   expect(data).toMatchSnapshot();
   expect(unsubscribe).toHaveBeenCalled();
+});
+
+it('should tear down the operation if the source subscription ends', () => {
+  const reexecuteOperation = jest.fn();
+  const unsubscribe = jest.fn();
+
+  const exchangeArgs = {
+    forward: () => empty as Source<OperationResult>,
+    client: { reexecuteOperation: reexecuteOperation as any } as Client,
+  };
+
+  const forwardSubscription: SubscriptionForwarder = () => ({
+    subscribe(observer) {
+      observer.complete();
+      return { unsubscribe };
+    },
+  });
+
+  pipe(
+    fromValue(subscriptionOperation),
+    subscriptionExchange({ forwardSubscription })(exchangeArgs),
+    publish
+  );
+
+  expect(unsubscribe).not.toHaveBeenCalled();
+  expect(reexecuteOperation).toHaveBeenCalled();
 });
