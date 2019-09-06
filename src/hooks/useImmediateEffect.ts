5102c6ce41fc29e34e654655a6981e4f3073508a
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-06 14:23:59 +0200

    (fix) - set refs in effect (#418)
    * (fix) - switch to using two refs this allows us to set only in useEffect, this entails the potential danger that we dispatch two network requests.
    
    * (refactor) - convert to boolean instead of enum to save size
    
    * (chore) - add safety guard to prevent memory leaks due to an unexpected interrupt resulting in the useEffect not running
    
    * (chore) - save more bytes

--- src/hooks/useImmediateEffect.ts
+++ src/hooks/useImmediateEffect.ts
@@ -1,35 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import { useRef, useEffect, EffectCallback } from 'react';
-
-type LifecycleState = 0 | 1 | 2;
-
-const WILL_MOUNT = 0;
-const DID_MOUNT = 1;
-const UPDATE = 2;
+import { noop } from '../utils';
 
 /** This is a drop-in replacement for useEffect that will execute the first effect that happens during initial mount synchronously */
 export const useImmediateEffect = (
   effect: EffectCallback,
   changes: ReadonlyArray<any>
 ) => {
-  const teardown = useRef<ReturnType<EffectCallback>>(undefined);
-  const state = useRef<LifecycleState>(WILL_MOUNT);
+  const teardown = useRef<() => void>(noop);
+  const isMounted = useRef<boolean>(false);
 
   // On initial render we just execute the effect
-  if (state.current === WILL_MOUNT) {
-    state.current = DID_MOUNT;
-    teardown.current = effect();
+  if (!isMounted.current) {
+    // There's the slight possibility that we had an interrupt due to
+    // conccurrent mode after running the effect.
+    // This could result in memory leaks.
+    teardown.current();
+    teardown.current = effect() || noop;
   }
 
   useEffect(() => {
     // Initially we skip executing the effect since we've already done so on
     // initial render, then we execute it as usual
-    if (state.current === UPDATE) {
-      return (teardown.current = effect());
-    } else {
-      state.current = UPDATE;
-      return teardown.current;
-    }
+    return isMounted.current
+      ? effect()
+      : ((isMounted.current = true), teardown.current);
   }, changes);
 };
