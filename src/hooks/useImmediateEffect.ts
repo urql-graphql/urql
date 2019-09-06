0925c02572c9c9ea545dd23b09888da08421780e
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-06 13:53:31 +0200

    (refactor) - convert to boolean instead of enum to save size

--- src/hooks/useImmediateEffect.ts
+++ src/hooks/useImmediateEffect.ts
@@ -2,31 +2,26 @@
 
 import { useRef, useEffect, EffectCallback } from 'react';
 
-type LifecycleState = 0 | 1;
-
-const MOUNT = 0;
-const UPDATE = 1;
-
 /** This is a drop-in replacement for useEffect that will execute the first effect that happens during initial mount synchronously */
 export const useImmediateEffect = (
   effect: EffectCallback,
   changes: ReadonlyArray<any>
 ) => {
   const teardown = useRef<ReturnType<EffectCallback>>(undefined);
-  const state = useRef<LifecycleState>(MOUNT);
+  const isMounted = useRef<boolean>(false);
 
   // On initial render we just execute the effect
-  if (state.current === MOUNT) {
+  if (!isMounted.current) {
     teardown.current = effect();
   }
 
   useEffect(() => {
     // Initially we skip executing the effect since we've already done so on
     // initial render, then we execute it as usual
-    if (state.current === UPDATE) {
+    if (isMounted.current) {
       return (teardown.current = effect());
     } else {
-      state.current = UPDATE;
+      isMounted.current = true;
       return teardown.current;
     }
   }, changes);
