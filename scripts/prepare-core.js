e6bdc4f1492309f71c042f3b69302693ee11ec70
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-10 16:08:31 +0200

    (chore) - sync core with main package

--- scripts/prepare-core.js
+++ scripts/prepare-core.js
@@ -12,24 +12,27 @@ if (fs.existsSync(pathToPkgJson)) {
   fs.unlinkSync(pathToPkgJson);
 }
 
-const contents = JSON.stringify({
-  name: '@urql/core',
-  version: '1.5.0',
-  private: true,
-  description:
-    'A highly customizable and versatile GraphQL client to use everywhere',
-  repository: 'https://www.github.com/FormidableLabs/urql',
-  bugs: {
-    url: 'https://github.com/FormidableLabs/urql/issues',
+const mainPkgJson = require('../package.json');
+
+const contents = JSON.stringify(
+  {
+    name: '@urql/core',
+    version: mainPkgJson.version,
+    private: true,
+    description: mainPkgJson.description,
+    repository: mainPkgJson.repository,
+    bugs: mainPkgJson.bugs,
+    homepage: mainPkgJson.homepage,
+    main: '../dist/cjs/core.js',
+    module: '../dist/es/core.js',
+    types: '../dist/types/index.core.d.ts',
+    source: '../src/client.ts',
+    sideEffects: false,
+    author: mainPkgJson.author,
+    license: mainPkgJson.license,
   },
-  homepage: 'https://formidable.com/open-source/urql',
-  main: '../dist/cjs/core.js',
-  module: '../dist/es/core.js',
-  types: '../dist/types/index.core.d.ts',
-  source: '../src/client.ts',
-  sideEffects: false,
-  author: 'Formidable',
-  license: 'MIT',
-}, undefined, 2);
+  undefined,
+  2
+);
 
 fs.writeFileSync(pathToPkgJson, contents);
