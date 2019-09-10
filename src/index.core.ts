3f03c592c58f69880c50bd9c28d8ff1ec729d8d3
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-10 16:47:10 +0200

    (chore) - remove index.core.ts

--- src/index.core.ts
+++
@@ -1,3 +0,0 @@
-// This is the entrypoint for the urql-core bundle.
-// It'll be output to dist/es/urql-core.js and dist/cjs/urql-core.js
-export * from './client';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // This is the entrypoint for the urql bundle.
 // It'll be output to dist/es/urql.js and dist/cjs/urql.js
-export * from './index.core';
+export * from './client';
 export * from './context';
 export * from './components';
 export * from './hooks';
