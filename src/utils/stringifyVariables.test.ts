2272d6040530e8c84b7d8f779fa5611cf4afb0d0
Phil Pluckthun <phil@kitten.sh>
2019-09-18 16:56:08 +0100

    Fix some issues and add tests

---
+++ src/utils/stringifyVariables.test.ts
@@ -0,0 +1,30 @@
+import { stringifyVariables } from './stringifyVariables';
+
+it('stringifies objects stabily', () => {
+  expect(stringifyVariables({ b: 'b', a: 'a' })).toBe('{"a":"a","b":"b"}');
+  expect(stringifyVariables({ x: { b: 'b', a: 'a' } })).toBe(
+    '{"x":{"a":"a","b":"b"}}'
+  );
+});
+
+it('stringifies arrays', () => {
+  expect(stringifyVariables([1, 2])).toBe('[1,2]');
+  expect(stringifyVariables({ x: [1, 2] })).toBe('{"x":[1,2]}');
+});
+
+it('stringifies scalars', () => {
+  expect(stringifyVariables(1)).toBe('1');
+  expect(stringifyVariables('test')).toBe('"test"');
+  expect(stringifyVariables(null)).toBe('null');
+  expect(stringifyVariables(undefined)).toBe('');
+  expect(stringifyVariables(Infinity)).toBe('null');
+  expect(stringifyVariables(1 / 0)).toBe('null');
+});
+
+it('throws for circular structures', () => {
+  expect(() => {
+    const x = { x: null } as any;
+    x.x = x;
+    stringifyVariables(x);
+  }).toThrow();
+});
--- src/utils/stringifyVariables.ts
+++ src/utils/stringifyVariables.ts
@@ -11,15 +11,17 @@ const stringify = (x: any): string => {
     return 'null';
   }
 
-  let out = '[';
+  let out = '';
   if (Array.isArray(x)) {
+    out = '[';
     for (let i = 0, l = x.length; i < l; i++) {
       if (i > 0) out += ',';
       const value = stringify(x[i]);
       out += value.length > 0 ? value : 'null';
     }
 
-    return out + ']';
+    out += ']';
+    return out;
   } else if (seen.has(x)) {
     throw new TypeError('Converting circular structure to JSON');
   }
@@ -27,17 +29,19 @@ const stringify = (x: any): string => {
   const keys = Object.keys(x).sort();
 
   seen.add(x);
+  out = '{';
   for (let i = 0, l = keys.length; i < l; i++) {
-    const key = stringify(keys[i]);
+    const key = keys[i];
     const value = stringify(x[key]);
     if (value.length !== 0) {
-      if (out.length > 0) out += ',';
-      out += key + ':' + value;
+      if (out.length > 1) out += ',';
+      out += stringify(key) + ':' + value;
     }
   }
 
   seen.delete(x);
-  return '{' + out + '}';
+  out += '}';
+  return out;
 };
 
 export const stringifyVariables = (x: any): string => {
