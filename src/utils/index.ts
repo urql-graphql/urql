3613834e5df955127058dc43e917329e317a9133
Phil Plückthun <phil@kitten.sh>
2019-09-18 17:02:15 +0100

    Replace fast-json-stable-stringify (#426)
    * Replace fast-json-stable-stringify
    
    * Fix some issues and add tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ export * from './request';
 export * from './result';
 export * from './typenames';
 export * from './toSuspenseSource';
+export * from './stringifyVariables';
 export * from './withPromise';
 
 export const noop = () => {
