3fb295090927396e0fbde0899780f3f606b67b89
parkerziegler <parker.ziegler@formidable.com>
2019-11-02 21:29:41 -0700

    Add Pokemon type icons to example.

--- example/components/pokemon_list.js
+++ example/components/pokemon_list.js
@@ -2,6 +2,26 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'urql';
 
+const typesToIcon = new Map([
+  ['Bug', '/types/Bug@2x.png'],
+  ['Dark', '/types/Dark@2x.png'],
+  ['Dragon', '/types/Dragon@2x.png'],
+  ['Electric', '/types/Electric@2x.png'],
+  ['Fairy', '/types/Fairy@2x.png'],
+  ['Fighting', '/types/Fight@2x.png'],
+  ['Fire', '/types/Fire@2x.png'],
+  ['Ghost', '/types/Ghost@2x.png'],
+  ['Grass', '/types/Grass@2x.png'],
+  ['Ground', '/types/Ground@2x.png'],
+  ['Ice', '/types/Ice@2x.png'],
+  ['Normal', '/types/Normal@2x.png'],
+  ['Poison', '/types/Poison@2x.png'],
+  ['Psychic', '/types/Psychic@2x.png'],
+  ['Rock', '/types/Rock@2x.png'],
+  ['Steel', '/types/Steel@2x.png'],
+  ['Water', '/types/Water@2x.png'],
+]);
+
 const queryPokemon = gql`
   query pokemon($first: Int!) {
     pokemons(first: $first) {
@@ -36,13 +56,35 @@ const PokemonList = () => {
           <div key={pokemon.id}>
             <img src={pokemon.image} className="pokemon-image" />
             <div>
-              <h3>{pokemon.name}</h3>
-              <p>{pokemon.types.join(', ')}</p>
+              <h2>{pokemon.name}</h2>
+              <div className="pokemon-type-container">
+                {pokemon.types.map(type => {
+                  if (typesToIcon.get(type)) {
+                    return (
+                      <div className="pokemon-type-container__type">
+                        <img
+                          src={typesToIcon.get(type)}
+                          className="pokemon-type-container__type-icon"
+                        />
+                        <span className="pokemon-type-container__type-text">
+                          {type}
+                        </span>
+                      </div>
+                    );
+                  }
+
+                  return null;
+                })}
+              </div>
             </div>
           </div>
         ))}
       </div>
       <style jsx>{`
+        * {
+          font-family: monospace;
+        }
+
         .pokemon-list {
           display: grid;
           grid-template-columns: repeat(4, 1fr);
@@ -53,6 +95,26 @@ const PokemonList = () => {
 
         .pokemon-image {
           height: 15rem;
+        }
+
+        .pokemon-type-container,
+        .pokemon-type-container__type {
+          display: flex;
+        }
+
+        .pokemon-type-container__type {
+          margin-right: 0.5rem;
+        }
+
+        .pokemon-type-container__type-text {
+          background: #f1f8ff;
+          color: #0366d6;
+          padding: 0.5rem;
+          border-radius: 0.5rem;
+        }
+
+        .pokemon-type-container__type-icon {
+          height: 2rem;
         }
       `}</style>
     </>
