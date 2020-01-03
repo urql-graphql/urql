8d1fce5c252743c32356dfaecd235bdac85753b3
jdecroock <decroockjovi@gmail.com>
2020-01-03 17:10:34 +0100

    init repo

---
+++ rollup.config.js
@@ -0,0 +1,195 @@
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import buble from 'rollup-plugin-buble';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import transformPipe from './scripts/transform-pipe';
+
+const pkgInfo = require('./package.json');
+
+let external = ['dns', 'fs', 'path', 'url'];
+if (pkgInfo.peerDependencies)
+  external.push(...Object.keys(pkgInfo.peerDependencies));
+if (pkgInfo.dependencies)
+  external.push(...Object.keys(pkgInfo.dependencies));
+
+const externalPredicate = new RegExp(`^(${external.join('|')})($|/)`);
+const externalTest = id => {
+  if (id === 'babel-plugin-transform-async-to-promises/helpers') {
+    return false;
+  }
+
+  return externalPredicate.test(id);
+};
+
+const terserPretty = terser({
+  sourcemap: true,
+  warnings: true,
+  ecma: 5,
+  keep_fnames: true,
+  ie8: false,
+  compress: {
+    pure_getters: true,
+    toplevel: true,
+    booleans_as_integers: false,
+    keep_fnames: true,
+    keep_fargs: true,
+    if_return: false,
+    ie8: false,
+    sequences: false,
+    loops: false,
+    conditionals: false,
+    join_vars: false
+  },
+  mangle: false,
+  output: {
+    beautify: true,
+    braces: true,
+    indent_level: 2
+  }
+});
+
+const terserMinified = terser({
+  sourcemap: true,
+  warnings: true,
+  ecma: 5,
+  ie8: false,
+  toplevel: true,
+  compress: {
+    keep_infinity: true,
+    pure_getters: true,
+    passes: 10
+  },
+  output: {
+    comments: false
+  }
+});
+
+const makePlugins = (isProduction = false, outputFolder) => [
+  nodeResolve({
+    mainFields: ['module', 'jsnext', 'main'],
+    browser: true
+  }),
+  commonjs({
+    ignoreGlobal: true,
+    include: /\/node_modules\//,
+    namedExports: {
+      'react': Object.keys(require('react'))
+    },
+  }),
+  typescript({
+    typescript: require('typescript'),
+    cacheRoot: './node_modules/.cache/.rts2_cache',
+    useTsconfigDeclarationDir: true,
+    tsconfigDefaults: {
+      compilerOptions: {
+        sourceMap: true
+      },
+    },
+    tsconfigOverride: {
+     exclude: [
+       'src/**/*.test.ts',
+       'src/**/*.test.tsx',
+       'src/**/test-utils/*'
+     ],
+     compilerOptions: {
+        declaration: !isProduction,
+        declarationDir: `${outputFolder}/types/`,
+        target: 'es6',
+      },
+    },
+  }),
+  buble({
+    transforms: {
+      unicodeRegExp: false,
+      dangerousForOf: true,
+      dangerousTaggedTemplateString: true
+    },
+    objectAssign: 'Object.assign',
+    exclude: 'node_modules/**'
+  }),
+  babel({
+    babelrc: false,
+    extensions: [...DEFAULT_EXTENSIONS, 'ts', 'tsx'],
+    exclude: 'node_modules/**',
+    presets: [],
+    plugins: [
+      transformPipe,
+      'babel-plugin-closure-elimination',
+      '@babel/plugin-transform-object-assign',
+      ['@babel/plugin-transform-react-jsx', {
+        pragma: 'React.createElement',
+        pragmaFrag: 'React.Fragment',
+        useBuiltIns: true
+      }],
+      ['babel-plugin-transform-async-to-promises', {
+        inlineHelpers: true,
+        externalHelpers: true
+      }]
+    ]
+  }),
+  isProduction && replace({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+  isProduction ? terserMinified : terserPretty
+].filter(Boolean);
+
+const makeConfig = () => ({
+  input: {
+    core: './src/client.ts',
+    urql: './src/index.ts'
+  },
+  external: externalTest,
+  treeshake: {
+    propertyReadSideEffects: false
+  }
+});
+
+export default [
+  {
+    ...makeConfig(),
+    plugins: makePlugins(false, './dist'),
+    output: [
+      {
+        sourcemap: true,
+        legacy: true,
+        freeze: false,
+        esModule: false,
+        dir: './dist/cjs',
+        format: 'cjs',
+      },
+      {
+        sourcemap: true,
+        legacy: true,
+        freeze: false,
+        esModule: false,
+        dir: './dist/es',
+        format: 'esm',
+      },
+    ],
+  },
+  {
+    ...makeConfig(),
+    plugins: makePlugins(true, './dist'),
+    onwarn: () => {},
+    output: [
+      {
+        sourcemap: false,
+        legacy: true,
+        freeze: false,
+        dir: './dist/cjs/min',
+        format: 'cjs',
+      },
+      {
+        sourcemap: false,
+        legacy: true,
+        freeze: false,
+        dir: './dist/es/min',
+        format: 'esm',
+      },
+    ],
+  },
+];
---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,100 @@
+import { DocumentNode } from 'graphql';
+import { pipe, subscribe, onEnd } from 'wonka';
+import { useState, useRef, useEffect, useCallback } from 'preact/hooks';
+import { OperationContext, RequestPolicy, CombinedError } from 'urql/core';
+
+import { useClient } from '../context';
+import { useRequest } from './useRequest';
+
+const initialState: UseQueryState<any> = {
+  fetching: false,
+  stale: false,
+  data: undefined,
+  error: undefined,
+  extensions: undefined,
+};
+
+export interface UseQueryArgs<V> {
+  query: string | DocumentNode;
+  variables?: V;
+  requestPolicy?: RequestPolicy;
+  pollInterval?: number;
+  context?: Partial<OperationContext>;
+  pause?: boolean;
+}
+
+export interface UseQueryState<T> {
+  fetching: boolean;
+  stale: boolean;
+  data?: T;
+  error?: CombinedError;
+  extensions?: Record<string, any>;
+}
+
+export type UseQueryResponse<T> = [
+  UseQueryState<T>,
+  (opts?: Partial<OperationContext>) => void
+];
+
+// eslint-disable-next-line
+const noop = () => {};
+
+export const useQuery = <T = any, V = object>(
+  args: UseQueryArgs<V>
+): UseQueryResponse<T> => {
+  const unsubscribe = useRef(noop);
+  const client = useClient();
+  const [state, setState] = useState<UseQueryState<T>>(initialState);
+
+  // This creates a request which will keep a stable reference
+  // if request.key doesn't change
+  const request = useRequest(args.query, args.variables);
+
+  const executeQuery = useCallback(
+    (opts?: Partial<OperationContext>) => {
+      unsubscribe.current();
+
+      setState(s => ({ ...s, fetching: true }));
+
+      [unsubscribe.current] = pipe(
+        client.executeQuery(request, {
+          requestPolicy: args.requestPolicy,
+          pollInterval: args.pollInterval,
+          ...args.context,
+          ...opts,
+        }),
+        onEnd(() => setState(s => ({ ...s, fetching: false }))),
+        subscribe(({ data, error, extensions, stale }) => {
+          setState({
+            fetching: false,
+            data,
+            error,
+            extensions,
+            stale: !!stale,
+          });
+        })
+      );
+    },
+    [
+      args.context,
+      args.requestPolicy,
+      args.pollInterval,
+      client,
+      request,
+      setState,
+    ]
+  );
+
+  useEffect(() => {
+    if (args.pause) {
+      unsubscribe.current();
+      setState(s => ({ ...s, fetching: false }));
+      return noop;
+    }
+
+    executeQuery();
+    return () => unsubscribe.current(); // eslint-disable-line
+  }, [executeQuery, args.pause, setState]);
+
+  return [state, executeQuery];
+};
