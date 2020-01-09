7b38017513835a8ee8fd4bb072b44c0eaa6df8fd
Phil Pluckthun <phil@kitten.sh>
2020-01-09 17:25:37 +0000

    Clean up console.log and remove test file

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,6 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import transformPipe from './scripts/transform-pipe';
-console.log(Object.keys(require('scheduler')))
 
 const pkgInfo = require('./package.json');
 
