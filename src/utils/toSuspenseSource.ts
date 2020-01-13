b8c8bb1af8c93151c10c445eefff65c38c211994
Phil Pluckthun <phil@kitten.sh>
2020-01-13 18:04:49 +0000

    Add simplified toSuspenseSource implementation

--- src/utils/toSuspenseSource.ts
+++ src/utils/toSuspenseSource.ts
@@ -1,48 +1,20 @@
-import { pipe, make, onPush, onEnd, subscribe, Source } from 'wonka';
+import { pipe, share, onPush, toPromise, takeWhile, take, Source } from 'wonka';
 
 /** This converts a Source to a suspense Source; It will forward the first result synchronously or throw a promise that resolves when the result becomes available */
-export const toSuspenseSource = <T>(source: Source<T>): Source<T> => {
-  // Create a new Source from scratch so we have full control over the Source's lifecycle
-  return make<T>(({ next, complete }) => {
-    let isCancelled = false;
-    let resolveSuspense: (value: T) => void;
-    let synchronousResult: undefined | T;
+export const toSuspenseSource = <T>(source: Source<T>): Source<T> => sink => {
+  const shared = share(source);
+  let hasResult = false;
+  let hasSuspended = false;
 
-    const { unsubscribe } = pipe(
-      source,
-      // The onPush and onEnd forward the underlying results as usual, so that when no
-      // suspense promise is thrown, the source behaves as it normally would
-      onPush(next),
-      onEnd(complete as any),
-      subscribe(value => {
-        // When this operation resolved synchronously assign the result to
-        // synchronousResult which will be picked up below
-        if (resolveSuspense === undefined) {
-          synchronousResult = value;
-        } else if (!isCancelled) {
-          // Otherwise resolve the thrown promise,
-          resolveSuspense(value);
-          // And end and teardown both sources, since suspense will abort the
-          // underlying rendering component anyway
-          complete();
-          unsubscribe();
-        }
-      })
-    );
+  pipe(
+    shared,
+    takeWhile(() => !hasSuspended),
+    onPush(() => (hasResult = true))
+  )(sink);
 
-    // If we have a synchronous result, push it onto this source, which is synchronous
-    // otherwise throw a new promise which will resolve later
-    if (synchronousResult === undefined) {
-      throw new Promise(resolve => {
-        resolveSuspense = resolve;
-      });
-    }
-
-    // Since promises aren't cancellable we have a flag that prevents
-    // the thrown promise from resolving if this source is cancelled
-    return () => {
-      isCancelled = true;
-      unsubscribe();
-    };
-  });
+  if (!hasResult) {
+    hasSuspended = true;
+    sink(0); /* End */
+    throw pipe(shared, take(1), toPromise);
+  }
 };
