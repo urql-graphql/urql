69b7010d40a891c0fdcbd245f3eb3562406a4e25
Phil Pluckthun <phil@kitten.sh>
2020-01-17 21:10:37 +0000

    Add useBehaviourSubject to useSource.ts

--- src/utils/useSource.ts
+++ src/utils/useSource.ts
@@ -1,6 +1,17 @@
-import { useMemo } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { Subscription, Unsubscribe, useSubscription } from 'use-subscription';
-import { Source, pipe, onPush, share, take, toArray, subscribe } from 'wonka';
+import {
+  Source,
+  make,
+  makeSubject,
+  pipe,
+  concat,
+  onPush,
+  share,
+  take,
+  toArray,
+  subscribe,
+} from 'wonka';
 
 export const useSource = <T>(source: Source<T>, init: T): T =>
   useSubscription(
@@ -34,3 +45,29 @@ export const useSource = <T>(source: Source<T>, init: T): T =>
       };
     }, [init, source])
   );
+
+export const useBehaviourSubject = <T>(value: T) => {
+  const [state] = useState((): [Source<T>, (value: T) => void] => {
+    let prevValue = value;
+
+    const prevValueSource = make<T>(observer => {
+      observer.next(prevValue);
+      observer.complete();
+      return () => {
+        /* noop */
+      };
+    });
+
+    const subject = makeSubject<T>();
+    const source = pipe(
+      concat([prevValueSource, subject.source]),
+      onPush(value => (prevValue = value))
+    );
+
+    return [source, subject.next];
+  });
+
+  useEffect(() => state[1](value), [state, value]);
+
+  return state;
+};
