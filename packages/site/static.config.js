6031afd4f74f13d48d1dd90933071c6874c5fba4
boygirl <lauren.eastridge@formidable.com>
2020-02-16 16:24:19 -0800

    fix docs sidebar and scrolling

--- packages/site/static.config.js
+++ packages/site/static.config.js
@@ -1,14 +1,15 @@
 import { resolve } from 'path';
 
 import { getSidebarItems } from './static-config-helpers/md-data-transforms';
-import { webpack } from './static-config-parts/static-webpack-config';
 import { metaData } from './static-config-parts/constants';
+import { createSharedData } from 'react-static/node'
 
-const docsContentPath = resolve(__dirname, '../../docs/');
+const docsContentPath = resolve(__dirname, '../../docs/core');
 
 export default {
   plugins: [
     'react-static-plugin-styled-components',
+    'react-static-plugin-sitemap',
     'react-static-plugin-react-router',
   ],
   paths: {
@@ -21,8 +22,6 @@ export default {
     devDist: 'node_modules/.cache/react-static/dist/',
     temp: 'node_modules/.cache/react-static/temp/',
   },
-
-  webpack,
   basePath: 'open-source/urql',
   stagingBasePath: 'open-source/urql',
   devBasePath: '',
@@ -33,11 +32,21 @@ export default {
 
   getRoutes: async () => {
     const sidebarItems = await getSidebarItems(docsContentPath);
-    const sidebarHeaders = sidebarItems.map(d => ({
+    const rawSidebarHeaders = sidebarItems.map(d => ({
       title: d.title,
       path: `/${d.slug}/`,
       slug: d.slug,
     }));
+    const rawToc = sidebarItems.reduce((memo, item) => {
+      const subArray = item.data.subHeadings.map(sh => ({
+        content: sh.value,
+        level: sh.depth,
+      }));
+      memo[item.slug] = subArray;
+      return memo;
+    }, {});
+    const toc = createSharedData(rawToc);
+    const sidebarHeaders = createSharedData(rawSidebarHeaders);
 
     return [
       {
@@ -47,31 +56,24 @@ export default {
       {
         path: '/docs',
         template: 'src/screens/docs',
+        sharedData: { toc, sidebarHeaders },
         getData: () => ({
           title: `${metaData.title} | Documentation`,
           markdown: sidebarItems[0].markdown,
           renderedMd: sidebarItems[0].content,
-          sidebarHeaders,
-          tocArray: sidebarItems[0].data.subHeadings.map(sh => ({
-            content: sh.value,
-            level: sh.depth,
-          })),
+          slug: sidebarItems[0].slug,
         }),
         // move slug + path to data in transform, renderedMd to data, and nuke markdown prop
         children: sidebarItems.map(
           ({ slug, path, markdown, content, data }) => ({
             path,
             template: 'src/screens/docs',
+            sharedData: { toc, sidebarHeaders },
             getData: () => ({
               title: data.title,
               markdown,
-              path: `/${slug}/`,
+              slug,
               renderedMd: content,
-              sidebarHeaders,
-              tocArray: data.subHeadings.map(sh => ({
-                content: sh.value,
-                level: sh.depth,
-              })),
             }),
           })
         ),
