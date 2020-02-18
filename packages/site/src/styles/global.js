67cf79ce25a21de636b0005112660a1e39fab0f7
Phil Pluckthun <phil@kitten.sh>
2020-02-18 17:30:45 +0000

    Remove old styling in pages/site

--- packages/site/src/styles/global.js
+++ packages/site/src/styles/global.js
@@ -1,52 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
-import normalize from 'styled-normalize';
-
-import prism from './prism';
 
 export const GlobalStyle = createGlobalStyle`
-    ${normalize}
-    ${prism}
-    html {
-        background: ${({ theme }) => theme.color.black};
-        box-sizing: border-box;
-        font-size: 62.5%;
-        overflow-x: hidden;
-    }
+  * {
+    box-sizing: inherit;
+  }
 
-    * {
-        box-sizing: inherit;
-    }
+  html {
+    background: ${({ theme }) => theme.color.black};
+    box-sizing: border-box;
+    font-size: 62.5%;
+    overflow-x: hidden;
+  }
 
-    body {
-        background: ${({ theme }) => theme.color.white};
-        color: ${({ theme }) => theme.color.darkGray};
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 1.3rem;
-        letter-spacing: -0.03em;
-        margin: 0;
-        padding: 0;
-    }
+  body {
+    background: ${({ theme }) => theme.color.white};
+    color: ${({ theme }) => theme.color.darkGray};
+    font-family: -apple-system, BlinkMacSystemFont, Helvetica, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+    font-size: 1.3rem;
+    margin: 0;
+    padding: 0;
+  }
 
-    p {
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        letter-spacing: -0.03em;
-    }
-
-    h1, h2, h3, h4, h5, h6 {
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        letter-spacing: 0;
-        font-weight: 500;
-    }
-
-    h1 {
-        font-size: 5rem;    
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    img {
-        max-width: 100%;
-    }
+  img {
+    max-width: 100%;
+  }
 `;
