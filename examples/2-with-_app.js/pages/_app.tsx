321328ce26fd20edb34ddd47b0576d731be96be7
Parker Ziegler <parkerziegler@users.noreply.github.com>
2020-02-24 11:41:48 -0800

    Demonstrate usage of getInitialProps in _app.js. (#40)

--- examples/2-with-_app.js/pages/_app.tsx
+++ examples/2-with-_app.js/pages/_app.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { withUrqlClient } from 'next-urql';
-import { AppProps } from 'next/app';
+import { withUrqlClient, NextUrqlAppContext } from 'next-urql';
+import NextApp, { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
 
+// Implement getInitialProps if your Page components call getInitialProps themselves.
+// https://nextjs.org/docs/advanced-features/custom-app
+App.getInitialProps = async (ctx: NextUrqlAppContext) => {
+  const appProps = await NextApp.getInitialProps(ctx);
+
+  return {
+    ...appProps,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
 export default withUrqlClient({ url: 'https://graphql-pokemon.now.sh' })(App);
