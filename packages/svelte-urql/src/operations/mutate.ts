1fb1d728ef7951ea47d79e63125a6df63be2485d
Phil Pluckthun <phil@kitten.sh>
2020-03-04 09:38:24 +0000

    Use client.mutation in mutate helper

--- packages/svelte-urql/src/operations/mutate.ts
+++ packages/svelte-urql/src/operations/mutate.ts
@@ -1,6 +1,5 @@
-import { createRequest, OperationResult, OperationContext } from '@urql/core';
+import { OperationResult, OperationContext } from '@urql/core';
 import { DocumentNode } from 'graphql';
-import { pipe, toPromise } from 'wonka';
 
 import { getClient } from '../context';
 
@@ -10,14 +9,14 @@ export const mutate = <T = any, V = object>(
   context?: Partial<OperationContext>
 ): PromiseLike<OperationResult<T>> => {
   const client = getClient();
-  const request = createRequest(query, variables as any);
-  const mutationResult$ = client.executeMutation(request, context);
-
   let promise: Promise<OperationResult<T>>;
 
   return {
     then(onValue) {
-      if (!promise) promise = pipe(mutationResult$, toPromise);
+      if (!promise) {
+        promise = client.mutation(query, variables as any, context).toPromise();
+      }
+
       return promise.then(onValue);
     },
   };
