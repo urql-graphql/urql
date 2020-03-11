e505762ba73c597968eb8cd0230b2c288d1c2ce4
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-03-11 16:27:02 -0600

    Add next-urql example with suspense exchange

---
+++ packages/next-urql/examples/5-with-suspense-exchange/pages/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+import { withUrqlClient, SSRExchange } from 'next-urql';
+import { dedupExchange, cacheExchange, fetchExchange } from 'urql';
+import fetch from 'isomorphic-unfetch';
+import { suspenseExchange } from '@urql/exchange-suspense';
+
+import PokémonList from '../components/pokemon_list';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <PokémonList />
+  </div>
+);
+
+export default withUrqlClient(
+  { url: 'https://graphql-pokemon.now.sh', fetch, suspense: true },
+  (ssrExchange: SSRExchange) => [
+    dedupExchange,
+    suspenseExchange,
+    cacheExchange,
+    ssrExchange,
+    fetchExchange,
+  ]
+)(Home);
