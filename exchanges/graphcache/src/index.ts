123db997d9ab0b66c1293ccfe6853b42aa9b201c
Phil Pluckthun <phil@kitten.sh>
2020-03-13 11:00:10 +0000

    Remove non-cacheExchange exports

--- exchanges/graphcache/src/index.ts
+++ exchanges/graphcache/src/index.ts
@@ -1,5 +1,3 @@
-export * from './operations';
 export * from './types';
-export { Store, initDataState, clearDataState } from './store';
 export { cacheExchange } from './cacheExchange';
 export { populateExchange } from '@urql/exchange-populate';
