5f5194e915fccaccf16b553e9922153b1dc4a65f
Phil Pluckthun <phil@kitten.sh>
2020-03-13 12:06:59 +0000

    Readd exports

--- exchanges/graphcache/src/index.ts
+++ exchanges/graphcache/src/index.ts
@@ -1,3 +1,11 @@
 export * from './types';
+export * from './operations';
+export {
+  Store,
+  noopDataState,
+  initDataState,
+  clearDataState,
+  reserveLayer,
+} from './store';
 export { cacheExchange } from './cacheExchange';
 export { populateExchange } from '@urql/exchange-populate';
