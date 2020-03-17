caedd0fcb381038773947e7c682b6bfc90cba375
Phil Plückthun <phil@kitten.sh>
2020-03-17 11:58:03 +0000

    (graphcache) - Add support for field-level invalidation to cache.invalidate (#627)
    * Add support for field invaldation to store.invalidate
    
    * Update public type for Cache
    
    * Add changeset
    
    * Update API docs

--- exchanges/graphcache/src/operations/invalidate.ts
+++ exchanges/graphcache/src/operations/invalidate.ts
@@ -1,6 +1,6 @@
 import * as InMemoryData from '../store/data';
-import { OperationRequest } from '../types';
-import { Store } from '../store';
+import { Variables, OperationRequest } from '../types';
+import { Store, keyOfField } from '../store';
 import { read } from './query';
 
 export const invalidate = (store: Store, request: OperationRequest) => {
@@ -19,4 +19,27 @@ export const invalidate = (store: Store, request: OperationRequest) => {
 
   InMemoryData.unforkDependencies();
   InMemoryData.gc(store.data);
+};
+
+interface PartialFieldInfo {
+  fieldKey: string;
+}
+
+export const invalidateEntity = (
+  entityKey: string,
+  field?: string,
+  args?: Variables
+) => {
+  const fields: PartialFieldInfo[] = field
+    ? [{ fieldKey: keyOfField(field, args) }]
+    : InMemoryData.inspectFields(entityKey);
+
+  for (let i = 0, l = fields.length; i < l; i++) {
+    const { fieldKey } = fields[i];
+    if (InMemoryData.readLink(entityKey, fieldKey)) {
+      InMemoryData.writeLink(entityKey, fieldKey, undefined);
+    } else {
+      InMemoryData.writeRecord(entityKey, fieldKey, undefined);
+    }
+  }
 };
