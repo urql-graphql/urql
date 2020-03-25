8e492f12d8dd97da4b5c1334c55185612235cba7
Jovi De Croock <jovi.decroock@formidable.com>
2020-03-25 13:50:40 +0100

    update babel plugin (#671)
    * update babel plugin
    
    * add backwards compat check
    
    * improve readability
    
    * remove types import
    
    * undo templating due to the addition of semicolons
    
    Co-authored-by: Andy Richardson <andy.john.richardson@gmail.com>

--- scripts/babel/transform-debug-target.js
+++ scripts/babel/transform-debug-target.js
@@ -1,3 +1,13 @@
+const dispatchProperty = 'dispatchDebug';
+const visited = 'visitedByDebugTargetTransformer';
+
+const warningDevCheckTemplate = `
+  if (process.env.NODE_ENV !== 'production' && typeof ${dispatchProperty} !== undefined) {
+    NODE;
+  }
+`.trim();
+
+
 const plugin = ({ template, types: t }) => {
   const wrapWithDevCheck = template(
     `
@@ -11,12 +21,34 @@ const plugin = ({ template, types: t }) => {
   );
   return {
     visitor: {
+      ObjectProperty(path) {
+        if (path.node.key && path.node.key.name === dispatchProperty && !path.node[visited]) {
+          path.node[visited] = true;
+		      path.node.value = t.conditionalExpression(
+            t.binaryExpression(
+              '!==',
+              t.memberExpression(
+                t.memberExpression(
+                  t.identifier('process'),
+                  t.identifier('env')
+                ),
+                t.identifier('NODE_ENV')
+              ),
+              t.stringLiteral('production')
+            ),
+            path.node.value,
+            t.arrowFunctionExpression([], t.blockStatement([]))
+          );
+        }
+      },
       ExpressionStatement(path) {
         if (
-          !path.node.expression.callee ||
-          path.node.expression.callee.name !== 'dispatchDebug'
+          !path.node[visited] &&
+          path.node.expression.callee &&
+          path.node.expression.callee.name === dispatchProperty
         ) {
-          return;
+          path.node[visited] = true;
+          path.replaceWith(wrapWithDevCheck({ NODE: path.node }));
         }
 
         path.node.visitedByDebugTargetTransformer = true;
