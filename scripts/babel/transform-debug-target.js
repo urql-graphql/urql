ebb5ad1cfd951cc488db0411c0c05e3fecb3d209
Andy Richardson <andy.john.richardson@gmail.com>
2020-03-25 12:40:58 +0000

    Add babel transform changes

--- scripts/babel/transform-debug-target.js
+++ scripts/babel/transform-debug-target.js
@@ -1,36 +1,29 @@
-const warningDevCheckTemplate = `
-  if (process.env.NODE_ENV !== 'production') {
-    NODE;
-  }
-`.trim();
-
-const visited = 'visitedByDebugTargetTransformer';
-const calleeProperty = 'dispatchEvent';
-const calleeObject = 'debugTarget';
-
 const plugin = ({ template, types: t }) => {
   const wrapWithDevCheck = template(
-    warningDevCheckTemplate,
-    { placeholderPattern: /^NODE$/ }
+    `
+      if (process.env.NODE_ENV !== 'production') {
+        NODE;
+      }
+    `.trim(),
+    {
+      placeholderPattern: /^NODE$/,
+    }
   );
-
   return {
     visitor: {
       ExpressionStatement(path) {
         if (
-          !path.node[visited] &&
-          path.node.expression.callee &&
-          path.node.expression.callee.object &&
-          path.node.expression.callee.object.property &&
-          path.node.expression.callee.object.property.name === calleeObject &&
-          path.node.expression.callee.property.name === calleeProperty
-       	) {
-          path.node[visited] = true;
-          path.replaceWith(wrapWithDevCheck({ NODE: path.node }));
+          !path.node.expression.callee ||
+          path.node.expression.callee.name !== 'dispatchDebug'
+        ) {
+          return;
         }
-      }
-    }
+
+        path.node.visitedByDebugTargetTransformer = true;
+        path.replaceWith(wrapWithDevCheck({ NODE: path.node }));
+      },
+    },
   };
 };
 
-export default plugin;
+module.exports = plugin;
