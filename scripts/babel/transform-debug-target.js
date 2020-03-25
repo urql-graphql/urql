7d784c24749b099cd0b1b56dc5538bab54b6b7b3
Jovi De Croock <decroockjovi@gmail.com>
2020-03-25 13:27:48 +0100

    improve readability

--- scripts/babel/transform-debug-target.js
+++ scripts/babel/transform-debug-target.js
@@ -1,11 +1,12 @@
-const visited = 'visitedByDebugTargetTransformer';
 const dispatchProperty = 'dispatchDebug';
-
+const visited = 'visitedByDebugTargetTransformer';
 const warningDevCheckTemplate = `
-  if (process.env.NODE_ENV !== 'production' && typeof ${dispatchProperty} !== 'undefined') {
+  if (process.env.NODE_ENV !== 'production' && typeof ${dispatchProperty} !== undefined) {
     NODE;
   }
 `.trim();
+
+const dispatchDebugTemplate = `dispatchDebug: process.env.NODE_ENV !== 'production' ? NODE : () => {}`.trim();
 
 const plugin = ({ template, types: t }) => {
   const wrapWithDevCheck = template(
@@ -13,33 +14,24 @@ const plugin = ({ template, types: t }) => {
     { placeholderPattern: /^NODE$/ }
   );
 
+  const wrapDispatchDebug = template(
+    dispatchDebugTemplate,
+    { placeholderPattern: /^NODE$/ }
+  );
+
   return {
     visitor: {
       ObjectProperty(path) {
-        if (path.node.key && path.node.key.name === dispatchProperty && !path.node[visited]) {
+        if (path.node.key && path.node.key.name === 'dispatchDebug' && !path.node[visited]) {
           path.node[visited] = true;
-		      path.node.value = t.conditionalExpression(
-            t.binaryExpression(
-              '!==',
-              t.memberExpression(
-                t.memberExpression(
-                  t.identifier('process'),
-                  t.identifier('env')
-                ),
-                t.identifier('NODE_ENV')
-              ),
-              t.stringLiteral('production')
-            ),
-            path.node.value,
-            t.arrowFunctionExpression([], t.blockStatement([]))
-          )
+		      path.replaceWith(wrapDispatchDebug({ NODE: path.node.value }));
         }
       },
       ExpressionStatement(path) {
         if (
           !path.node[visited] &&
           path.node.expression.callee &&
-          path.node.expression.callee.name === dispatchProperty
+          path.node.expression.callee.name === 'dispatchDebug'
         ) {
           path.node[visited] = true;
           path.replaceWith(wrapWithDevCheck({ NODE: path.node }));
