5de1c12f14c981531d0475cb6bff413d286a0e16
Phil Pluckthun <phil@kitten.sh>
2020-04-20 15:19:35 +0100

    Update fetchExchange tests

--- packages/core/src/exchanges/fetch.test.ts
+++ packages/core/src/exchanges/fetch.test.ts
@@ -1,11 +1,9 @@
 import { empty, fromValue, pipe, Source, subscribe, toPromise } from 'wonka';
-import gql from 'graphql-tag';
-import { print } from 'graphql';
 
 import { Client } from '../client';
 import { queryOperation } from '../test-utils';
 import { OperationResult, OperationType } from '../types';
-import { fetchExchange, convertToGet } from './fetch';
+import { fetchExchange } from './fetch';
 
 const fetch = (global as any).fetch as jest.Mock;
 const abort = jest.fn();
@@ -172,41 +170,5 @@ describe('on teardown', () => {
 
     expect(fetch).toHaveBeenCalledTimes(0);
     expect(abort).toHaveBeenCalledTimes(0);
-  });
-});
-
-describe('convert for GET', () => {
-  it('should do a basic conversion', () => {
-    const query = `query ($id: ID!) { node(id: $id) { id } }`;
-    const variables = { id: 2 };
-    expect(convertToGet('http://localhost:3000', { query, variables })).toBe(
-      `http://localhost:3000?query=${encodeURIComponent(
-        query
-      )}&variables=${encodeURIComponent(JSON.stringify(variables))}`
-    );
-  });
-
-  it('should do a basic conversion with fragments', () => {
-    const nodeFragment = gql`
-      fragment nodeFragment on Node {
-        id
-      }
-    `;
-
-    const variables = { id: 2 };
-    const query = print(gql`
-      query($id: ID!) {
-        node(id: $id) {
-          ...nodeFragment
-        }
-      }
-      ${nodeFragment}
-    `);
-
-    expect(convertToGet('http://localhost:3000', { query, variables })).toBe(
-      `http://localhost:3000?query=${encodeURIComponent(
-        query
-      )}&variables=${encodeURIComponent(JSON.stringify(variables))}`
-    );
   });
 });
