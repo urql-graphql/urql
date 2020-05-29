3b7e31bf0a38099b4069744aa209c39abbbe7729
Phil Pluckthun <phil@kitten.sh>
2020-05-29 18:13:21 +0100

    Add basic error handling to default-storage

--- exchanges/graphcache/src/default-storage/index.ts
+++ exchanges/graphcache/src/default-storage/index.ts
@@ -22,24 +22,35 @@ const database$ = new Promise<IDBDatabase>((resolve, reject) => {
 });
 
 export const readMetadata = (): Promise<SerializedRequest[]> =>
-  database$.then(database => {
-    const transaction = database.transaction(METADATA_STORE_NAME, 'readonly');
-    const store = transaction.objectStore(METADATA_STORE_NAME);
-    const request = store.get('metadata');
+  database$
+    .then(
+      (database): Promise<SerializedRequest[]> => {
+        const transaction = database.transaction(
+          METADATA_STORE_NAME,
+          'readonly'
+        );
+        const store = transaction.objectStore(METADATA_STORE_NAME);
+        const request = store.get('metadata');
 
-    return new Promise(resolve => {
-      request.onsuccess = () => {
-        resolve(request.result);
-      };
-    });
-  });
+        return new Promise(resolve => {
+          request.onsuccess = () => {
+            resolve(request.result);
+          };
+        });
+      }
+    )
+    .catch(() => []);
 
 export const writeMetadata = (metadata: SerializedRequest[]) => {
-  database$.then(database => {
-    const transaction = database.transaction(METADATA_STORE_NAME, 'readonly');
-    const store = transaction.objectStore(METADATA_STORE_NAME);
-    store.put(metadata, 'metadata');
-  });
+  database$
+    .then(database => {
+      const transaction = database.transaction(METADATA_STORE_NAME, 'readonly');
+      const store = transaction.objectStore(METADATA_STORE_NAME);
+      store.put(metadata, 'metadata');
+    })
+    .catch(() => {
+      /* noop */
+    });
 };
 
 const batch: Record<string, string | undefined> = Object.create(null);
@@ -58,51 +69,70 @@ const deserializeBatch = (input: string) => {
 
 export const writeData = (entries: SerializedEntries): Promise<void> => {
   Object.assign(batch, entries);
-  return database$.then(database => {
-    const transaction = database.transaction(ENTRIES_STORE_NAME, 'readwrite');
-    const store = transaction.objectStore(ENTRIES_STORE_NAME);
-    store.put(serializeBatch(), timestamp);
+  return database$
+    .then(
+      (database): Promise<void> => {
+        const transaction = database.transaction(
+          ENTRIES_STORE_NAME,
+          'readwrite'
+        );
+        const store = transaction.objectStore(ENTRIES_STORE_NAME);
+        store.put(serializeBatch(), timestamp);
 
-    return new Promise(resolve => {
-      transaction.oncomplete = () => {
-        resolve();
-      };
+        return new Promise((resolve, reject) => {
+          transaction.onabort = transaction.onerror = () => {
+            reject(transaction.error);
+          };
+
+          transaction.oncomplete = () => {
+            resolve();
+          };
+        });
+      }
+    )
+    .catch(() => {
+      /* noop */
     });
-  });
 };
 
 export const readData = (): Promise<SerializedEntries> => {
-  return database$.then(database => {
-    const transaction = database.transaction(ENTRIES_STORE_NAME, 'readwrite');
-    const store = transaction.objectStore(ENTRIES_STORE_NAME);
-    const request = (store.openKeyCursor || store.openCursor).call(store);
+  return database$
+    .then(database => {
+      const transaction = database.transaction(ENTRIES_STORE_NAME, 'readwrite');
+      const store = transaction.objectStore(ENTRIES_STORE_NAME);
+      const request = (store.openKeyCursor || store.openCursor).call(store);
 
-    const chunks: string[] = [];
-    request.onsuccess = function () {
-      const key = this.result && this.result.key;
-      if (typeof key === 'number' && key >= maxAge) {
-        const request = store.get(key);
-        const index = chunks.length;
-        chunks.push('');
+      const chunks: string[] = [];
+      request.onsuccess = function () {
+        const key = this.result && this.result.key;
+        if (typeof key === 'number' && key >= maxAge) {
+          const request = store.get(key);
+          const index = chunks.length;
+          chunks.push('');
 
-        request.onsuccess = () => {
-          if (typeof request.result === 'string') {
-            if (key === timestamp)
-              Object.assign(batch, deserializeBatch(request.result));
-            chunks[index] = request.result;
-          }
-        };
-      } else if (key) {
-        store.delete(key);
-      }
-    };
-
-    return new Promise(resolve => {
-      transaction.oncomplete = () => {
-        resolve(deserializeBatch(chunks.join('')));
+          request.onsuccess = () => {
+            if (typeof request.result === 'string') {
+              if (key === timestamp)
+                Object.assign(batch, deserializeBatch(request.result));
+              chunks[index] = request.result;
+            }
+          };
+        } else if (key) {
+          store.delete(key);
+        }
       };
-    });
-  });
+
+      return new Promise<SerializedEntries>((resolve, reject) => {
+        transaction.onabort = transaction.onerror = () => {
+          reject(transaction.error);
+        };
+
+        transaction.oncomplete = () => {
+          resolve(deserializeBatch(chunks.join('')));
+        };
+      });
+    })
+    .catch(() => batch);
 };
 
 export const onOnline = (cb: () => void) => {
