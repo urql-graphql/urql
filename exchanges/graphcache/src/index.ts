4fb72353612559b66eb68f4bc9ba950f740731da
Phil Pluckthun <phil@kitten.sh>
2020-05-29 15:46:47 +0100

    Remove the populateExchange export from Graphcache

--- exchanges/graphcache/src/index.ts
+++ exchanges/graphcache/src/index.ts
@@ -2,4 +2,3 @@ export * from './types';
 export { query, write, writeOptimistic } from './operations';
 export { Store, noopDataState, reserveLayer } from './store';
 export { cacheExchange } from './cacheExchange';
-export { populateExchange } from '@urql/exchange-populate';
