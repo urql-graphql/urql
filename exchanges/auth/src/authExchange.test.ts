fee95ea54dbf55ec543adc1eefab114ab86be12c
Phil Pluckthun <phil@kitten.sh>
2020-09-07 16:10:46 +0100

    Update tests and test for mutate() method

--- exchanges/auth/src/authExchange.test.ts
+++ exchanges/auth/src/authExchange.test.ts
@@ -1,23 +1,23 @@
-import {
-  pipe,
-  fromValue,
-  toPromise,
-  take,
-  makeSubject,
-  tap,
-  publish,
-  map,
-} from 'wonka';
+import { pipe, fromValue, toPromise, take, makeSubject, tap, map } from 'wonka';
+
+import { print } from 'graphql';
 import { authExchange } from './authExchange';
+import { Client, Operation, OperationResult } from '@urql/core';
 import { queryOperation } from '@urql/core/test-utils';
 
+const operations: Operation[] = [];
 const exchangeArgs = {
   forward: op$ =>
     pipe(
       op$,
-      map(operation => ({ operation }))
+      map(
+        (operation: Operation): OperationResult => {
+          operations.push(operation);
+          return { operation };
+        }
+      )
     ),
-  client: {},
+  client: new Client({ url: '/api' }),
 } as any;
 
 const withAuthHeader = (operation, token) => {
@@ -41,22 +41,148 @@ const withAuthHeader = (operation, token) => {
   };
 };
 
-describe('on request', () => {
-  it('adds the auth header correctly', async () => {
-    const res = await pipe(
-      fromValue(queryOperation),
-      authExchange({
-        getAuth: async () => ({ token: 'my-token' }),
-        willAuthError: () => false,
-        addAuthToOperation: ({ authState, operation }) => {
-          return withAuthHeader(operation, authState!.token);
-        },
-      })(exchangeArgs),
-      take(1),
-      toPromise
-    );
+beforeEach(() => {
+  operations.length = 0;
+});
 
-    const expectedOperation = {
+it('adds the auth header correctly', async () => {
+  const res = await pipe(
+    fromValue(queryOperation),
+    authExchange({
+      getAuth: async () => ({ token: 'my-token' }),
+      willAuthError: () => false,
+      addAuthToOperation: ({ authState, operation }) => {
+        return withAuthHeader(operation, authState!.token);
+      },
+    })(exchangeArgs),
+    take(1),
+    toPromise
+  );
+
+  const expectedOperation = {
+    ...queryOperation,
+    context: {
+      ...queryOperation.context,
+      authAttempt: false,
+      fetchOptions: {
+        ...(queryOperation.context.fetchOptions || {}),
+        headers: {
+          Authorization: 'my-token',
+        },
+      },
+    },
+  };
+
+  expect(res).toEqual({ operation: expectedOperation });
+});
+
+it('adds the auth header correctly when it is fetched asynchronously', async () => {
+  const res = await pipe(
+    fromValue(queryOperation),
+    authExchange<{ token: string }>({
+      getAuth: async () => {
+        await Promise.resolve();
+        return { token: 'async-token' };
+      },
+      willAuthError: () => false,
+      addAuthToOperation: ({ authState, operation }) => {
+        return withAuthHeader(operation, authState!.token);
+      },
+    })(exchangeArgs),
+    take(1),
+    toPromise
+  );
+
+  const expectedOperation = {
+    ...queryOperation,
+    context: {
+      ...queryOperation.context,
+      authAttempt: false,
+      fetchOptions: {
+        ...(queryOperation.context.fetchOptions || {}),
+        headers: {
+          Authorization: 'async-token',
+        },
+      },
+    },
+  };
+
+  expect(res).toEqual({ operation: expectedOperation });
+});
+
+it('supports calls to the mutate() method in getAuth()', async () => {
+  const res = await pipe(
+    fromValue(queryOperation),
+    authExchange<{ token: string }>({
+      getAuth: async ({ mutate }) => {
+        const result = await mutate('mutation { auth }');
+        expect(print(result.operation.query)).toBe('mutation {\n  auth\n}\n');
+        return { token: 'async-token' };
+      },
+      willAuthError: () => false,
+      addAuthToOperation: ({ authState, operation }) => {
+        return withAuthHeader(operation, authState?.token);
+      },
+    })(exchangeArgs),
+    take(2),
+    toPromise
+  );
+
+  const expectedOperation = {
+    ...queryOperation,
+    context: {
+      ...queryOperation.context,
+      authAttempt: false,
+      fetchOptions: {
+        ...(queryOperation.context.fetchOptions || {}),
+        headers: {
+          Authorization: 'async-token',
+        },
+      },
+    },
+  };
+
+  expect(res).toEqual({ operation: expectedOperation });
+});
+
+it('adds the same token to subsequent operations', async () => {
+  const { source, next } = makeSubject<any>();
+
+  const result = jest.fn();
+  const auth$ = pipe(
+    source,
+    authExchange({
+      getAuth: async () => {
+        await Promise.resolve();
+        return { token: 'my-token' };
+      },
+      willAuthError: () => false,
+      addAuthToOperation: ({ authState, operation }) => {
+        return withAuthHeader(operation, authState!.token);
+      },
+    })(exchangeArgs),
+    tap(result),
+    take(2),
+    toPromise
+  );
+
+  next(queryOperation);
+
+  const secondQuery = {
+    ...queryOperation,
+    context: {
+      ...queryOperation.context,
+      foo: 'bar',
+    },
+  };
+
+  next(secondQuery);
+
+  await auth$;
+  expect(result).toHaveBeenCalledTimes(2);
+
+  expect(result).nthCalledWith(1, {
+    operation: {
       ...queryOperation,
       context: {
         ...queryOperation.context,
@@ -68,104 +194,22 @@ describe('on request', () => {
           },
         },
       },
-    };
-
-    expect(res).toEqual({ operation: expectedOperation });
+    },
   });
 
-  it('adds the auth header correctly when it is fetched asynchronously', async () => {
-    const res = await pipe(
-      fromValue(queryOperation),
-      authExchange({
-        getAuth: async () => {
-          return await new Promise<{ token: string }>(resolve =>
-            setTimeout(() => resolve({ token: 'async-token' }), 500)
-          );
-        },
-        willAuthError: () => false,
-        addAuthToOperation: ({ authState, operation }) => {
-          return withAuthHeader(operation, authState!.token);
-        },
-      })(exchangeArgs),
-      take(1),
-      toPromise
-    );
-
-    const expectedOperation = {
-      ...queryOperation,
+  expect(result).nthCalledWith(2, {
+    operation: {
+      ...secondQuery,
       context: {
-        ...queryOperation.context,
+        ...secondQuery.context,
         authAttempt: false,
         fetchOptions: {
-          ...(queryOperation.context.fetchOptions || {}),
+          ...(secondQuery.context.fetchOptions || {}),
           headers: {
-            Authorization: 'async-token',
+            Authorization: 'my-token',
           },
         },
       },
-    };
-
-    expect(res).toEqual({ operation: expectedOperation });
-  });
-
-  it('adds the same token to subsequent operations', async () => {
-    const { source, next } = makeSubject<any>();
-
-    const result = jest.fn();
-
-    await pipe(
-      source,
-      authExchange({
-        getAuth: async () => ({ token: 'my-token' }),
-        willAuthError: () => false,
-        addAuthToOperation: ({ authState, operation }) => {
-          return withAuthHeader(operation, authState!.token);
-        },
-      })(exchangeArgs),
-      tap(result),
-      publish
-    );
-
-    next(queryOperation);
-    const secondQuery = {
-      ...queryOperation,
-      context: {
-        ...queryOperation.context,
-        foo: 'bar',
-      },
-    };
-    next(secondQuery);
-    expect(result).toHaveBeenCalledTimes(2);
-    expect(result).nthCalledWith(1, {
-      operation: {
-        ...queryOperation,
-        context: {
-          ...queryOperation.context,
-          authAttempt: false,
-          fetchOptions: {
-            ...(queryOperation.context.fetchOptions || {}),
-            headers: {
-              Authorization: 'my-token',
-            },
-          },
-        },
-      },
-    });
-
-    expect(result).nthCalledWith(2, {
-      operation: {
-        ...secondQuery,
-        context: {
-          ...secondQuery.context,
-          authAttempt: false,
-          fetchOptions: {
-            ...(secondQuery.context.fetchOptions || {}),
-            headers: {
-              Authorization: 'my-token',
-            },
-          },
-        },
-      },
-    });
+    },
   });
 });
