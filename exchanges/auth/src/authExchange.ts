ac01f60dfa62dd8491398f0b6fceeab1b1ee3360
Phil Pluckthun <phil@kitten.sh>
2020-09-07 14:07:45 +0100

    Add a mutate method to the authExchange's getAuth params

--- exchanges/auth/src/authExchange.ts
+++ exchanges/auth/src/authExchange.ts
@@ -5,13 +5,25 @@ import {
   fromPromise,
   fromValue,
   filter,
+  onStart,
+  take,
   makeSubject,
+  toPromise,
   merge,
   share,
   takeUntil,
 } from 'wonka';
 
-import { Operation, CombinedError, Exchange } from '@urql/core';
+import {
+  Operation,
+  OperationContext,
+  OperationResult,
+  CombinedError,
+  Exchange,
+  createRequest,
+} from '@urql/core';
+
+import { DocumentNode } from 'graphql';
 
 export interface AuthConfig<T> {
   /** addAuthToOperation() must be provided to add the custom `authState` to an Operation's context, so that it may be picked up by the `fetchExchange`. */
@@ -30,7 +42,15 @@ export interface AuthConfig<T> {
   }): boolean;
 
   /** getAuth() handles how the application refreshes or reauthenticates given a stale `authState` and should return a new `authState` or `null`. */
-  getAuth(params: { authState: T | null }): Promise<T | null>;
+  getAuth(params: {
+    authState: T | null;
+    /** The mutate() method may be used to send one-off mutations to the GraphQL API for the purpose of authentication. */
+    mutate<Data = any, Variables extends object = {}>(
+      query: DocumentNode | string,
+      variables?: Variables,
+      context?: Partial<OperationContext>
+    ): Promise<OperationResult<Data>>;
+  }): Promise<T | null>;
 }
 
 const addAuthAttemptToOperation = (
@@ -50,40 +70,62 @@ export function authExchange<T>({
   didAuthError,
   willAuthError,
 }: AuthConfig<T>): Exchange {
-  return ({ forward }) => {
+  return ({ client, forward }) => {
     const retryQueue: Map<number, Operation> = new Map();
-    let authState: T | null = null;
-
-    const updateAuthState = (newAuthState: T | null) => {
-      authState = newAuthState;
-      pendingPromise = undefined;
-      retryQueue.forEach(retryOperation);
-      retryQueue.clear();
-    };
-
-    let pendingPromise: Promise<any> | void = getAuth({ authState }).then(
-      updateAuthState
-    );
-
-    const refreshAuth = (operation: Operation): Promise<any> => {
-      // add to retry queue to try again later
-      retryQueue.set(operation.key, addAuthAttemptToOperation(operation, true));
-
-      // check that another operation isn't already doing refresh
-      if (!pendingPromise) {
-        pendingPromise = getAuth({ authState })
-          .then(updateAuthState)
-          .catch(() => updateAuthState(null));
-      }
-
-      return pendingPromise;
-    };
-
     const { source: retrySource$, next: retryOperation } = makeSubject<
       Operation
     >();
 
+    let authState: T | null = null;
+
     return operations$ => {
+      function mutate<Data = any, Variables extends object = {}>(
+        query: DocumentNode | string,
+        variables?: Variables,
+        context?: Partial<OperationContext>
+      ): Promise<OperationResult<Data>> {
+        const operation = client.createRequestOperation(
+          'mutation',
+          createRequest(query, variables),
+          context
+        );
+
+        return pipe(
+          result$,
+          onStart(() => retryOperation(operation)),
+          filter(result => result.operation.key === operation.key),
+          take(1),
+          toPromise
+        );
+      }
+
+      const updateAuthState = (newAuthState: T | null) => {
+        authState = newAuthState;
+        pendingPromise = undefined;
+        retryQueue.forEach(retryOperation);
+        retryQueue.clear();
+      };
+
+      let pendingPromise: Promise<any> | void = getAuth({
+        authState,
+        mutate,
+      }).then(updateAuthState);
+
+      const refreshAuth = (operation: Operation): Promise<any> => {
+        // add to retry queue to try again later
+        operation = addAuthAttemptToOperation(operation, true);
+        retryQueue.set(operation.key, operation);
+
+        // check that another operation isn't already doing refresh
+        if (!pendingPromise) {
+          pendingPromise = getAuth({ authState, mutate })
+            .then(updateAuthState)
+            .catch(() => updateAuthState(null));
+        }
+
+        return pendingPromise;
+      };
+
       const sharedOps$ = pipe(operations$, share);
 
       const teardownOps$ = pipe(
@@ -136,9 +178,10 @@ export function authExchange<T>({
         map(operation => addAuthToOperation({ operation, authState }))
       );
 
+      const result$ = pipe(merge([opsWithAuth$, teardownOps$]), forward, share);
+
       return pipe(
-        merge([opsWithAuth$, teardownOps$]),
-        forward,
+        result$,
         filter(({ error, operation }) => {
           if (error && didAuthError && didAuthError({ error, authState })) {
             if (!operation.context.authAttempt) {
