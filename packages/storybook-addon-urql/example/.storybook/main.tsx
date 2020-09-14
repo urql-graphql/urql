d69ea3059be12b17400587080ffdbd9086e6a132
Andy Richardson <andy.john.richardson@gmail.com>
2020-09-14 18:23:14 +0100

    Rename folder

--- packages/storybook-addon-urql/example/.storybook/main.tsx
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.*'],
-  addons: ['@storybook/preset-create-react-app'],
-};
--- packages/storybook-addon-urql/example/.storybook/preview.tsx
+++
@@ -1,4 +0,0 @@
-import { addDecorator } from '@storybook/react';
-import { urqlDecorator } from 'storybook-addon-urql';
-
-addDecorator(urqlDecorator);
