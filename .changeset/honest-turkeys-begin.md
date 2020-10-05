8e7fc5ce1451b24f569343011da717362c1ba3f0
Jovi De Croock <jovi.decroock@formidable.com>
2020-10-05 14:45:59 +0200

    (svelte) - cache & network query should reset stale to false (#1032)
    * try potential fix for cache-and-network stale true
    
    * Add changeset
    
    * move fix to OperationStore and add test
    
    * alter changeset wording
    
    * fix test

---
+++ .changeset/honest-turkeys-begin.md
@@ -0,0 +1,5 @@
+---
+"@urql/svelte": patch
+---
+
+Fix `stale` keeping a `truthy` value on a `cache-and-network` operation.
--- packages/svelte-urql/src/operationStore.test.ts
+++ packages/svelte-urql/src/operationStore.test.ts
@@ -64,6 +64,46 @@ it('adds getters and setters for known values', () => {
   expect(store.query).toBe('{ imperative }');
 });
 
+it('adds stale when not present in update', () => {
+  const variables = {};
+  const context = {};
+  const store = operationStore('{ test }', variables, context);
+
+  const update = {
+    query: '{ update }',
+    variables: undefined,
+    context: { requestPolicy: 'cache-and-network' },
+    fetching: true,
+    data: { update: true },
+    error: undefined,
+    extensions: undefined,
+  };
+
+  _markStoreUpdate(update);
+  store.set(update as any);
+
+  expect(store.query).toBe(update.query);
+  expect(store.variables).toBe(update.variables);
+  expect(store.context).toBe(update.context);
+  expect(store.stale).toBe(false);
+  expect(store.fetching).toBe(update.fetching);
+  expect(store.data).toBe(update.data);
+  expect(store.error).toBe(update.error);
+  expect(store.extensions).toBe(update.extensions);
+
+  const subscriber = jest.fn();
+  store.subscribe(subscriber);
+  expect(subscriber).toHaveBeenCalledTimes(1);
+
+  const state = subscriber.mock.calls[0][0];
+  expect(state.stale).toBe(false);
+  expect(state.query).toBe('{ update }');
+
+  store.query = '{ imperative }';
+  expect(subscriber).toHaveBeenCalledTimes(2);
+  expect(store.query).toBe('{ imperative }');
+});
+
 it('throws when illegal values are set', () => {
   const store = operationStore('{ test }');
 
