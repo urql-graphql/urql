649ba65bc777fa356f70f0a7823e456049a27d33
Phil Pluckthun <phil@kitten.sh>
2020-10-14 13:03:39 +0100

    Remove private Graphcache exports
    In theory these undocumented APIs could be used to write a custom
    normalized cache based on Graphcache's internal logic. However, we
    forgot to expose `initDataState` anyway and additionally these APIs
    were not actually in use by anyone. So for now we'll remove them.

--- exchanges/graphcache/src/index.ts
+++ exchanges/graphcache/src/index.ts
@@ -1,5 +1,3 @@
 export * from './types';
-export { query, write, writeOptimistic } from './operations';
-export { Store, noopDataState, reserveLayer } from './store';
 export { cacheExchange } from './cacheExchange';
 export { offlineExchange } from './offlineExchange';
