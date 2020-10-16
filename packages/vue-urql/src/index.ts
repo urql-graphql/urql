d5f69be53340a3272bc5d0502c403e0aced3c7b2
Jovi De Croock <decroockjovi@gmail.com>
2020-10-16 13:47:24 +0200

    bump and export

--- packages/vue-urql/src/index.ts
+++ packages/vue-urql/src/index.ts
@@ -1,4 +1,5 @@
-export * from '@urql/core';
+// TODO: closure fails on this export for some reason.
+// export * from '@urql/core';
 export * from './useClient';
 export * from './useQuery';
 export * from './useMutation';
