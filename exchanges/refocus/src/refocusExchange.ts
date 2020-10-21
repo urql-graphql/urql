ebdb50eb5b1c45e1a5125ce3eb9e6540db32db47
tatchi <tinencor@gmail.com>
2020-10-21 08:45:21 +0200

    (refocusExchange) switch to visibilitychange api

--- exchanges/refocus/src/refocusExchange.ts
+++ exchanges/refocus/src/refocusExchange.ts
@@ -1,12 +1,36 @@
 import { pipe, tap } from 'wonka';
 import { Exchange, Operation } from '@urql/core';
 
+// Source: https://github.com/vercel/swr/blob/4d6be3df441f925d235a4b26914efcc85ae335a3/src/libs/is-document-visible.ts
+const isDocumentVisible = (): boolean => {
+  if (
+    typeof document !== 'undefined' &&
+    typeof document.visibilityState !== 'undefined'
+  ) {
+    return document.visibilityState !== 'hidden';
+  }
+  // always assume it's visible
+  return true;
+};
+
+// Source: https://github.com/vercel/swr/blob/4d6be3df441f925d235a4b26914efcc85ae335a3/src/libs/is-online.ts
+const isOnline = (): boolean => {
+  if (typeof navigator.onLine !== 'undefined') {
+    return navigator.onLine;
+  }
+  // always assume it's online
+  return true;
+};
+
 export const refocusExchange = (): Exchange => {
   return ({ client, forward }) => ops$ => {
     const watchedOperations = new Map<number, Operation>();
     const observedOperations = new Map<number, number>();
 
-    window.addEventListener('focus', () => {
+    window.addEventListener('visibilitychange', () => {
+      if (!isDocumentVisible() || !isOnline()) {
+        return;
+      }
       watchedOperations.forEach(op => {
         client.reexecuteOperation(
           client.createRequestOperation('query', op, {
