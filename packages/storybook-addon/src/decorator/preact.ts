87d209943d15c19daaa3c2c6ad51b07d9db56ad5
Endre Vegh <endre.vegh@yahoo.com>
2020-11-05 13:49:51 +0000

    add urql devtools as dep

--- packages/storybook-addon/src/decorator/preact.ts
+++ packages/storybook-addon/src/decorator/preact.ts
@@ -1,25 +1,17 @@
 /** FUTURE EXPORT - NOT CURRENTLY BEING USED */
 import { addDecorator } from '@storybook/preact';
 import { Provider, createClient } from '@urql/preact';
+import { devtoolsExchange } from '@urql/devtools';
 import { createElement } from 'preact';
 import { getStorybookExchange } from '../exchange';
-
-/* eslint-disable */
-const { devtoolsExchange } = require('@urql/devtools');
 
 export const urqlDecorator: Parameters<typeof addDecorator>[0] = (
   Story,
   context
 ) => {
-  const exchanges = [getStorybookExchange(context)];
-
-  if (devtoolsExchange) {
-    exchanges.unshift(devtoolsExchange);
-  }
-
   const client = createClient({
     url: '/graphql',
-    exchanges,
+    exchanges: [devtoolsExchange, getStorybookExchange(context)],
   });
 
   return createElement(Provider, { value: client, children: Story(context) });
--- packages/storybook-addon/src/decorator/react.ts
+++ packages/storybook-addon/src/decorator/react.ts
@@ -2,24 +2,16 @@ import '../storybook';
 import { addDecorator } from '@storybook/react';
 import { createElement } from 'react';
 import { Provider, createClient } from 'urql';
+import { devtoolsExchange } from '@urql/devtools';
 import { getStorybookExchange } from '../exchange';
-
-/* eslint-disable */
-const { devtoolsExchange } = require('@urql/devtools');
 
 export const urqlDecorator: Parameters<typeof addDecorator>[0] = (
   Story,
   context
 ) => {
-  const exchanges = [getStorybookExchange(context)];
-
-  if (devtoolsExchange) {
-    exchanges.unshift(devtoolsExchange);
-  }
-
   const client = createClient({
     url: 'storehhh',
-    exchanges,
+    exchanges: [devtoolsExchange, getStorybookExchange(context)],
   });
 
   return createElement(Provider, { value: client, children: Story(context) });
