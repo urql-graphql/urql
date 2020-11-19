9585752837dd77342899f75c6f8b4bbccc05228d
Juan Hart <formidable@Juans-MacBook-Pro.local>
2020-11-19 12:22:08 -0600

    successfully created urql client that performs operations with execute-exchange

--- exchanges/graphcache/benchmarks/urqlClient.js
+++ exchanges/graphcache/benchmarks/urqlClient.js
@@ -1,14 +1,48 @@
-window.process = { env: { NODE_ENV: "development" } };
-
-import { createClient } from 'urql/core';
+import { createClient, dedupExchange } from '@urql/core';
 import { cacheExchange } from '@urql/exchange-graphcache';
 import { executeExchange } from '@urql/exchange-execute';
+import { buildSchema } from 'graphql';
+
+export const cache = cacheExchange({});
+
+// // local schema to be used with Execute Exchange
+const schema = buildSchema(`
+    type Todo {
+        id: ID!
+        text: String!
+    }
+
+    type Query {
+        todos: [Todo]!
+    }
+
+    type Mutation {
+        addTodo( text: String! ): Todo!
+    }
+`);
+
+// // local state to be used with Execute Exchange
+const todos = [
+    { id: 1, text: '1st todo' },
+    { id: 2, text: '2nd todo' }
+];
+
+// // root value with resolvers to be used with Execute Exchange
+const rootValue = {
+    todos: () => todos,
+    addTodo: args => {
+        const todo = { id: todos.length.toString(), ...args };
+        todos.push(todo);
+        return todo;
+    }
+};
 
 const client = createClient({
     url: "http://localhost:3000/graphql",
     exchanges: [
-        cacheExchange({}),
-        executeExchange({})
+        dedupExchange,
+        cache,
+        executeExchange({schema, rootValue})
     ]
 });
 
