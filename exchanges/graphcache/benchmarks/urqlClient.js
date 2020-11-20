70db57d01364649d7e5626100c53ab9574e67ea2
Juan Hart <formidable@Juans-MacBook-Pro.local>
2020-11-20 14:53:46 -0600

    refactored test queries to use async/await to get POC prior to writing read/write benchmarks

--- exchanges/graphcache/benchmarks/urqlClient.js
+++ exchanges/graphcache/benchmarks/urqlClient.js
@@ -3,7 +3,29 @@ import { cacheExchange } from '@urql/exchange-graphcache';
 import { executeExchange } from '@urql/exchange-execute';
 import { buildSchema } from 'graphql';
 
-export const cache = cacheExchange({});
+export const cache = cacheExchange({
+    resolvers: {
+        Todo: {
+            text(parent, args, cache, info) {
+                console.log("query => parent", parent);
+                console.log("query => args", args);
+                console.log("query => cache", cache);
+                console.log("query => info", info);
+                return parent.text;
+            }
+        }
+    },
+    updates: {
+        Mutation: {
+            addTodo: (result, args, cache) => {
+                console.log("mutations => result", result);
+                console.log("mutations => args", args);
+                console.log("mutations => cache", cache);
+                return result;
+            }
+        }
+    }
+});
 
 // // local schema to be used with Execute Exchange
 const schema = buildSchema(`
@@ -22,14 +44,13 @@ const schema = buildSchema(`
 `);
 
 // // local state to be used with Execute Exchange
-const todos = [
-    { id: 1, text: '1st todo' },
-    { id: 2, text: '2nd todo' }
-];
+const todos = [];
 
 // // root value with resolvers to be used with Execute Exchange
 const rootValue = {
-    todos: () => todos,
+    todos: () => {
+        return todos;
+    },
     addTodo: args => {
         const todo = { id: todos.length.toString(), ...args };
         todos.push(todo);
@@ -41,8 +62,9 @@ const client = createClient({
     url: "http://localhost:3000/graphql",
     exchanges: [
         dedupExchange,
-        cache,
-        executeExchange({schema, rootValue})
+        // cache,
+        cacheExchange({}),
+        executeExchange({ schema, rootValue })
     ]
 });
 
