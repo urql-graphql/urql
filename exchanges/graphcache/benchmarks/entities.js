850e55848754f481ded13e5b51c1e3779aca6dfe
Juan Hart <formidable@Juans-MacBook-Pro.local>
2020-11-30 20:54:16 -0600

    added functions to produce different entities to be used in larger read/write benchmarks

--- exchanges/graphcache/benchmarks/entities.js
+++ exchanges/graphcache/benchmarks/entities.js
@@ -1,7 +1,35 @@
-// create a function that will produce Todo entries
-// will take in number relative to iterator count
-export const makeTodo = num => ({
-    id: `${num}`,
-    text: `Todo ${num}`,
-    complete: Boolean(num % 2),
+// functions to produce objects representing entities => todos, writers, books, stores, employees
+export const makeTodo = i => ({
+    id: `${i}`,
+    due: new Date(+new Date() - Math.floor(Math.random() * 10000000000)),
+    text: `Todo ${i}`,
+    complete: Boolean(i % 2),
+});
+export const makeWriter = i => ({
+    id: `${i}`,
+    name: `writer ${i}`,
+    amountOfBooks: Math.random() * 100,
+    recognised: Boolean(i % 2),
+    number: i,
+    interests: 'star wars',
+});
+export const makeBook = i => ({
+    id: `${i}`,
+    title: `book ${i}`,
+    published: Boolean(i % 2),
+    genre: 'Fantasy',
+    rating: (i / Math.random()) * 100,
+    release: new Date(+new Date() - Math.floor(Math.random() * 10000000000)),
+});
+export const makeStore = i => ({
+    id: `${i}`,
+    name: `store ${i}`,
+    started: new Date(+new Date() - Math.floor(Math.random() * 10000000000)),
+    country: countries[Math.floor(Math.random()) * 4],
+});
+export const makeEmployee = i => ({
+    id: `${i}`,
+    name: `employee ${i}`,
+    dateOfBirth: new Date(+new Date() - Math.floor(Math.random() * 10000000000)),
+    origin: countries[Math.floor(Math.random()) * 4],
 });
