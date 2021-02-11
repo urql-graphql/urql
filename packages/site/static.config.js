3efc1ce6c165d4b6f1bc97d30490af647b09a3f6
bacebu4 <vasua14735@icloud.com>
2021-02-11 20:33:55 +0300

    added redirect

--- packages/site/static.config.js
+++ packages/site/static.config.js
@@ -54,5 +54,9 @@ export default {
       path: '404',
       template: require.resolve('./src/screens/404'),
     },
+    {
+      path: '/docs/graphcache/custom-updates',
+      redirect: 'docs/graphcache/cache-updates',
+    },
   ],
 };
