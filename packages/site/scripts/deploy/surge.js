24bcc78f655a05a813807902e2ee20a4a24185f4
Phil Pluckthun <phil@kitten.sh>
2021-04-21 16:28:30 +0100

    Fix PR_NUM for surge deploy script

--- packages/site/scripts/deploy/surge.js
+++ packages/site/scripts/deploy/surge.js
@@ -5,6 +5,17 @@ const path = require('path');
 const chalk = require('chalk');
 const execa = require('execa');
 
+if (!process.env.SURGE_LOGIN || !process.env.SURGE_TOKEN) {
+  console.warn('No SURGE_* env variables received. Skipping.');
+  process.exit(0);
+}
+
+if (!process.env.PR_NUMBER) {
+  console.warn('No PR_NUMBER env variable received. Skipping.');
+  process.exit(0);
+}
+
+const PR_NUM = process.env.PR_NUMBER;
 const PROJECT = 'urql';
 const SRC = path.resolve(__dirname, '../../dist');
 const DOMAIN = `formidable-com-${PROJECT}-staging-${PR_NUM}.surge.sh`;
