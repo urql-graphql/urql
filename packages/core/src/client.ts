7fd44744f10cbc8747353539b12a71199603a01a
Phil Pluckthun <phil@kitten.sh>
2021-04-22 14:36:51 +0100

    Add tests for shared source behaviour

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -238,7 +238,8 @@ export class Client {
           (op: Operation) =>
             op.kind === operation.kind &&
             op.key === operation.key &&
-            op.context.requestPolicy !== 'cache-only'
+            (op.context.requestPolicy === 'network-only' ||
+              op.context.requestPolicy === 'cache-and-network')
         ),
         take(1)
       );
