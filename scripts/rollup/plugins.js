cb984cb7742f06c85f49a2276cacbd23c804a349
jdecroock <decroockjovi@gmail.com>
2021-04-26 18:44:22 +0200

    remove closure-compiler, the current rollup plugin isn't really maintained and we were seeing frequent issues with metro and bundles compiled by closure

--- scripts/rollup/plugins.js
+++ scripts/rollup/plugins.js
@@ -7,7 +7,6 @@ import sucrase from '@rollup/plugin-sucrase';
 import buble from '@rollup/plugin-buble';
 import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
-import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import visualizer from 'rollup-plugin-visualizer';
 import { terser } from 'rollup-plugin-terser';
 
@@ -103,10 +102,6 @@ export const makeOutputPlugins = ({ isProduction, extension }) => {
   return [
     isProduction && replace({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    !settings.mayReexport && compiler({
-      formatting: 'PRETTY_PRINT',
-      compilation_level: 'SIMPLE_OPTIMIZATIONS'
     }),
     cleanup({ extension }),
     isProduction ? terserMinified : terserPretty,
