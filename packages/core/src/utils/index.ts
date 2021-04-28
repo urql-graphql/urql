e8ae745bbad273483d715a0ced779d2b7fb238a6
Phil Pluckthun <phil@kitten.sh>
2021-04-28 13:21:24 +0100

    (core) - Make Client more forgiving by sharing result sources that emit cached results (#1515)
    * Replace operation semaphore with shared behavior source
    
    Replace the client's counting of active operations with a
    single, shared operation result source in a Map. This achieves
    the same semaphore behaviour but replaces it with a binary
    semaphore-like cache, where the cached source remembers the
    last active result.
    
    * Prevent emitting cached result if a new result emitted first
    
    * Reuse client.query() in client.readQuery()
    
    * Add tests for shared source behaviour
    
    * Abstract replay logic in Client
    
    * Add test for shared subscription behaviour
    
    * Fix linting issues

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -4,7 +4,7 @@ export * from './result';
 export * from './typenames';
 export * from './stringifyVariables';
 export * from './maskTypename';
-export * from './withPromise';
+export * from './streamUtils';
 export * from './operation';
 
 export const noop = () => {
