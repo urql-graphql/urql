07f84c8c8bb17545b3dd12a381d2ca1b52108f6a
Yanko Valera <yanko.valera@formidable.com>
2021-04-29 15:13:33 +0200

    no message

--- examples/with-pagination/src/pages/PaginatedNpmSearch.js
+++ examples/with-pagination/src/pages/PaginatedNpmSearch.js
@@ -1,9 +1,53 @@
 import React, { useState } from "react";
-
-import SearchResultPage from './components/SearchResultPage';
+import { gql, useQuery } from "urql";
 
 const limit = 5;
 const query = "graphql";
+
+const NPM_SEARCH = gql`
+  query Search($query: String!, $first: Int!, $after: String) {
+    search(query: $query, first: $first, after: $after) {
+      nodes {
+        id
+        name
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
+
+const SearchResultPage = ({ variables, onLoadMore, isLastPage }) => {
+  const [result] = useQuery({ query: NPM_SEARCH, variables });
+
+  const { data, fetching, error } = result;
+
+  const searchResults = data?.search;
+
+  return (
+    <div>
+      {error && <p>Oh no... {error.message}</p>}
+
+      {fetching && <p>Loading...</p>}
+
+      {searchResults && (
+        <>
+          {searchResults.nodes.map((packageInfo) => (
+            <div key={packageInfo.id}>{packageInfo.id}: {packageInfo.name}</div>
+          ))}
+
+          {isLastPage && searchResults.pageInfo.hasNextPage && (
+            <button onClick={() => onLoadMore(searchResults.pageInfo.endCursor)}>
+              load more
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 const PaginatedNpmSearch = () => {
   const [pageVariables, setPageVariables] = useState([
