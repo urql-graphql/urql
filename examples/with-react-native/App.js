f6979d5be57c05a2c5ad911f9d4ca1501b7c688e
Yanko Valera <yankovalera@gmail.com>
2021-05-05 09:21:15 +0200

    Add with-react-native example (#1607)
    * Add with-react-native example

---
+++ examples/with-react-native/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createClient, Provider } from 'urql';
+
+import PokemonList from './src/screens/PokemonList';
+
+const client = createClient({
+  url: 'https://trygql.dev/graphql/basic-pokedex',
+});
+
+const App = () => {
+  return (
+    <Provider value={client}>
+      <PokemonList />
+    </Provider>
+  );
+};
+
+export default App;
