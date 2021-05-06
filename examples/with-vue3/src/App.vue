9cf7266ca2188aadc77fd8021d1c4203750997bb
Phil Pluckthun <phil@kitten.sh>
2021-05-06 15:40:51 +0100

    Add updated vue-urql example to examples

---
+++ examples/with-vue3/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Suspense>
+    <template #default>
+      <PokemonList />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import { provideClient } from '@urql/vue';
+import PokemonList from './PokemonList.vue'
+
+export default {
+  name: 'App',
+  setup() {
+    provideClient({
+      url: 'https://trygql.dev/graphql/basic-pokedex',
+    });
+  },
+  components: {
+    PokemonList
+  }
+}
+</script>
