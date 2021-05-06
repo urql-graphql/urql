ecbf5bb14d2b9bde7281fe9eabc50e13c7cdcbba
Yanko Valera <yanko.valera@formidable.com>
2021-05-06 15:32:15 +0200

    no message

--- examples/with-cache-updates/src/client/index.js
+++ examples/with-cache-updates/src/client/index.js
@@ -3,114 +3,115 @@ import { makeOperation } from '@urql/core';
 import { authExchange } from '@urql/exchange-auth';
 import { cacheExchange } from '@urql/exchange-graphcache';
 
-const getAuth = async ({ authState }) => {
-  if (!authState) {
-    const token = localStorage.getItem('authToken');
+const cache = cacheExchange({
+  updates: {
+    Mutation: {
+      createLink(result, _args, cache, _info) {
+        const LinksList = gql`
+          query Links($first: Int!) {
+            links(first: $first) {
+              nodes {
+                id
+                canonicalUrl
+              }
+            }
+          }
+        `;
 
-    if (token) {
-      return { token };
-    }
+        const linksPages = cache
+          .inspectFields('Query')
+          .filter(field => field.fieldName === 'links');
 
-    return null;
-  }
+        if (linksPages.length > 0) {
+          const lastField = linksPages[linksPages.length - 1];
 
-  // This is where auth has gone wrong and we need to clean up and redirect to a login page
-  localStorage.clear();
-  window.location.reload();
+          cache.updateQuery(
+            {
+              query: LinksList,
+              variables: { first: lastField.arguments.first },
+            },
+            data => {
+              data.links.nodes.push(result.createLink.node);
 
-  return null;
-};
-
-const addAuthToOperation = ({ authState, operation }) => {
-  if (!authState || !authState.token) {
-    return operation;
-  }
-
-  const fetchOptions =
-    typeof operation.context.fetchOptions === 'function'
-      ? operation.context.fetchOptions()
-      : operation.context.fetchOptions || {};
-
-  return makeOperation(operation.kind, operation, {
-    ...operation.context,
-    fetchOptions: {
-      ...fetchOptions,
-      headers: {
-        ...fetchOptions.headers,
-        Authorization: `Bearer ${authState.token}`,
+              return data;
+            }
+          );
+        }
       },
     },
-  });
-};
-
-const didAuthError = ({ error }) => {
-  return error.graphQLErrors.some(e => e.extensions?.code === 'UNAUTHORIZED');
-};
-
-const willAuthError = ({ operation, authState }) => {
-  if (!authState) {
-    // Detect our login mutation and let this operation through:
-    return (
-      operation.kind !== 'mutation' ||
-      // Here we find any mutation definition with the "signin" field
-      !operation.query.definitions.some(definition => {
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.selectionSet.selections.some(node => {
-            // The field name is just an example, since register may also be an exception
-            return node.kind === 'Field' && node.name.value === 'signin';
-          })
-        );
-      })
-    );
-  }
-
-  return false;
-};
+  },
+});
 
 const client = createClient({
   url: 'https://trygql.dev/graphql/web-collections',
   exchanges: [
     dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          createLink(result, _args, cache, _info) {
-            const LinksList = gql`
-              query Links($first: Int!) {
-                links(first: $first) {
-                  nodes {
-                    id
-                    canonicalUrl
-                  }
-                }
-              }
-            `;
+    cache,
+    authExchange({
+      getAuth: async ({ authState }) => {
+        if (!authState) {
+          const token = localStorage.getItem('authToken');
 
-            const linksPages = cache
-              .inspectFields('Query')
-              .filter(field => field.fieldName === 'links');
+          if (token) {
+            return { token };
+          }
 
-            if (linksPages.length > 0) {
-              const lastField = linksPages[linksPages.length - 1];
+          return null;
+        }
 
-              cache.updateQuery(
-                {
-                  query: LinksList,
-                  variables: { first: lastField.arguments.first },
-                },
-                data => {
-                  data.links.nodes.push(result.createLink.node);
+        // This is where auth has gone wrong and we need to clean up and redirect to a login page
+        localStorage.clear();
+        window.location.reload();
 
-                  return data;
-                }
-              );
-            }
+        return null;
+      },
+      addAuthToOperation: ({ authState, operation }) => {
+        if (!authState || !authState.token) {
+          return operation;
+        }
+
+        const fetchOptions =
+          typeof operation.context.fetchOptions === 'function'
+            ? operation.context.fetchOptions()
+            : operation.context.fetchOptions || {};
+
+        return makeOperation(operation.kind, operation, {
+          ...operation.context,
+          fetchOptions: {
+            ...fetchOptions,
+            headers: {
+              ...fetchOptions.headers,
+              Authorization: `Bearer ${authState.token}`,
+            },
           },
-        },
+        });
+      },
+      didAuthError: ({ error }) => {
+        return error.graphQLErrors.some(
+          e => e.extensions?.code === 'UNAUTHORIZED'
+        );
+      },
+      willAuthError: ({ operation, authState }) => {
+        if (!authState) {
+          // Detect our login mutation and let this operation through:
+          return (
+            operation.kind !== 'mutation' ||
+            // Here we find any mutation definition with the "signin" field
+            !operation.query.definitions.some(definition => {
+              return (
+                definition.kind === 'OperationDefinition' &&
+                definition.selectionSet.selections.some(node => {
+                  // The field name is just an example, since register may also be an exception
+                  return node.kind === 'Field' && node.name.value === 'signin';
+                })
+              );
+            })
+          );
+        }
+
+        return false;
       },
     }),
-    authExchange({ getAuth, addAuthToOperation, didAuthError, willAuthError }),
     fetchExchange,
   ],
 });
