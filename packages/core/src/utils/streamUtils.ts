69bf236e0d4d4ee15198a35159a648bf54cf683d
Phil Pluckthun <phil@kitten.sh>
2021-05-09 15:55:16 +0100

    (core) - Fix concurrent operations not respecting request policies (#1626)
    * Fix up implementation of shared single-source results
    
    * Add test for new request policy behaviour
    
    * Add changeset

--- packages/core/src/utils/streamUtils.ts
+++ packages/core/src/utils/streamUtils.ts
@@ -1,66 +1,9 @@
-import {
-  Operator,
-  Source,
-  pipe,
-  toPromise,
-  take,
-  share,
-  onPush,
-  onStart,
-  onEnd,
-  subscribe,
-  make,
-} from 'wonka';
+import { Source, pipe, toPromise, take } from 'wonka';
 
-import { OperationResult, PromisifiedSource } from '../types';
+import { PromisifiedSource } from '../types';
 
 export function withPromise<T>(source$: Source<T>): PromisifiedSource<T> {
   (source$ as PromisifiedSource<T>).toPromise = () =>
     pipe(source$, take(1), toPromise);
   return source$ as PromisifiedSource<T>;
-}
-
-export type ReplayMode = 'pre' | 'post';
-
-export function replayOnStart<T extends OperationResult>(
-  mode: ReplayMode,
-  start: () => void
-): Operator<T, T> {
-  return source$ => {
-    let replay: T | void;
-
-    const shared$ = pipe(
-      source$,
-      onEnd(() => {
-        replay = undefined;
-      }),
-      onPush(value => {
-        replay = value;
-      }),
-      share
-    );
-
-    return make<T>(observer => {
-      const prevReplay = replay;
-
-      return pipe(
-        shared$,
-        onEnd(observer.complete),
-        onStart(() => {
-          if (mode === 'pre') {
-            start();
-          }
-
-          if (prevReplay !== undefined && prevReplay === replay) {
-            observer.next(
-              mode === 'pre' ? { ...prevReplay, stale: true } : prevReplay
-            );
-          } else if (mode === 'post') {
-            start();
-          }
-        }),
-        subscribe(observer.next)
-      ).unsubscribe;
-    });
-  };
 }
