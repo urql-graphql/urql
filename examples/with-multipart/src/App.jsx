52e3a001021a22796d8572e2de4ff95d590687ea
Phil Pluckthun <phil@kitten.sh>
2021-05-10 16:41:26 +0100

    Migrate examples/with-multipart to Vite

--- examples/with-multipart/src/App.jsx
+++ examples/with-multipart/src/App.jsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { createClient, Provider } from "urql";
 import { multipartFetchExchange } from "@urql/exchange-multipart-fetch";
 
-import FileUpload from "./pages/FileUpload";
+import FileUpload from "./FileUpload";
 
 const client = createClient({
   url: "https://trygql.formidable.dev/graphql/uploads-mock",
   exchanges: [multipartFetchExchange],
-  ],
 });
 
 function App() {
