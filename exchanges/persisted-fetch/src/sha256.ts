dc57ed415fb444740bc26cff0306d36d22e51cfa
_nderscore <_@nderscore.com>
2021-06-26 17:58:58 -0400

    Make indirect eval call to optimize for minifiers

--- exchanges/persisted-fetch/src/sha256.ts
+++ exchanges/persisted-fetch/src/sha256.ts
@@ -31,7 +31,17 @@ const sha256Browser = (bytes: Uint8Array): Promise<Uint8Array> => {
 };
 
 const nodeCrypto =
-  typeof window === 'undefined' ? eval("require('crypto')") : null;
+  typeof window === 'undefined'
+    ? (() => {
+        try {
+          // Call eval indirectly to guarantee no side-effects in module scope
+          // (optimization for minifiers)
+          return (0, eval)("r => r('crypto')")(require);
+        } catch (e) {
+          return null;
+        }
+      })()
+    : null;
 
 export const hash = async (query: string): Promise<string> => {
   if (
