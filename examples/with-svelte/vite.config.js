867f8dab23885052999201d3067b461208350d89
Phil Pluckthun <phil@kitten.sh>
2021-07-06 16:56:14 +0100

    Fix up examples/with-svelte

--- examples/with-svelte/vite.config.js
+++ examples/with-svelte/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    exclude: ['@urql/svelte'],
+  },
 });
