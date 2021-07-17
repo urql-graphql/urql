b5b29269b14fe5f7e96d5c66a5ca26ad8ff9c4d8
Jovi De Croock <decroockjovi@gmail.com>
2021-07-17 10:22:33 +0200

    use isRef

--- packages/vue-urql/src/utils.ts
+++ packages/vue-urql/src/utils.ts
@@ -1,9 +1,9 @@
-import { Ref } from 'vue';
+import { Ref, isRef } from 'vue';
 
 export function unwrapPossibleProxy<V>(
   possibleProxy: V | Ref<V> | undefined
-): V {
-  return possibleProxy && typeof possibleProxy === 'object'
-    ? JSON.parse(JSON.stringify(possibleProxy))
+): V | undefined {
+  return possibleProxy && isRef(possibleProxy)
+    ? possibleProxy.value
     : possibleProxy;
 }
