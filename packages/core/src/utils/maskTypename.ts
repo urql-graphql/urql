f85b47efbdee0ca1e483c90a8b18d0eb5fe4a982
Jovi De Croock <jovi.decroock@formidable.com>
2021-11-10 19:08:08 +0100

    (core) - Fix maskTypename not handling nested arrays (#2074)
    * fix arrays in maskTypename
    
    * fix typing issue
    
    * fix issue with nested data

--- packages/core/src/utils/maskTypename.ts
+++ packages/core/src/utils/maskTypename.ts
@@ -1,21 +1,24 @@
 export const maskTypename = (data: any): any => {
   if (!data || typeof data !== 'object') return data;
 
-  return Object.keys(data).reduce((acc, key: string) => {
-    const value = data[key];
-    if (key === '__typename') {
-      Object.defineProperty(acc, '__typename', {
-        enumerable: false,
-        value,
-      });
-    } else if (Array.isArray(value)) {
-      acc[key] = value.map(maskTypename);
-    } else if (value && typeof value === 'object' && '__typename' in value) {
-      acc[key] = maskTypename(value);
-    } else {
-      acc[key] = value;
-    }
+  return Object.keys(data).reduce(
+    (acc, key: string) => {
+      const value = data[key];
+      if (key === '__typename') {
+        Object.defineProperty(acc, '__typename', {
+          enumerable: false,
+          value,
+        });
+      } else if (Array.isArray(value)) {
+        acc[key] = value.map(maskTypename);
+      } else if (value && typeof value === 'object' && '__typename' in value) {
+        acc[key] = maskTypename(value);
+      } else {
+        acc[key] = value;
+      }
 
-    return acc;
-  }, {});
+      return acc;
+    },
+    Array.isArray(data) ? [] : {}
+  );
 };
