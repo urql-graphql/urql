eea94d42ba54c56298f4db086e7d5c14fc388ef9
jdecroock <decroockjovi@gmail.com>
2021-11-11 17:15:39 +0100

    try and add a core override

---
+++ exchanges/graphcache/addCoreOverride.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+
+const distTypes = path.resolve(process.cwd(), 'dist', 'types');
+
+const contents = `
+import { Client as BaseClient } from '@urql/core';
+import { Cache } from './types';
+
+declare module '@urql/core' {
+  export interface Client extends BaseClient {
+    cache: Cache;
+  }
+}
+`;
+
+// Create file
+fs.writeFileSync(path.resolve(distTypes, 'core.d.ts'), contents, 'utf-8');
+
+// Alter existing file
+const existing = fs.readFileSync(path.resolve(distTypes, 'index.d.ts'));
+const newContents = `import './core.d.ts';
+${existing}
+`;
+fs.writeFileSync(path.resolve(distTypes, 'index.d.ts'), newContents, 'utf-8');
