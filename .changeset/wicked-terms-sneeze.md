d2b2dbd57a4c5a268708c7a22e9ae7c21b16a3d8
Jovi De Croock <decroockjovi@gmail.com>
2021-12-25 10:22:24 +0100

    leverage the new use-sync-external-store package

---
+++ .changeset/wicked-terms-sneeze.md
@@ -0,0 +1,5 @@
+---
+'urql': minor
+---
+
+Leverage the nex `use-sync-external-store` for `useQuery` to make the implementation more in line with React. Our current implementation works already with React Suspense, ... but having an implementation that coexists with React will be better for future changes
--- packages/react-urql/src/hooks/useQuery.ts
+++ packages/react-urql/src/hooks/useQuery.ts
@@ -1,9 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
 import { DocumentNode } from 'graphql';
-import { Source, pipe, subscribe, onEnd, onPush, takeWhile } from 'wonka';
-import { useState, useEffect, useCallback, useMemo } from 'react';
-
+import { onPush, pipe, subscribe, takeWhile } from 'wonka';
+import { useCallback, useMemo, useState } from 'react';
+import { useSyncExternalStore } from 'use-sync-external-store/shim';
 import {
   Client,
   TypedDocumentNode,
@@ -14,10 +13,11 @@ import {
   Operation,
 } from '@urql/core';
 
-import { useClient } from '../context';
 import { useRequest } from './useRequest';
+import { useClient } from '../context';
+import { hasDepsChanged, computeNextState, initialState } from './state';
+import { sourceT } from 'wonka/dist/types/src/Wonka_types.gen';
 import { getCacheForClient } from './cache';
-import { initialState, computeNextState, hasDepsChanged } from './state';
 
 export interface UseQueryArgs<Variables = object, Data = any> {
   query: string | DocumentNode | TypedDocumentNode<Data, Variables>;
@@ -41,55 +41,41 @@ export type UseQueryResponse<Data = any, Variables = object> = [
   (opts?: Partial<OperationContext>) => void
 ];
 
+const notFetching = initialState;
+const fetching = { ...initialState, fetching: true };
+
 const isSuspense = (client: Client, context?: Partial<OperationContext>) =>
   client.suspense && (!context || context.suspense !== false);
-
-let currentInit = false;
 
 export function useQuery<Data = any, Variables = object>(
   args: UseQueryArgs<Variables, Data>
 ): UseQueryResponse<Data, Variables> {
   const client = useClient();
-  const cache = getCacheForClient(client);
-  const suspense = isSuspense(client, args.context);
   const request = useRequest<Data, Variables>(args.query, args.variables);
+  const suspense = isSuspense(client, args.context);
+  const cache = getCacheForClient(client);
 
-  const source = useMemo(() => {
-    if (args.pause) return null;
+  // We need to keep the source in state for 'executeQuery'
+  const [sourcyboi, setSource] = useState<{
+    source: sourceT<OperationResult<Data, Variables>> | null;
+    prevValue: OperationResult<Data, Variables>;
+    deps: Array<any>;
+  }>({
+    source: null,
+    prevValue: notFetching as any,
+    deps: [],
+  });
 
-    const source = client.executeQuery(request, {
-      requestPolicy: args.requestPolicy,
-      ...args.context,
-    });
+  const { source, deps } = sourcyboi;
 
-    return suspense
-      ? pipe(
-          source,
-          onPush(result => {
-            cache.set(request.key, result);
-          })
-        )
-      : source;
-  }, [
-    cache,
-    client,
-    request,
-    suspense,
-    args.pause,
-    args.requestPolicy,
-    args.context,
-  ]);
+  const [getSnapshot, sub] = useMemo(() => {
+    let result = cache.get(request.key);
 
-  const getSnapshot = useCallback(
-    (
-      source: Source<OperationResult<Data, Variables>> | null,
-      suspense: boolean
-    ): Partial<UseQueryState<Data, Variables>> => {
-      if (!source) return { fetching: false };
-
-      let result = cache.get(request.key);
-      if (!result) {
-        let resolve: (value: unknown) => void;
+    const getSnapshot = () => {
+      if (!source) {
+        return notFetching;
+      } else if (!result) {
+        let resolve;
 
         const subscription = pipe(
           source,
@@ -99,14 +85,10 @@ export function useQuery<Data = any, Variables = object>(
             if (resolve) resolve(result);
           })
         );
-
         if (result == null && suspense) {
-          const promise = new Promise(_resolve => {
+          throw (result = new Promise(_resolve => {
             resolve = _resolve;
-          });
-
-          cache.set(request.key, promise);
-          throw promise;
+          }));
         } else {
           subscription.unsubscribe();
         }
@@ -114,112 +96,89 @@ export function useQuery<Data = any, Variables = object>(
         throw result;
       }
 
-      return (result as OperationResult<Data, Variables>) || { fetching: true };
-    },
-    [cache, request]
-  );
-
-  const deps = [
-    client,
-    request,
-    args.requestPolicy,
-    args.context,
-    args.pause,
-  ] as const;
-
-  const [state, setState] = useState(() => {
-    currentInit = true;
-    try {
-      return [
-        source,
-        computeNextState(initialState, getSnapshot(source, suspense)),
-        deps,
-      ] as const;
-    } finally {
-      currentInit = false;
-    }
-  });
-
-  let currentResult = state[1];
-  if (source !== state[0] && hasDepsChanged(state[2], deps)) {
-    setState([
-      source,
-      (currentResult = computeNextState(
-        state[1],
-        getSnapshot(source, suspense)
-      )),
-      deps,
-    ]);
-  }
-
-  useEffect(() => {
-    const source = state[0];
-    const request = state[2][1];
-
-    let hasResult = false;
-
-    const updateResult = (result: Partial<UseQueryState<Data, Variables>>) => {
-      hasResult = true;
-      if (!currentInit) {
-        setState(state => {
-          const nextResult = computeNextState(state[1], result);
-          return state[1] !== nextResult
-            ? [state[0], nextResult, state[2]]
-            : state;
-        });
-      }
+      return result || fetching;
     };
 
-    if (source) {
-      const subscription = pipe(
-        source,
-        onEnd(() => {
-          updateResult({ fetching: false });
-        }),
-        subscribe(updateResult)
-      );
+    const sub = notify => {
+      if (!source) {
+        return () => {
+          /*noop*/
+        };
+      }
 
-      if (!hasResult) updateResult({ fetching: true });
+      const unsub = pipe(
+        source,
+        subscribe(_result => {
+          result = _result;
+          notify();
+        })
+      ).unsubscribe;
 
       return () => {
         cache.dispose(request.key);
-        subscription.unsubscribe();
+        unsub();
       };
-    } else {
-      updateResult({ fetching: false });
-    }
-  }, [cache, state[0], state[2][1]]);
+    };
+
+    return [getSnapshot, sub];
+  }, [source]);
 
   const executeQuery = useCallback(
     (opts?: Partial<OperationContext>) => {
-      const context = {
+      const fetchSource = client.executeQuery(request, {
         requestPolicy: args.requestPolicy,
         ...args.context,
         ...opts,
-      };
-
-      setState(state => {
-        const source = suspense
-          ? pipe(
-              client.executeQuery(request, context),
-              onPush(result => {
-                cache.set(request.key, result);
-              })
-            )
-          : client.executeQuery(request, context);
-        return [source, state[1], deps];
       });
+      const source = suspense
+        ? pipe(
+            fetchSource,
+            onPush(result => {
+              cache.set(request.key, result);
+            })
+          )
+        : fetchSource;
+      setSource(prev => ({
+        prevValue: prev.prevValue,
+        deps: prev.deps,
+        source,
+      }));
     },
-    [
-      client,
-      cache,
-      request,
-      suspense,
-      getSnapshot,
-      args.requestPolicy,
-      args.context,
-    ]
+    [suspense, client, request, args.requestPolicy, args.context]
   );
 
-  return [currentResult, executeQuery];
+  let result = useSyncExternalStore(sub, getSnapshot, getSnapshot);
+  sourcyboi.prevValue = result = computeNextState(
+    sourcyboi.prevValue as any,
+    result
+  );
+
+  const currDeps = [
+    client,
+    request,
+    args.pause,
+    args.requestPolicy,
+    args.context,
+  ];
+  if (hasDepsChanged(deps, currDeps)) {
+    const fetchSource = client.executeQuery(request, {
+      requestPolicy: args.requestPolicy,
+      ...args.context,
+    });
+    const source = suspense
+      ? pipe(
+          fetchSource,
+          onPush(result => {
+            cache.set(request.key, result);
+          })
+        )
+      : fetchSource;
+    setSource({
+      prevValue: result as any,
+      source: args.pause ? null : source,
+      deps: currDeps,
+    });
+  }
+
+  return [result as any, executeQuery];
 }
