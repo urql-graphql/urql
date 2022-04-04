d2a220870826521cbab6af485d669e8e81e3d5f6
Andy Richardson <andy.john.richardson@gmail.com>
2022-04-04 16:46:30 +0100

    Add suspense support

--- packages/storybook-addon/src/decorator/preact.ts
+++ packages/storybook-addon/src/decorator/preact.ts
@@ -1,18 +1,22 @@
 /** FUTURE EXPORT - NOT CURRENTLY BEING USED */
 import { addDecorator } from '@storybook/preact';
-import { Provider, createClient } from '@urql/preact';
+import { Provider, createClient, ClientOptions } from '@urql/preact';
 import { devtoolsExchange } from '@urql/devtools';
 import { createElement } from 'preact';
 import { getStorybookExchange } from '../exchange';
 
-export const urqlDecorator: Parameters<typeof addDecorator>[0] = (
-  Story,
-  context
-) => {
+const createDecorator = (
+  opts?: Partial<ClientOptions>
+): Parameters<typeof addDecorator>[0] => (Story, context) => {
   const client = createClient({
-    url: '/graphql',
+    ...opts,
+    url: 'storehhh',
     exchanges: [devtoolsExchange, getStorybookExchange(context)],
   });
 
   return createElement(Provider, { value: client, children: Story(context) });
 };
+
+export const urqlDecorator = createDecorator();
+
+export const urqlDecoratorSuspense = createDecorator({ suspense: true });
--- packages/storybook-addon/src/decorator/react.ts
+++ packages/storybook-addon/src/decorator/react.ts
@@ -1,18 +1,22 @@
 import '../storybook';
 import { addDecorator } from '@storybook/react';
 import { createElement } from 'react';
-import { Provider, createClient } from 'urql';
+import { Provider, createClient, ClientOptions } from 'urql';
 import { devtoolsExchange } from '@urql/devtools';
 import { getStorybookExchange } from '../exchange';
 
-export const urqlDecorator: Parameters<typeof addDecorator>[0] = (
-  Story,
-  context
-) => {
+const createDecorator = (
+  opts?: Partial<ClientOptions>
+): Parameters<typeof addDecorator>[0] => (Story, context) => {
   const client = createClient({
+    ...opts,
     url: 'storehhh',
     exchanges: [devtoolsExchange, getStorybookExchange(context)],
   });
 
   return createElement(Provider, { value: client }, Story(context));
 };
+
+export const urqlDecorator = createDecorator();
+
+export const urqlDecoratorSuspense = createDecorator({ suspense: true });
