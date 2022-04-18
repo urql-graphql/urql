fbd95b7cb40022f2be0702423da39cddd790e404
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-04-18 11:41:43 -0400

    chore: rename methods to indicate context

--- packages/svelte-urql/src/context.ts
+++ packages/svelte-urql/src/context.ts
@@ -3,16 +3,16 @@ import { Client, ClientOptions } from '@urql/core';
 const _contextKey = '$$_urql';
 
 /** retrieve your client to svelte context */
-export const getClient = (): Client => getContext(_contextKey);
+export const getContextClient = (): Client => getContext(_contextKey);
 
 /** save your client to svelte context  */
-export const setClient = (client: Client): void => {
+export const setContextClient = (client: Client): void => {
   setContext(_contextKey, client);
 };
 
 /** create a client and save it to svelte context */
-export const initClient = (args: ClientOptions): Client => {
+export const initContextClient = (args: ClientOptions): Client => {
   const client = new Client(args);
-  setClient(client);
+  setContextClient(client);
   return client;
 };
