80a416221db69a9a9efed569d159522a88b23e26
Phil Pluckthun <phil@kitten.sh>
2022-06-15 16:32:53 +0100

    Fix cjs-check-plugin

--- scripts/rollup/config.js
+++ scripts/rollup/config.js
@@ -67,7 +67,7 @@ const output = ({ format, isProduction }) => {
     esModule: format !== 'esm',
     externalLiveBindings: format !== 'esm',
     generatedCode: {
-      preset: 'es2015',
+      preset: 'es5',
       reservedNamesAsProps: false,
       objectShorthand: false,
       constBindings: false,
