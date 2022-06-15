b3279056850998d7e1ffb10465168567b46b3838
Phil Pluckthun <phil@kitten.sh>
2022-06-15 15:52:22 +0100

    Remove IE11 support from @urql/exchange-persisted-fetch

--- exchanges/persisted-fetch/src/sha256.ts
+++ exchanges/persisted-fetch/src/sha256.ts
@@ -1,34 +1,14 @@
-const globals: { crypto?: Crypto; msCrypto?: Crypto } = (typeof window !==
-'undefined'
+const globals: { crypto?: Crypto } = (typeof window !== 'undefined'
   ? window
   : typeof self !== 'undefined'
   ? self
   : null) as any;
-const webCrypto = globals && (globals.crypto || globals.msCrypto);
+const webCrypto = globals && globals.crypto;
 
-const sha256Browser = (bytes: Uint8Array): Promise<Uint8Array> => {
-  const hash = webCrypto!.subtle.digest({ name: 'SHA-256' }, bytes);
-  return new Promise((resolve, reject) => {
-    if (globals.msCrypto) {
-      // IE11
-      (hash as any).oncomplete = function onComplete(event: any) {
-        resolve(new Uint8Array(event.target.result));
-      };
-      (hash as any).onerror = function onError(error: Error) {
-        reject(error);
-      };
-    } else {
-      // Standard promise-based
-      Promise.resolve(hash)
-        .then(function (result) {
-          resolve(new Uint8Array(result));
-        })
-        .catch(function (error) {
-          reject(error);
-        });
-    }
-  });
-};
+const sha256Browser = (bytes: Uint8Array): Promise<Uint8Array> =>
+  webCrypto!.subtle
+    .digest({ name: 'SHA-256' }, bytes)
+    .then(result => new Uint8Array(result));
 
 let nodeCrypto: Promise<typeof import('crypto')> | void;
 if (typeof window === 'undefined' && !webCrypto) {
@@ -52,18 +32,7 @@ export const hash = async (query: string): Promise<string> => {
       crypto.createHash('sha256').update(query).digest('hex')
     );
   } else if (webCrypto) {
-    let buf: Uint8Array;
-    if (typeof TextEncoder !== 'undefined') {
-      buf = new TextEncoder().encode(query);
-    } else {
-      buf = new Uint8Array(query.length);
-      for (let i = 0, l = query.length; i < l; i++) {
-        // NOTE: We assume that the input GraphQL Query only uses UTF-8 at most
-        // since GraphQL mostly consists of ASCII, this is completely fine
-        buf[i] = query.charCodeAt(i);
-      }
-    }
-
+    const buf = new TextEncoder().encode(query);
     const out = await sha256Browser(buf);
 
     let hash = '';
