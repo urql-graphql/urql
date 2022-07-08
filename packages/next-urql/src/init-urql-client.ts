aa8942b96cf7a34ced42b1a291c733539cd3a55d
jdecroock <decroockjovi@gmail.com>
2022-07-08 11:01:26 +0200

    a few fixes

--- packages/next-urql/src/init-urql-client.ts
+++ packages/next-urql/src/init-urql-client.ts
@@ -4,21 +4,17 @@ export function initUrqlClient(
   clientOptions: ClientOptions,
   canEnableSuspense: boolean
 ): Client | null {
-  let urqlClient: Client | null = null;
   // Create a new Client for every server-side rendered request.
   // This ensures we reset the state for each rendered page.
   // If there is an exising client instance on the client-side, use it.
   const isServer = typeof window === 'undefined';
-  if (isServer || !urqlClient) {
-    urqlClient = createClient({
-      ...clientOptions,
-      suspense: canEnableSuspense && (isServer || clientOptions.suspense),
-    });
-    // Serialize the urqlClient to null on the client-side.
-    // This ensures we don't share client and server instances of the urqlClient.
-    (urqlClient as any).toJSON = () => null;
-  }
-
+  const urqlClient = createClient({
+    ...clientOptions,
+    suspense: canEnableSuspense && (isServer || clientOptions.suspense),
+  });
+  // Serialize the urqlClient to null on the client-side.
+  // This ensures we don't share client and server instances of the urqlClient.
+  (urqlClient as any).toJSON = () => null;
   // Return both the Client instance and the ssrCache.
   return urqlClient;
 }
