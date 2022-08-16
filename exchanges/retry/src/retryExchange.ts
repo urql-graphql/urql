b4233627d09cb22cd50329a26d9d711e0c6c9f25
Jovi De Croock <decroockjovi@gmail.com>
2022-08-16 23:31:35 +0200

    make randomDelay work

--- exchanges/retry/src/retryExchange.ts
+++ exchanges/retry/src/retryExchange.ts
@@ -44,7 +44,7 @@ export const retryExchange = ({
   const MIN_DELAY = initialDelayMs || 1000;
   const MAX_DELAY = maxDelayMs || 15000;
   const MAX_ATTEMPTS = maxNumberAttempts || 2;
-  const RANDOM_DELAY = randomDelay || true;
+  const RANDOM_DELAY = randomDelay !== undefined ? !!randomDelay : true;
 
   return ({ forward, dispatchDebug }) => ops$ => {
     const sharedOps$ = pipe(ops$, share);
