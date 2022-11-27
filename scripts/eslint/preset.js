82ce30c8b796d8200f26f1acba273e6892851a5f
Phil Pluckthun <phil@kitten.sh>
2022-11-27 14:39:50 +0000

    Fix urql-docs build and lint

--- scripts/eslint/preset.js
+++ scripts/eslint/preset.js
@@ -19,7 +19,6 @@ module.exports = {
     '@typescript-eslint/no-misused-new': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/array-type': 'off',
-    'import/no-internal-modules': 'off',
 
     '@typescript-eslint/no-unused-vars': ['error', {
       argsIgnorePattern: '^_',
