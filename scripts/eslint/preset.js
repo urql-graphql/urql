96c78263c7c1b404ef1f1355704d8d851428fce5
Phil Pluckthun <phil@kitten.sh>
2022-11-28 05:30:57 +0000

    Merge eslint presets into one file

--- scripts/eslint/preset.js
+++ scripts/eslint/preset.js
@@ -1,27 +1,124 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
+    },
+  },
+
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'dist-prod/',
+    'build/',
+    'coverage/',
+    'benchmark/',
+    'scripts/',
+    'docs/',
+  ],
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
   extends: [
-    './common.js',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/member-ordering': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-object-literal-type-assertion': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-misused-new': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/array-type': 'off',
 
-    '@typescript-eslint/no-unused-vars': ['error', {
-      argsIgnorePattern: '^_',
+  plugins: [
+    'prettier',
+    'es5',
+  ],
+
+  rules: {
+    'sort-keys': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'prefer-arrow/prefer-arrow-functions': 'off',
+
+    'es5/no-for-of': 'off',
+    'es5/no-generators': 'off',
+    'es5/no-typeof-symbol': 'warn',
+
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      arrowParens: 'avoid',
+      trailingComma: 'es5',
     }],
   },
+
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/member-ordering': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-misused-new': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/array-type': 'off',
+        'import/no-internal-modules': 'off',
+
+        '@typescript-eslint/no-unused-vars': ['error', {
+          argsIgnorePattern: '^_',
+        }],
+      },
+    },
+
+    {
+      files: ['**/*.tsx'],
+      extends: ['plugin:react/recommended'],
+      plugins: ['react-hooks'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/no-children-prop': 'off',
+      },
+    },
+
+    {
+      files: ['*.test.ts', '*.test.tsx', '*.spec.ts', '*.spec.tsx'],
+      globals: { vi: true },
+      rules: {
+        'es5/no-for-of': 'off',
+        'es5/no-generators': 'off',
+        'es5/no-typeof-symbol': 'off',
+      }
+    },
+
+    {
+      files: ['*.js'],
+      rules: {
+        'consistent-return': 'warn',
+        'no-magic-numbers': 'off',
+        'es5/no-es6-methods': 'off',
+      },
+    },
+
+    {
+      files: ['*.jsx'],
+      extends: ['plugin:react/recommended'],
+      rules: {
+        'consistent-return': 'warn',
+        'no-magic-numbers': 'off',
+        'react/jsx-key': 'off',
+        'react/jsx-handler-names': 'off',
+        'es5/no-es6-methods': 'off',
+      },
+    },
+  ],
 };
