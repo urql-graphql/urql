9dcc827b72d1775a1b311cf84e887ee331395dd8
Phil Pluckthun <phil@kitten.sh>
2022-12-01 13:18:07 +0000

    Add test for formatDocument + stringifyDocument

--- packages/core/src/utils/request.test.ts
+++ packages/core/src/utils/request.test.ts
@@ -3,80 +3,95 @@ import { expect, it, describe } from 'vitest';
 import { parse, print } from 'graphql';
 import { gql } from '../gql';
 import { createRequest, stringifyDocument } from './request';
+import { formatDocument } from './typenames';
 
-it('should hash identical queries identically', () => {
-  const reqA = createRequest('{ test }', undefined);
-  const reqB = createRequest('{ test }', undefined);
-  expect(reqA.key).toBe(reqB.key);
-});
-
-it('should hash identical DocumentNodes identically', () => {
-  const reqA = createRequest(parse('{ testB }'), undefined);
-  const reqB = createRequest(parse('{ testB }'), undefined);
-  expect(reqA.key).toBe(reqB.key);
-  expect(reqA.query).toBe(reqB.query);
-});
-
-it('should use the hash from a key if available', () => {
-  const doc = parse('{ testC }');
-  (doc as any).__key = 1234;
-  const req = createRequest(doc, undefined);
-  expect(req.key).toBe(1234);
-});
-
-it('should hash DocumentNodes and strings identically', () => {
-  const docA = parse('{ field }');
-  const docB = print(docA);
-  const reqA = createRequest(docA, undefined);
-  const reqB = createRequest(docB, undefined);
-  expect(reqA.key).toBe(reqB.key);
-  expect(reqA.query).toBe(reqB.query);
-});
-
-it('should hash graphql-tag documents correctly', () => {
-  const doc = gql`
-    {
-      testD
-    }
-  `;
-  createRequest(doc, undefined);
-  expect((doc as any).__key).not.toBe(undefined);
-});
-
-it('should return a valid query object', () => {
-  const doc = gql`
-    {
-      testE
-    }
-  `;
-  const val = createRequest(doc, undefined);
-
-  expect(val).toMatchObject({
-    key: expect.any(Number),
-    query: expect.any(Object),
-    variables: {},
+describe('createRequest', () => {
+  it('should hash identical queries identically', () => {
+    const reqA = createRequest('{ test }', undefined);
+    const reqB = createRequest('{ test }', undefined);
+    expect(reqA.key).toBe(reqB.key);
   });
-});
 
-it('should return a valid query object with variables', () => {
-  const doc = print(
-    gql`
+  it('should hash identical queries identically', () => {
+    const reqA = createRequest('{ test }', undefined);
+    const reqB = createRequest('{ test }', undefined);
+    expect(reqA.key).toBe(reqB.key);
+  });
+
+  it('should hash identical DocumentNodes identically', () => {
+    const reqA = createRequest(parse('{ testB }'), undefined);
+    const reqB = createRequest(parse('{ testB }'), undefined);
+    expect(reqA.key).toBe(reqB.key);
+    expect(reqA.query).toBe(reqB.query);
+  });
+
+  it('should use the hash from a key if available', () => {
+    const doc = parse('{ testC }');
+    (doc as any).__key = 1234;
+    const req = createRequest(doc, undefined);
+    expect(req.key).toBe(1234);
+  });
+
+  it('should hash DocumentNodes and strings identically', () => {
+    const docA = parse('{ field }');
+    const docB = print(docA);
+    const reqA = createRequest(docA, undefined);
+    const reqB = createRequest(docB, undefined);
+    expect(reqA.key).toBe(reqB.key);
+    expect(reqA.query).toBe(reqB.query);
+  });
+
+  it('should hash graphql-tag documents correctly', () => {
+    const doc = gql`
       {
-        testF
+        testD
       }
-    `
-  );
-  const val = createRequest(doc, { test: 5 });
+    `;
+    createRequest(doc, undefined);
+    expect((doc as any).__key).not.toBe(undefined);
+  });
 
-  expect(print(val.query)).toBe(doc);
-  expect(val).toMatchObject({
-    key: expect.any(Number),
-    query: expect.any(Object),
-    variables: { test: 5 },
+  it('should return a valid query object', () => {
+    const doc = gql`
+      {
+        testE
+      }
+    `;
+    const val = createRequest(doc, undefined);
+
+    expect(val).toMatchObject({
+      key: expect.any(Number),
+      query: expect.any(Object),
+      variables: {},
+    });
+  });
+
+  it('should return a valid query object with variables', () => {
+    const doc = print(
+      gql`
+        {
+          testF
+        }
+      `
+    );
+    const val = createRequest(doc, { test: 5 });
+
+    expect(print(val.query)).toBe(doc);
+    expect(val).toMatchObject({
+      key: expect.any(Number),
+      query: expect.any(Object),
+      variables: { test: 5 },
+    });
   });
 });
 
-describe('stringifyDocument (internal API)', () => {
+describe('stringifyDocument ', () => {
+  it('should reprint formatted documents', () => {
+    const doc = parse('{ test { field } }');
+    const formatted = formatDocument(doc);
+    expect(stringifyDocument(formatted)).toBe(print(formatted));
+  });
+
   it('should remove comments', () => {
     const doc = `
       { #query
