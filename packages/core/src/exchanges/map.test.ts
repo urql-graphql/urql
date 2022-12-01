03c4a5d209dbdc0089dd35993debb064ab23953c
Phil Pluckthun <phil@kitten.sh>
2022-12-01 11:41:33 +0000

    Add promise test for mapExchange and simplify

--- packages/core/src/exchanges/map.test.ts
+++ packages/core/src/exchanges/map.test.ts
@@ -1,26 +1,20 @@
-import { makeSubject, map, tap, pipe, publish, Subject } from 'wonka';
-import { vi, expect, describe, it, beforeEach } from 'vitest';
+import { map, tap, pipe, fromValue, toArray, toPromise } from 'wonka';
+import { vi, expect, describe, it } from 'vitest';
 
 import { Client } from '../client';
 import { queryOperation } from '../test-utils';
+import { Operation } from '../types';
+import { mapExchange } from './map';
+
 import {
   makeOperation,
   makeErrorResult,
   makeResult,
   CombinedError,
 } from '../utils';
-import { Operation } from '../types';
-import { mapExchange } from './map';
-
-const error = new Error('Sad times');
-let input: Subject<Operation>;
-
-beforeEach(() => {
-  input = makeSubject<Operation>();
-});
 
 describe('onOperation', () => {
-  it('triggers and maps on operations', async () => {
+  it('triggers and maps on operations', () => {
     const mockOperation = makeOperation('query', queryOperation, {
       ...queryOperation.context,
       mock: true,
@@ -28,7 +22,7 @@ describe('onOperation', () => {
 
     const onOperation = vi.fn().mockReturnValue(mockOperation);
     const onExchangeResult = vi.fn();
-    const { source: ops$, next, complete } = input;
+
     const exchangeArgs = {
       forward: op$ =>
         pipe(
@@ -39,21 +33,23 @@ describe('onOperation', () => {
       client: {} as Client,
       dispatchDebug: () => null,
     };
-    const exchange = mapExchange({ onOperation })(exchangeArgs)(ops$);
 
-    publish(exchange);
-    next(queryOperation);
-    complete();
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onOperation })(exchangeArgs),
+      toArray
+    );
+
     expect(onOperation).toBeCalledTimes(1);
     expect(onOperation).toBeCalledWith(queryOperation);
     expect(onExchangeResult).toBeCalledTimes(1);
     expect(onExchangeResult).toBeCalledWith(mockOperation);
   });
 
-  it('triggers and forwards identity when returning undefined', async () => {
+  it('triggers and forwards identity when returning undefined', () => {
     const onOperation = vi.fn().mockReturnValue(undefined);
     const onExchangeResult = vi.fn();
-    const { source: ops$, next, complete } = input;
+
     const exchangeArgs = {
       forward: op$ =>
         pipe(
@@ -64,15 +60,49 @@ describe('onOperation', () => {
       client: {} as Client,
       dispatchDebug: () => null,
     };
-    const exchange = mapExchange({ onOperation })(exchangeArgs)(ops$);
 
-    publish(exchange);
-    next(queryOperation);
-    complete();
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onOperation })(exchangeArgs),
+      toArray
+    );
+
     expect(onOperation).toBeCalledTimes(1);
     expect(onOperation).toBeCalledWith(queryOperation);
     expect(onExchangeResult).toBeCalledTimes(1);
     expect(onExchangeResult).toBeCalledWith(queryOperation);
+  });
+
+  it('awaits returned promises as needed', async () => {
+    const mockOperation = makeOperation('query', queryOperation, {
+      ...queryOperation.context,
+      mock: true,
+    });
+
+    const onOperation = vi.fn().mockResolvedValue(mockOperation);
+    const onExchangeResult = vi.fn();
+
+    const exchangeArgs = {
+      forward: op$ =>
+        pipe(
+          op$,
+          tap(onExchangeResult),
+          map((operation: Operation) => makeResult(operation, { data: null }))
+        ),
+      client: {} as Client,
+      dispatchDebug: () => null,
+    };
+
+    await pipe(
+      fromValue(queryOperation),
+      mapExchange({ onOperation })(exchangeArgs),
+      toPromise
+    );
+
+    expect(onOperation).toBeCalledTimes(1);
+    expect(onOperation).toBeCalledWith(queryOperation);
+    expect(onExchangeResult).toBeCalledTimes(1);
+    expect(onExchangeResult).toBeCalledWith(mockOperation);
   });
 });
 
@@ -87,7 +117,6 @@ describe('onResult', () => {
     const onResult = vi.fn().mockReturnValue(mockResult);
     const onExchangeResult = vi.fn();
 
-    const { source: ops$, next, complete } = input;
     const exchangeArgs = {
       forward: op$ =>
         pipe(
@@ -98,11 +127,13 @@ describe('onResult', () => {
       dispatchDebug: () => null,
     };
 
-    const exchange = mapExchange({ onResult })(exchangeArgs)(ops$);
-    pipe(exchange, tap(onExchangeResult), publish);
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onResult })(exchangeArgs),
+      tap(onExchangeResult),
+      toArray
+    );
 
-    next(queryOperation);
-    complete();
     expect(onResult).toBeCalledTimes(1);
     expect(onResult).toBeCalledWith(makeResult(queryOperation, { data: null }));
     expect(onExchangeResult).toBeCalledTimes(1);
@@ -110,35 +141,71 @@ describe('onResult', () => {
   });
 
   it('triggers and forwards identity when returning undefined', async () => {
-    const onOperation = vi.fn().mockReturnValue(undefined);
+    const onResult = vi.fn().mockReturnValue(undefined);
     const onExchangeResult = vi.fn();
-    const { source: ops$, next, complete } = input;
+
     const exchangeArgs = {
       forward: op$ =>
         pipe(
           op$,
-          tap(onExchangeResult),
           map((operation: Operation) => makeResult(operation, { data: null }))
         ),
       client: {} as Client,
       dispatchDebug: () => null,
     };
-    const exchange = mapExchange({ onOperation })(exchangeArgs)(ops$);
 
-    publish(exchange);
-    next(queryOperation);
-    complete();
-    expect(onOperation).toBeCalledTimes(1);
-    expect(onOperation).toBeCalledWith(queryOperation);
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onResult })(exchangeArgs),
+      tap(onExchangeResult),
+      toArray
+    );
+
+    const result = makeResult(queryOperation, { data: null });
+    expect(onResult).toBeCalledTimes(1);
+    expect(onResult).toBeCalledWith(result);
     expect(onExchangeResult).toBeCalledTimes(1);
-    expect(onExchangeResult).toBeCalledWith(queryOperation);
+    expect(onExchangeResult).toBeCalledWith(result);
+  });
+
+  it('awaits returned promises as needed', async () => {
+    const mockOperation = makeOperation('query', queryOperation, {
+      ...queryOperation.context,
+      mock: true,
+    });
+
+    const mockResult = makeErrorResult(mockOperation, new Error('Mock'));
+    const onResult = vi.fn().mockResolvedValue(mockResult);
+    const onExchangeResult = vi.fn();
+
+    const exchangeArgs = {
+      forward: op$ =>
+        pipe(
+          op$,
+          map((operation: Operation) => makeResult(operation, { data: null }))
+        ),
+      client: {} as Client,
+      dispatchDebug: () => null,
+    };
+
+    await pipe(
+      fromValue(queryOperation),
+      mapExchange({ onResult })(exchangeArgs),
+      tap(onExchangeResult),
+      toPromise
+    );
+
+    expect(onResult).toBeCalledTimes(1);
+    expect(onResult).toBeCalledWith(makeResult(queryOperation, { data: null }));
+    expect(onExchangeResult).toBeCalledTimes(1);
+    expect(onExchangeResult).toBeCalledWith(mockResult);
   });
 });
 
 describe('onError', () => {
   it('does not trigger when there are no errors', async () => {
     const onError = vi.fn();
-    const { source: ops$, next, complete } = input;
+
     const exchangeArgs = {
       forward: op$ =>
         pipe(
@@ -148,17 +215,20 @@ describe('onError', () => {
       client: {} as Client,
       dispatchDebug: () => null,
     };
-    const exchange = mapExchange({ onError })(exchangeArgs)(ops$);
 
-    publish(exchange);
-    next(queryOperation);
-    complete();
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onError })(exchangeArgs),
+      toArray
+    );
+
     expect(onError).toBeCalledTimes(0);
   });
 
   it('triggers correctly when the operations has an error', async () => {
     const onError = vi.fn();
-    const { source: ops$, next, complete } = input;
+    const error = new Error('Sad times');
+
     const exchangeArgs = {
       forward: op$ =>
         pipe(
@@ -168,75 +238,17 @@ describe('onError', () => {
       client: {} as Client,
       dispatchDebug: () => null,
     };
-    const exchange = mapExchange({ onError })(exchangeArgs)(ops$);
 
-    publish(exchange);
-    next(queryOperation);
-    complete();
+    pipe(
+      fromValue(queryOperation),
+      mapExchange({ onError })(exchangeArgs),
+      toArray
+    );
+
     expect(onError).toBeCalledTimes(1);
     expect(onError).toBeCalledWith(
       new CombinedError({ networkError: error }),
       queryOperation
-    );
-  });
-
-  it('triggers correctly multiple times the operations has an error', async () => {
-    const onError = vi.fn();
-    const { source: ops$, next, complete } = input;
-
-    const firstQuery = {
-      ...queryOperation,
-      context: {
-        ...queryOperation.context,
-        item: 1,
-      },
-    };
-
-    const secondQuery = {
-      ...queryOperation,
-      context: {
-        ...queryOperation.context,
-        item: 2,
-      },
-    };
-
-    const thirdQuery = {
-      ...queryOperation,
-      context: {
-        ...queryOperation.context,
-        item: 3,
-      },
-    };
-
-    const exchangeArgs = {
-      forward: op$ =>
-        pipe(
-          op$,
-          map((operation: Operation) => {
-            if (operation.context.item === 2) {
-              return { operation };
-            }
-            return makeErrorResult(operation, error);
-          })
-        ),
-      client: {} as Client,
-      dispatchDebug: () => null,
-    };
-    const exchange = mapExchange({ onError })(exchangeArgs)(ops$);
-
-    publish(exchange);
-    next(firstQuery);
-    next(secondQuery);
-    next(thirdQuery);
-    complete();
-    expect(onError).toBeCalledTimes(2);
-    expect(onError).toBeCalledWith(
-      new CombinedError({ networkError: error }),
-      firstQuery
-    );
-    expect(onError).toBeCalledWith(
-      new CombinedError({ networkError: error }),
-      thirdQuery
     );
   });
 });
