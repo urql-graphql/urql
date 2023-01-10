d667e36cad410d0ed235b369da35ce74ebc3827e
Jovi De Croock <decroockjovi@gmail.com>
2023-01-10 17:11:29 +0100

    refactor(populate): fields-based populate (#2897)

--- exchanges/populate/src/helpers/node.ts
+++ exchanges/populate/src/helpers/node.ts
@@ -1,24 +1,15 @@
 import {
   NameNode,
-  SelectionNode,
-  SelectionSetNode,
   GraphQLOutputType,
   isWrappingType,
   GraphQLWrappingType,
   Kind,
 } from 'graphql';
 
-export type SelectionSet = ReadonlyArray<SelectionNode>;
 export type GraphQLFlatType = Exclude<GraphQLOutputType, GraphQLWrappingType>;
 
 /** Returns the name of a given node */
 export const getName = (node: { name: NameNode }): string => node.name.value;
-
-/** Returns the SelectionSet for a given inline or defined fragment node */
-export const getSelectionSet = (node: {
-  selectionSet?: SelectionSetNode;
-}): SelectionSet =>
-  node.selectionSet !== undefined ? node.selectionSet.selections : [];
 
 export const unwrapType = (
   type: null | undefined | GraphQLOutputType
