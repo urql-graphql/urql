cc33df9d653d5288ad8e83c2f93da538c88d49a9
Phil Pluckthun <phil@kitten.sh>
2023-03-07 03:00:17 +0000

    Add initial TSDocs

--- exchanges/auth/src/authExchange.ts
+++ exchanges/auth/src/authExchange.ts
@@ -25,8 +25,24 @@ import {
 
 import { DocumentNode } from 'graphql';
 
+/** Utilities to use while refreshing authentication tokens. */
 export interface AuthUtilities {
-  /** The mutate() method may be used to send one-off mutations to the GraphQL API for the purpose of authentication. */
+  /** Sends a mutation to your GraphQL API, bypassing earlier exchanges and authentication.
+   *
+   * @param query - a GraphQL document containing the mutation operation that will be executed.
+   * @param variables - the variables used to execute the operation.
+   * @param context - {@link OperationContext} options that'll be used in future exchanges.
+   * @returns A `Promise` of an {@link OperationResult} for the GraphQL mutation.
+   *
+   * @remarks
+   * The `mutation()` utility method is useful when your authentication requires you to make a GraphQL mutation
+   * request to update your authentication tokens. In these cases, you likely wish to bypass prior exchanges and
+   * the authentication in the `authExchange` itself.
+   *
+   * This method bypasses the usual mutation flow of the `Client` and instead issues the mutation as directly
+   * as possible. This also means that it doesn’t carry your `Client`'s default {@link OperationContext}
+   * options, so you may have to pass them again, if needed.
+   */
   mutate<Data = any, Variables extends AnyVariables = AnyVariables>(
     query: DocumentNode | TypedDocumentNode<Data, Variables> | string,
     variables: Variables,
@@ -34,17 +50,76 @@ export interface AuthUtilities {
   ): Promise<OperationResult<Data>>;
 }
 
+/** Configuration for the `authExchange` returned by the initializer function you write. */
 export interface AuthConfig {
-  /** addAuthToOperation() must be provided to add the custom `authState` to an Operation's context, so that it may be picked up by the `fetchExchange`. */
+  /** Called for every operation to add authentication data to your operation.
+   *
+   * @param operation - An {@link Operation} that needs authentication tokens added.
+   * @returns a new {@link Operation} with added authentication tokens.
+   *
+   * @remarks
+   * The {@link authExchange} will call this function you provide and expects that you
+   * add your authentication tokens to your operation here, on the {@link Operation}
+   * that is returned.
+   *
+   * Hint: You likely want to modify your `fetchOptions.headers` here, for instance to
+   * add an `Authorization` header.
+   */
   addAuthToOperation(operation: Operation): Operation;
 
-  /** didAuthError() may be provided to tweak the detection of an authentication error that this exchange should handle. */
-  didAuthError?(error: CombinedError, operation: Operation): boolean;
-
-  /** willAuthError() may be provided to detect a potential operation that'll receive authentication error so that getAuth() can be run proactively. */
+  /** Called before an operation is forwaded onwards to make a request.
+   *
+   * @param operation - An {@link Operation} that needs authentication tokens added.
+   * @returns a boolean, if true, authentication must be refreshed.
+   *
+   * @remarks
+   * The {@link authExchange} will call this function before an {@link Operation} is
+   * forwarded onwards to your following exchanges.
+   *
+   * When this function returns `true`, the `authExchange` will call
+   * {@link AuthConfig.refreshAuth} before forwarding more operations
+   * to prompt you to update your authentication tokens.
+   *
+   * Hint: If you define this function, you can use it to check whether your authentication
+   * tokens have expired.
+   */
   willAuthError?(operation: Operation): boolean;
 
-  /** getAuth() handles how the application refreshes or reauthenticates given a stale `authState` and should return a new `authState` or `null`. */
+  /** Called after receiving an operation result to check whether it has failed with an authentication error.
+   *
+   * @param error - A {@link CombinedError} that a result has come back with.
+   * @param operation - The {@link Operation} of that has failed.
+   * @returns a boolean, if true, authentication must be refreshed.
+   *
+   * @remarks
+   * The {@link authExchange} will call this function if it sees an {@link OperationResult}
+   * with a {@link CombinedError} on it, implying that it may have failed due to an authentication
+   * error.
+   *
+   * When this function returns `true`, the `authExchange` will call
+   * {@link AuthConfig.refreshAuth} before forwarding more operations
+   * to prompt you to update your authentication tokens.
+   * Afterwards, this operation will be retried once.
+   *
+   * Hint: You should define a function that detects your API’s authentication
+   * errors, e.g. using `result.extensions`.
+   */
+  didAuthError(error: CombinedError, operation: Operation): boolean;
+
+  /** Called to refresh the authentication state.
+   *
+   * @remarks
+   * The {@link authExchange} will call this function if either {@link AuthConfig.willAuthError}
+   * or {@link AuthConfig.didAuthError} have returned `true` prior, which indicates that the
+   * authentication state you hold has expired or is out-of-date.
+   *
+   * When this function is called, you should refresh your authentication state.
+   * For instance, if you have a refresh token and an access token, you should rotate
+   * these tokens with your API by sending the refresh token.
+   *
+   * Hint: You can use the {@link fetch} API here, or use {@link AuthUtilities.mutate}
+   * if your API requires a GraphQL mutation to refresh your authentication state.
+   */
   refreshAuth(): Promise<void>;
 }
 
@@ -57,6 +132,28 @@ const addAuthAttemptToOperation = (
     authAttempt,
   });
 
+/** Creates an `Exchange` handling control flow for authentication.
+ *
+ * @param init - An initializer function that returns an {@link AuthConfig} wrapped in a `Promise`.
+ * @returns the created authentication {@link Exchange}.
+ *
+ * @remarks
+ * The `authExchange` is used to create an exchange handling authentication and
+ * the control flow of refresh authentication.
+ *
+ * You must pass an initializer function, which receives {@link AuthUtilities} and
+ * must return an {@link AuthConfig} wrapped in a `Promise`.
+ * When this exchange is used in your `Client`, it will first call your initializer
+ * function, which gives you an opportunity to get your authentication state, e.g.
+ * from local storage.
+ *
+ * You may then choose to validate this authentication state and update it, and must
+ * then return an {@link AuthConfig}.
+ *
+ * This configuration defines how you add authentication state to {@link Operation | Operations},
+ * when your authentication state expires, when an {@link OperationResult} has errored
+ * with an authentication error, and how to refresh your authentication state.
+ */
 export function authExchange(
   init: (utilities: AuthUtilities) => Promise<AuthConfig>
 ): Exchange {
