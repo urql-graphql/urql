655ee42b7ddfbaca98aaf77c77674193a825126b
Phil Pluckthun <phil@kitten.sh>
2023-03-11 00:14:26 +0000

    Update fallback exchange type condition and development branch

--- packages/core/src/exchanges/fallback.ts
+++ packages/core/src/exchanges/fallback.ts
@@ -1,5 +1,5 @@
 import { filter, pipe, tap } from 'wonka';
-import { Operation, ExchangeIO, ExchangeInput } from '../types';
+import { ExchangeIO, ExchangeInput } from '../types';
 
 /** Used by the `Client` as the last exchange to warn about unhandled operations.
  *
@@ -14,24 +14,28 @@ export const fallbackExchange: ({
   dispatchDebug,
 }: Pick<ExchangeInput, 'dispatchDebug'>) => ExchangeIO = ({
   dispatchDebug,
-}) => ops$ =>
-  pipe(
-    ops$,
-    tap<Operation>(operation => {
-      if (
-        operation.kind !== 'teardown' &&
-        process.env.NODE_ENV !== 'production'
-      ) {
-        const message = `No exchange has handled operations of kind "${operation.kind}". Check whether you've added an exchange responsible for these operations.`;
+}) => ops$ => {
+  if (process.env.NODE_ENV !== 'production') {
+    ops$ = pipe(
+      ops$,
+      tap(operation => {
+        if (
+          operation.kind !== 'teardown' &&
+          process.env.NODE_ENV !== 'production'
+        ) {
+          const message = `No exchange has handled operations of kind "${operation.kind}". Check whether you've added an exchange responsible for these operations.`;
 
-        dispatchDebug({
-          type: 'fallbackCatch',
-          message,
-          operation,
-        });
-        console.warn(message);
-      }
-    }),
-    // All operations that skipped through the entire exchange chain should be filtered from the output
-    filter<any>(() => false)
-  );
+          dispatchDebug({
+            type: 'fallbackCatch',
+            message,
+            operation,
+          });
+          console.warn(message);
+        }
+      })
+    );
+  }
+
+  // All operations that skipped through the entire exchange chain should be filtered from the output
+  return filter((_x): _x is never => false)(ops$);
+};
