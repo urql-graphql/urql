b6e0c0432f4209bc1f4cd6dcafe47174a522b547
Phil Pluckthun <phil@kitten.sh>
2023-03-17 01:41:51 +0000

    Fix examples/with-next

--- examples/with-next/pages/_app.js
+++ examples/with-next/pages/_app.js
@@ -4,9 +4,9 @@ import { cacheExchange, fetchExchange } from 'urql';
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default withUrqlClient(
-  () => ({
+  ssrExchange => ({
     url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
-    exchanges: [cacheExchange, fetchExchange],
+    exchanges: [cacheExchange, ssrExchange, fetchExchange],
   }),
   { ssr: false }
 )(App);
