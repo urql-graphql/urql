a14a8dc591798bc17b5d428497ad369eff07f169
Phil Pluckthun <phil@kitten.sh>
2023-03-17 05:11:17 +0000

    Add TSDocs for Graphcache types (except resolvers and Cache)

--- exchanges/graphcache/src/types.ts
+++ exchanges/graphcache/src/types.ts
@@ -2,77 +2,260 @@ import { AnyVariables, TypedDocumentNode } from '@urql/core';
 import { GraphQLError, DocumentNode, FragmentDefinitionNode } from 'graphql';
 import { IntrospectionData } from './ast';
 
-// Helper types
+/** Nullable GraphQL list types of `T`.
+ *
+ * @remarks
+ * Any GraphQL list of a given type `T` that is nullable is
+ * expected to contain nullable values. Nested lists are
+ * also taken into account in Graphcache.
+ */
 export type NullArray<T> = Array<null | T | NullArray<T>>;
 
+/** Dictionary of GraphQL Fragment definitions by their names.
+ *
+ * @remarks
+ * A map of {@link FragmentDefinitionNode | FragmentDefinitionNodes} by their
+ * fragment names from the original GraphQL document that Graphcache is
+ * executing.
+ */
 export interface Fragments {
   [fragmentName: string]: void | FragmentDefinitionNode;
 }
 
-// Scalar types are not entities as part of response data
+/** Non-object JSON values as serialized by a GraphQL API
+ * @see {@link https://spec.graphql.org/October2021/#sel-DAPJDHAAEJHAKmzP} for the
+ * GraphQL spec’s serialization format.
+ */
 export type Primitive = null | number | boolean | string;
 
+/** Any GraphQL scalar object
+ *
+ * @remarks
+ * A GraphQL schema may define custom scalars that are resolved
+ * and serialized as objects. These objects could also be turned
+ * on the client-side into a non-JSON object, e.g. a `Date`.
+ *
+ * @see {@link https://spec.graphql.org/October2021/#sec-Scalars} for the
+ * GraphQL spec’s information on custom scalars.
+ */
 export interface ScalarObject {
   constructor?: Function;
   [key: string]: any;
 }
 
+/** GraphQL scalar value
+ * @see {@link https://spec.graphql.org/October2021/#sec-Scalars} for the GraphQL
+ * spec’s definition of scalars
+ */
 export type Scalar = Primitive | ScalarObject;
 
+/** Fields that Graphcache expects on GraphQL object (“entity”) results.
+ *
+ * @remarks
+ * Any object that comes back from a GraphQL API will have
+ * a `__typename` field from GraphQL Object types.
+ *
+ * The `__typename` field must be present as Graphcache updates
+ * GraphQL queries with type name introspection.
+ * Furthermore, Graphcache always checks for its default key
+ * fields, `id` and `_id` to be present.
+ */
 export interface SystemFields {
+  /** GraphQL Object type name as returned by Type Name Introspection.
+   * @see {@link https://spec.graphql.org/October2021/#sec-Type-Name-Introspection} for
+   * more information on GraphQL’s Type Name introspection.
+   */
   __typename: string;
   _id?: string | number | null;
   id?: string | number | null;
 }
 
+/** Scalar values are stored separately from relations between entities.
+ * @internal
+ */
 export type EntityField = undefined | Scalar | NullArray<Scalar>;
+
+/** Values on GraphQL object (“entity”) results.
+ *
+ * @remarks
+ * Any field that comes back from a GraphQL API will have
+ * values that are scalars, other objects, or arrays
+ * of scalars or objects.
+ */
 export type DataField = Scalar | Data | NullArray<Scalar> | NullArray<Data>;
 
+/** Definition of GraphQL object (“entity”) fields.
+ *
+ * @remarks
+ * Any object that comes back from a GraphQL API will have
+ * values that are scalars, other objects, or arrays
+ * of scalars or objects, i.e. the {@link DataField} type.
+ */
 export interface DataFields {
   [fieldName: string]: DataField;
 }
 
+/** Definition of GraphQL variables objects.
+ * @remarks
+ * Variables, as passed to GraphQL queries, can only contain scalar values.
+ *
+ * @see {@link https://spec.graphql.org/October2021/#sec-Coercing-Variable-Values} for the
+ * GraphQL spec’s coercion of GraphQL variables.
+ */
 export interface Variables {
   [name: string]: Scalar | Scalar[] | Variables | NullArray<Variables>;
 }
 
+/** Definition of GraphQL objects (“entities”).
+ *
+ * @remarks
+ * An entity is expected to consist of a `__typename`
+ * fields, optionally the default `id` or `_id` key
+ * fields, and scalar values or other entities
+ * otherwise.
+ */
 export type Data = SystemFields & DataFields;
+
+/** An entity, a key of an entity, or `null`
+ *
+ * @remarks
+ * When Graphcache accepts a reference to an entity, you may pass it a key of an entity,
+ * as retrieved for instance by {@link Cache.keyOfEntity} or a partial GraphQL object
+ * (i.e. an object with a `__typename` and key field).
+ */
 export type Entity = null | Data | string;
+
+/** A key of an entity, or `null`; or a list of keys.
+ *
+ * @remarks
+ * When Graphcache accepts a reference to one or more entities, you may pass it a
+ * key, an entity, or a list of entities or keys. This is often passed to {@link Cache.link}
+ * to update a field pointing to other GraphQL objects.
+ */
 export type Link<Key = string> = null | Key | NullArray<Key>;
-export type Connection = [Variables, string];
+
+/** Arguments passed to a Graphcache field resolver.
+ *
+ * @remarks
+ * Arguments a field receives are similar to variables and can
+ * only contain scalars or other arguments objects. This
+ * is equivalent to the {@link Variables} type.
+ *
+ * @see {@link https://spec.graphql.org/October2021/#sec-Coercing-Field-Arguments} for the
+ * GraphQL spec’s coercion of field arguments.
+ */
 export type FieldArgs = Variables | null | undefined;
 
+/** Metadata about an entity’s cached field.
+ *
+ * @remarks
+ * As returned by {@link Cache.inspectFields}, `FieldInfo` specifies an entity’s cached field,
+ * split into the field’s key itself and the field’s original name and arguments.
+ */
 export interface FieldInfo {
+  /** The field’s key which combines `fieldName` and `arguments`. */
   fieldKey: string;
+  /** The field’s name, as defined on a GraphQL Object type. */
   fieldName: string;
+  /** The arguments passed to the field as found on the cache. */
   arguments: Variables | null;
 }
 
+/** A key to an entity field split back into the entity’s key and the field’s key part.
+ * @internal
+ */
 export interface KeyInfo {
   entityKey: string;
   fieldKey: string;
 }
 
-// This is an input operation
+/** Abstract type for GraphQL requests.
+ *
+ * @remarks
+ * Similarly to `@urql/core`’s `GraphQLRequest` type, `OperationRequest`
+ * requires the minimum fields that Grapcache requires to execute a
+ * GraphQL operation: its query document and variables.
+ */
 export interface OperationRequest {
   query: DocumentNode | TypedDocumentNode<any, any>;
   variables?: any;
 }
 
+/** Metadata object passed to all resolver functions.
+ *
+ * @remarks
+ * `ResolveInfo`, similar to GraphQL.js’ `GraphQLResolveInfo` object,
+ * gives your resolvers a global state of the current GraphQL
+ * document traversal.
+ *
+ * `parent`, `parenTypeName`, `parentKey`, and `parentFieldKey`
+ * are particularly useful to make reusable resolver functions that
+ * must know on which field and type they’re being called on.
+ */
 export interface ResolveInfo {
+  /** The parent GraphQL object.
+   *
+   * @remarks
+   * The GraphQL object that the resolver has been called on. Because this is
+   * a reference to raw GraphQL data, this may be incomplete or contain
+   * aliased fields!
+   */
   parent: Data;
+  /** The parent object’s typename that the resolver has been called on. */
   parentTypeName: string;
+  /** The parent object’s entity key that the resolver has been called on. */
   parentKey: string;
+  /** Current field’s key that the resolver has been called on. */
   parentFieldKey: string;
+  /** Current field that the resolver has been called on. */
   fieldName: string;
+  /** Map of fragment definitions from the {@link DocumentNode}. */
   fragments: Fragments;
+  /** Full original {@link Variables} object on the {@link OperationRequest}. */
   variables: Variables;
+  /** Error that occurred for the current field, if any.
+   *
+   * @remarks
+   * If a {@link GraphQLError.path} points at the current field, the error
+   * will be set and provided here. This can be useful to recover from an
+   * error on a specific field.
+   */
   error: GraphQLError | undefined;
+  /** Flag used to indicate whether the current GraphQL query is only partially cached.
+   *
+   * @remarks
+   * When Graphcache has {@link CacheExchangeOpts.schema} introspection information,
+   * it can automatically generate partial results and trigger a full API request
+   * in the background.
+   * Hence, this field indicates whether any data so far has only been partially
+   * resolved from the cache, and is only in use on {@link Resolver | Resolvers}.
+   *
+   * However, you can also flip this flag to `true` manually to indicate to
+   * the {@link cacheExchange} that it should still make a network request.
+   */
   partial?: boolean;
+  /** Flag used to indicate whether the current GraphQL mutation is optimistically executed.
+   *
+   * @remarks
+   * An {@link UpdateResolver} is called for both API mutation responses and
+   * optimistic mutation reuslts, as generated by {@link OptimisticMutationResolver}.
+   *
+   * Since an update sometimes needs to perform different actions if it’s run
+   * optimistically, this flag is set to `true` during optimisti cupdates.
+   */
   optimistic?: boolean;
+  /** Internal state used by Graphcache.
+   * @internal
+   */
   __internal?: unknown;
 }
 
+/** GraphQL document and variables that should be queried against the cache.
+ *
+ * @remarks
+ * `QueryInput` is a generic GraphQL request that should be executed against
+ * cached data, as accepted by {@link cache.readQuery}.
+ */
 export interface QueryInput<T = Data, V = Variables> {
   query: string | DocumentNode | TypedDocumentNode<T, V>;
   variables?: V;
@@ -131,7 +314,15 @@ export interface Cache {
   link(entity: Entity, field: string, value: Link<Entity>): void;
 }
 
-type ResolverResult =
+/** Values a {@link Resolver} may return.
+ *
+ * @remarks
+ * A resolver may return any value that a GraphQL object may contain.
+ *
+ * Additionally however, a resolver may return `undefined` to indicate that data
+ * isn’t available from the cache, i.e. to trigger a cache miss.
+ */
+export type ResolverResult =
   | DataField
   | (DataFields & { __typename?: string })
   | null
@@ -293,6 +484,16 @@ export type UpdatesConfig = {
   } | void;
 };
 
+/** Remaps result type to allow for nested optimistic mutation resolvers.
+ *
+ * @remarks
+ * An {@link OptimisticMutationResolver} can not only return partial, nested
+ * mutation result data, but may also contain more optimistic mutation resolvers
+ * for nested fields, which allows fields with arguments to optimistically be
+ * resolved to dynamic values.
+ *
+ * @see {@link OptimisticMutationConfig} for more information.
+ */
 export type MakeFunctional<T> = T extends { __typename: string }
   ? WithTypename<{
       [P in keyof T]?: MakeFunctional<T[P]>;
@@ -420,11 +621,19 @@ export interface StorageAdapter {
   onOnline?(cb: () => void): any;
 }
 
+/** Set of keys that have been modified or accessed.
+ * @internal
+ */
 export type Dependencies = Set<string>;
 
-/** The type of cache operation being executed. */
+/** The type of cache operation being executed.
+ * @internal
+ */
 export type OperationType = 'read' | 'write';
 
+/** Casts a given object type to have a required typename field.
+ * @internal
+ */
 export type WithTypename<T extends { __typename?: any }> = T & {
   __typename: NonNullable<T['__typename']>;
 };
