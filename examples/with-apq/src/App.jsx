65d6b91404bfa88e6e0f0bdb7d4e680797c234f3
Phil Pluckthun <phil@kitten.sh>
2023-03-17 00:55:00 +0000

    Update examples/with-apq to use @urql/exchange-persisted

--- examples/with-apq/src/App.jsx
+++ examples/with-apq/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { createClient, Provider, fetchExchange } from 'urql';
-import { persistedFetchExchange } from '@urql/exchange-persisted-fetch';
+import { Client, Provider, fetchExchange } from 'urql';
+import { persistedExchange } from '@urql/exchange-persisted';
 
 import LocationsList from './LocationsList';
 
-const client = createClient({
+const client = new Client({
   url: 'https://trygql.formidable.dev/graphql/apq-weather',
   exchanges: [
-    persistedFetchExchange({
+    persistedExchange({
       preferGetForPersistedQueries: true,
     }),
     fetchExchange,
