2b64bdbd5822b3c3a60c79a75a946fca0376a4a5
Phil Pluckthun <phil@kitten.sh>
2023-03-17 01:16:55 +0000

    Update client-creation and dependencies in examples/with-react-native

--- examples/with-react-native/App.js
+++ examples/with-react-native/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { createClient, Provider } from 'urql';
+import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 
 import PokemonList from './src/screens/PokemonList';
 
-const client = createClient({
+const client = new Client({
   url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+  exchanges: [cacheExchange, fetchExchange],
 });
 
 const App = () => {
