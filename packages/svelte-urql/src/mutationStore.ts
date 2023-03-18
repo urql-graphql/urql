f6b39ab41961eb70c195c036d9ed8dc9cc80d600
Phil Pluckthun <phil@kitten.sh>
2023-03-18 16:18:50 +0000

    fix(svelte): Move handler from mutationStore to subscriptionStore (#3078)

--- packages/svelte-urql/src/mutationStore.ts
+++ packages/svelte-urql/src/mutationStore.ts
@@ -15,8 +15,6 @@ import {
   initialResult,
 } from './common';
 
-export type SubscriptionHandler<T, R> = (prev: R | undefined, data: T) => R;
-
 export type MutationArgs<
   Data = any,
   Variables extends AnyVariables = AnyVariables
@@ -27,19 +25,15 @@ export type MutationArgs<
 
 export function mutationStore<
   Data = any,
-  Result = Data,
   Variables extends AnyVariables = AnyVariables
->(
-  args: MutationArgs<Data, Variables>,
-  handler?: SubscriptionHandler<Data, Result>
-): OperationResultStore<Result, Variables> {
+>(args: MutationArgs<Data, Variables>): OperationResultStore<Data, Variables> {
   const request = createRequest(args.query, args.variables as Variables);
   const operation = args.client.createRequestOperation(
     'mutation',
     request,
     args.context
   );
-  const initialState: OperationResultState<Result, Variables> = {
+  const initialState: OperationResultState<Data, Variables> = {
     ...initialResult,
     operation,
     fetching: true,
@@ -58,16 +52,13 @@ export function mutationStore<
         extensions,
       }))
     ),
-    scan((result: OperationResultState<Result, Variables>, partial: any) => {
-      // If a handler has been passed, it's used to merge new data in
-      const data =
-        partial.data !== undefined
-          ? typeof handler === 'function'
-            ? handler(result.data, partial.data)
-            : partial.data
-          : result.data;
-      return { ...result, ...partial, data };
-    }, initialState),
+    scan(
+      (result: OperationResultState<Data, Variables>, partial) => ({
+        ...result,
+        ...partial,
+      }),
+      initialState
+    ),
     subscribe(result => {
       result$.set(result);
     })
