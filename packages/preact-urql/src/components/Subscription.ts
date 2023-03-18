33ded998dcc238b87be6cef29b9b528bb00db30b
Phil Pluckthun <phil@kitten.sh>
2023-03-18 15:28:56 +0000

    Add TSDocs to preact-urql/react-urql’s component wrappers

--- packages/preact-urql/src/components/Subscription.ts
+++ packages/preact-urql/src/components/Subscription.ts
@@ -1,29 +1,58 @@
 import { VNode } from 'preact';
-import { AnyVariables, OperationContext } from '@urql/core';
+import { AnyVariables } from '@urql/core';
 
 import {
   useSubscription,
   UseSubscriptionArgs,
   UseSubscriptionState,
+  UseSubscriptionExecute,
   SubscriptionHandler,
 } from '../hooks';
 
+/** Props accepted by {@link Subscription}.
+ *
+ * @remarks
+ * `SubscriptionProps` are the props accepted by the {@link Subscription} component,
+ * which is identical to {@link UseSubscriptionArgs} with an added
+ * {@link SubscriptionProps.handler} prop, which {@link useSubscription} usually
+ * accepts as an additional argument.
+ *
+ * The result, the {@link SubscriptionState} object, will be passed to
+ * a {@link SubscriptionProps.children} function, passed as children
+ * to the `Subscription` component.
+ */
 export type SubscriptionProps<
   Data = any,
   Result = Data,
   Variables extends AnyVariables = AnyVariables
 > = UseSubscriptionArgs<Variables, Data> & {
+  /** Accepts the {@link SubscriptionHandler} as a prop. */
   handler?: SubscriptionHandler<Data, Result>;
-  children: (arg: SubscriptionState<Result, Variables>) => VNode<any>;
+  children(arg: SubscriptionState<Result, Variables>): VNode<any>;
 };
 
+/** Object that {@link SubscriptionProps.children} is called with.
+ *
+ * @remarks
+ * This is an extented {@link UseSubscriptionState} with an added
+ * {@link SubscriptionState.executeSubscription} method, which is usually
+ * part of a tuple returned by {@link useSubscription}.
+ */
 export interface SubscriptionState<
   Data = any,
   Variables extends AnyVariables = AnyVariables
 > extends UseSubscriptionState<Data, Variables> {
-  executeSubscription: (opts?: Partial<OperationContext>) => void;
+  /** Alias to {@link useSubscription}’s `executeMutation` function. */
+  executeSubscription: UseSubscriptionExecute;
 }
 
+/** Component Wrapper around {@link useSubscription} to run a GraphQL subscription.
+ *
+ * @remarks
+ * `Subscription` is a component wrapper around the {@link useSubscription} hook
+ * that calls the {@link SubscriptionProps.children} prop, as a function,
+ * with the {@link SubscriptionState} object.
+ */
 export function Subscription<
   Data = any,
   Result = Data,
