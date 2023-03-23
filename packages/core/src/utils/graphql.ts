67e28c877bbc91538ba46a526c7b272f40220c8e
Phil Pluckthun <phil@kitten.sh>
2023-03-23 19:07:40 +0000

    Add graphql shim types

---
+++ packages/core/src/utils/graphql.ts
@@ -0,0 +1,22 @@
+import type * as GraphQLWeb from '@0no-co/graphql.web';
+import type * as GraphQL from 'graphql';
+
+type OrNever<T> = 0 extends 1 & T ? never : T;
+
+export type FieldNode = GraphQLWeb.FieldNode | OrNever<GraphQL.FieldNode>;
+
+export type InlineFragmentNode =
+  | GraphQLWeb.InlineFragmentNode
+  | OrNever<GraphQL.InlineFragmentNode>;
+
+export type GraphQLError =
+  | GraphQLWeb.GraphQLError
+  | OrNever<GraphQL.GraphQLError>;
+
+export type DocumentNode =
+  | GraphQLWeb.DocumentNode
+  | OrNever<GraphQL.DocumentNode>;
+
+export type DefinitionNode =
+  | GraphQLWeb.DefinitionNode
+  | OrNever<GraphQL.DefinitionNode>;
