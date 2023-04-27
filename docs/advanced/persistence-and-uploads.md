82ff504c356d35eead18686d4ef05132475365eb
Nick Richmond <nick@getnacelle.com>
2023-04-27 18:57:16 -0400

    fix: persisted queries can use GETs within URL limits

--- docs/advanced/persistence-and-uploads.md
+++ docs/advanced/persistence-and-uploads.md
@@ -61,10 +61,7 @@ const client = new Client({
 });
 ```
 
-As we can see, typically it's recommended to set `preferGetForPersistedQueries` to `true` to force
-all persisted queries to use GET requests instead of POST so that CDNs can do their job.
-It does so by setting the `preferGetMethod` option to `'force'` when it's
-updating operations.
+As we can see, typically it's recommended to set `preferGetForPersistedQueries` to `true` to encourage persisted queries to use GET requests instead of POST so that CDNs can do their job. When set to `true` or `'within-url-limit'`, persisted queries will use GET requests if the resulting URL doesn't exceed the 2048 character limit.
 
 The `fetchExchange` can see the modifications that the `persistedExchange` is
 making to operations, and understands to leave out the `query` from any request
--- exchanges/persisted/src/persistedExchange.ts
+++ exchanges/persisted/src/persistedExchange.ts
@@ -17,6 +17,7 @@ import {
   CombinedError,
   Exchange,
   Operation,
+  OperationContext,
 } from '@urql/core';
 
 import { hash } from './sha256';
@@ -29,19 +30,23 @@ const isPersistedUnsupported = (error: CombinedError): boolean =>
 
 /** Input parameters for the {@link persistedExchange}. */
 export interface PersistedExchangeOptions {
-  /** Enforces GET method requests to be made for Persisted Queries.
+  /** Controls whether GET method requests will be made for Persisted Queries.
    *
    * @remarks
-   * When enabled, the `persistedExchange` will set
+   * When set to `true` or `'within-url-limit'`, the `persistedExchange`
+   * will use GET requests on persisted queries when the request URL
+   * doesn't exceed the 2048 character limit.
+   *
+   * When set to `force`, the `persistedExchange` will set
    * `OperationContext.preferGetMethod` to `'force'` on persisted queries,
    * which will force requests to be made using a GET request.
    *
-   * This is frequently used to make GraphQL requests more cacheable
-   * on CDNs.
+   * GET requests are frequently used to make GraphQL requests more
+   * cacheable on CDNs.
    *
    * @defaultValue `true` - enabled
    */
-  preferGetForPersistedQueries?: boolean;
+  preferGetForPersistedQueries?: OperationContext['preferGetMethod'];
   /** Enforces non-automatic persisted queries by ignoring APQ errors.
    *
    * @remarks
@@ -118,7 +123,8 @@ export const persistedExchange =
   ({ forward }) => {
     if (!options) options = {};
 
-    const preferGetForPersistedQueries = !!options.preferGetForPersistedQueries;
+    const preferGetForPersistedQueries =
+      options.preferGetForPersistedQueries ?? true;
     const enforcePersistedQueries = !!options.enforcePersistedQueries;
     const hashFn = options.generateHash || hash;
     const enableForMutation = !!options.enableForMutation;
@@ -159,11 +165,9 @@ export const persistedExchange =
                 sha256Hash,
               },
             };
-            if (
-              persistedOperation.kind === 'query' &&
-              preferGetForPersistedQueries
-            ) {
-              persistedOperation.context.preferGetMethod = 'force';
+            if (persistedOperation.kind === 'query') {
+              persistedOperation.context.preferGetMethod =
+                preferGetForPersistedQueries;
             }
           }
 
