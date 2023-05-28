c19645eef6dfdfa7a962fdaab367913710c75234
jdecroock <decroockjovi@gmail.com>
2023-05-28 18:40:01 +0200

    tsdoc for rsc

--- packages/next-urql/src/rsc.ts
+++ packages/next-urql/src/rsc.ts
@@ -1,6 +1,25 @@
 import * as React from 'react';
 import { Client } from '@urql/core';
 
+/** Function to cache an urql-client across React Server Components.
+ *
+ * @param makeClient - A function that creates an urql-client.
+ * @returns an object containing a getClient method.
+ *
+ * @example
+ * ```ts
+ * import { cacheExchange, createClient, fetchExchange, gql } from '@urql/core';
+ * import { registerUrql } from '@urql/next/rsc';
+ * const makeClient = () => {
+ *   return createClient({
+ *     url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+ *     exchanges: [cacheExchange, fetchExchange],
+ *   });
+ * };
+ *
+ * const { getClient } = registerUrql(makeClient);
+ * ```
+ */
 export function registerUrql(makeClient: () => Client) {
   // @ts-ignore you exist don't worry
   const getClient = React.cache(makeClient);
