3f147341dcd383a804643b5275d3aaf78fa5d39b
jdecroock <decroockjovi@gmail.com>
2023-05-28 18:43:32 +0200

    provider tsdoc

--- packages/next-urql/src/Provider.ts
+++ packages/next-urql/src/Provider.ts
@@ -8,6 +8,40 @@ export const SSRContext = React.createContext<SSRExchange | undefined>(
   undefined
 );
 
+/** Provider for `@urql/next` during non-rsc interactions.
+ *
+ * @remarks
+ * `Provider` accepts a {@link Client} and provides it to all GraphQL hooks, it
+ * also accepts an {@link SSRExchange} to distribute data when re-hydrating
+ * on the client.
+ *
+ * @example
+ * ```tsx
+ * import {
+ *  UrqlProvider,
+ *  ssrExchange,
+ *  cacheExchange,
+ *  fetchExchange,
+ *  createClient,
+ * } from '@urql/next';
+ *
+ * const ssr = ssrExchange();
+ * const client = createClient({
+ *   url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+ *   exchanges: [cacheExchange, ssr, fetchExchange],
+ *   suspense: true,
+ * });
+ *
+ * export default function Layout({ children }: React.PropsWithChildren) {
+ *   return (
+ *     <UrqlProvider client={client} ssr={ssr}>
+ *      {children}
+ *     </UrqlProvider>
+ *   );
+ * }
+ *
+ * ```
+ */
 export function UrqlProvider({
   children,
   ssr,
