d17a1c256a04655b05006539dd69a0813e2f51c4
Phil Pluckthun <phil@kitten.sh>
2023-06-28 00:00:25 +0100

    demo(#3295): Apply preferGetMethod

--- examples/with-react/src/App.jsx
+++ examples/with-react/src/App.jsx
@@ -6,6 +6,13 @@ import PokemonList from './PokemonList';
 const client = new Client({
   url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
   exchanges: [cacheExchange, fetchExchange],
+  preferGetMethod: 'force',
+  requestPolicy: 'network-only',
+  fetchOptions: {
+    headers: {
+      'x-example': 'store-code',
+    },
+  },
 });
 
 function App() {
