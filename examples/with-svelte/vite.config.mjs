f6038e8c11465ededf69211da63ccb2842479bfc
Phil Pluckthun <phil@kitten.sh>
2023-07-08 02:08:23 +0100

    Update vite-plugin-svelte

--- examples/with-svelte/vite.config.mjs
+++ examples/with-svelte/vite.config.mjs
@@ -4,7 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  optimizeDeps: {
-    exclude: ['@urql/svelte'],
-  },
 });
