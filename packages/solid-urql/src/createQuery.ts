09313b67fea247cf30f4ffcd9d505b809fe86f97
nvos <personal@creoi.dev>
2023-07-28 21:21:26 +0200

    docs(solid): Add createQuery code docs

--- packages/solid-urql/src/createQuery.ts
+++ packages/solid-urql/src/createQuery.ts
@@ -28,6 +28,27 @@ import {
   switchMap,
 } from 'wonka';
 
+/** Triggers {@link createQuery} to execute a new GraphQL query operation.
+ *
+ * @remarks
+ * When called, {@link createQuery} will re-execute the GraphQL query operation
+ * it currently holds, even if {@link CreateQueryArgs.pause} is set to `true`.
+ *
+ * This is useful for executing a paused query or re-executing a query
+ * and get a new network result, by passing a new request policy.
+ *
+ * ```ts
+ * const [result, reExecuteQuery] = createQuery({ query });
+ *
+ * const refresh = () => {
+ *   // Re-execute the query with a network-only policy, skipping the cache
+ *   reExecuteQuery({ requestPolicy: 'network-only' });
+ * };
+ * ```
+ *
+ */
+export type CreateQueryExecute = (opts?: Partial<OperationContext>) => void;
+
 /** State of the current query, your {@link createQuery} hook is executing.
  *
  * @remarks
@@ -58,23 +79,108 @@ export type CreateQueryState<
   fetching: boolean;
 };
 
+/**
+ * Input arguments for the {@link createQuery} hook.
+ */
 export type CreateQueryArgs<
   Data = any,
   Variables extends AnyVariables = AnyVariables
 > = {
+  /** The GraphQL query that `createQuery` executes. */
   query: DocumentInput<Data, Variables>;
-  requestPolicy?: MaybeAccessor<RequestPolicy>;
-  context?: MaybeAccessor<Partial<OperationContext>>;
-  pause?: MaybeAccessor<boolean>;
+
+  /** The variables for the GraphQL {@link CreateQueryArgs.query} that `createQuery` executes. */
   variables?: MaybeAccessor<Variables>;
+
+  /** Updates the {@link RequestPolicy} for the executed GraphQL query operation.
+   *
+   * @remarks
+   * `requestPolicy` modifies the {@link RequestPolicy} of the GraphQL query operation
+   * that `useQuery` executes, and indicates a caching strategy for cache exchanges.
+   *
+   * For example, when set to `'cache-and-network'`, {@link createQuery} will
+   * receive a cached result with `stale: true` and an API request will be
+   * sent in the background.
+   *
+   * @see {@link OperationContext.requestPolicy} for where this value is set.
+   */
+  requestPolicy?: MaybeAccessor<RequestPolicy>;
+
+  /** Updates the {@link OperationContext} for the executed GraphQL query operation.
+   *
+   * @remarks
+   * `context` may be passed to {@link createQuery}, to update the {@link OperationContext}
+   * of a query operation. This may be used to update the `context` that exchanges
+   * will receive for a single hook.
+   *
+   * In order to re-execute query on context change pass {@link Accessor} instead
+   * of raw value.
+   */
+  context?: MaybeAccessor<Partial<OperationContext>>;
+
+  /** Prevents {@link useQuery} from automatically executing GraphQL query operations.
+   *
+   * @remarks
+   * `pause` may be set to `true` to stop {@link createQuery} from executing
+   * automatically. The hook will stop receiving updates from the {@link Client}
+   * and won’t execute the query operation, until either it’s set to `false`
+   * or the {@link CreateQueryExecute} function is called.
+   */
+  pause?: MaybeAccessor<boolean>;
 };
 
+/** Result tuple returned by the {@link createQuery} hook.
+ *
+ * @remarks
+ * the first element is the {@link createQuery}’s result and state,
+ * a {@link CreateQueryState} object,
+ * and the second is used to imperatively re-execute the query
+ * via a {@link CreateQueryExecute} function.
+ */
+export type CreateQueryResult<
+  Data = any,
+  Variables extends AnyVariables = AnyVariables
+> = [CreateQueryState<Data, Variables>, CreateQueryExecute];
+
+/** Hook to run a GraphQL query and get updated GraphQL results.
+ *
+ * @param args - a {@link CreateQueryArgs} object, to pass a `query`, `variables`, and options.
+ * @returns a {@link CreateQueryResult} tuple of a {@link CreateQueryState} result, and re-execute function.
+ *
+ * @remarks
+ * `createQuery` allows GraphQL queries to be defined and executed.
+ * Given {@link CreateQueryArgs.query}, it executes the GraphQL query with the
+ * context’s {@link Client}.
+ *
+ * The returned result updates when the `Client` has new results
+ * for the query, and changes when your input `args` change.
+ *
+ * Additionally, if the `suspense` option is enabled on the `Client`,
+ * the `createQuery` hook will suspend instead of indicating that it’s
+ * waiting for a result via {@link UseQueryState.fetching}.
+ *
+ * @example
+ * ```tsx
+ * import { gql, createQuery } from '@urql/solid';
+ *
+ * const TodosQuery = gql`
+ *   query { todos { id, title } }
+ * `;
+ *
+ * const Todos = () => {
+ *   const [result, reExecuteQuery] = createQuery({
+ *     query: TodosQuery,
+ *   });
+ *   // ...
+ * };
+ * ```
+ */
 export const createQuery = <
   Data = any,
   Variables extends AnyVariables = AnyVariables
 >(
   args: CreateQueryArgs<Data, Variables>
-) => {
+): CreateQueryResult => {
   const client = useClient();
   const getContext = asAccessor(args.context),
     getPause = asAccessor(args.pause),
@@ -85,6 +191,7 @@ export const createQuery = <
     OperationResultSource<OperationResult<Data, Variables>> | undefined
   >();
 
+  // Combine suspense param coming from context and client with context being priority
   const isSuspense = createMemo(() => {
     const ctx = getContext();
     if (ctx !== undefined && ctx.suspense !== undefined) {
@@ -113,6 +220,12 @@ export const createQuery = <
   const [result, setResult] =
     createStore<CreateQueryState<Data, Variables>>(initialResult);
 
+  // `createResource` is only way to trigger suspense, suspense handling works as follows:
+  // 1. Calling `dataResource` mounts resource to closes suspense. Thus depending on where it is called
+  // it could mount in same component or parent. For usage convince we call it by proxy when using `data`
+  // 2. After any changes we call `refetch` which results in creation of new promise and depending on `fetching`
+  // value we either discard promise without resolving or resolve it. Discard doesn't result in anything while
+  // resolve finishes suspense
   const [dataResource, { refetch, mutate }] = createResource<
     CreateQueryState<Data, Variables>
   >(() => {
@@ -129,11 +242,13 @@ export const createQuery = <
   const sub = pipe(
     resultSourceSubject.source,
     switchMap(subscription$ => {
+      // On pause source is set to undefined
       if (subscription$ === undefined) {
         return fromValue({ fetching: false });
       }
 
       return concat([
+        // Fetch start state
         fromValue({ fetching: true, stale: false }),
         pipe(
           subscription$,
@@ -146,9 +261,11 @@ export const createQuery = <
             extensions,
           }))
         ),
-        fromValue({ fetching: false }),
+        // Fetch finish state
+        fromValue({ fetching: false, stale: false }),
       ]);
     }),
+    // Merging partial results into previous
     scan(
       (result: CreateQueryState<Data, Variables>, partial: any) => ({
         ...result,
@@ -188,7 +305,7 @@ export const createQuery = <
     refetch();
   });
 
-  const executeQuery = (opts?: Partial<OperationContext>) => {
+  const executeQuery: CreateQueryExecute = opts => {
     const request = createRequest(args.query, getVariables() as any);
     const context: Partial<OperationContext> = {
       requestPolicy: opts?.requestPolicy ?? getRequestPolicy(),
@@ -213,5 +330,5 @@ export const createQuery = <
   };
 
   const proxy = new Proxy(result, handler);
-  return [proxy, executeQuery] as const;
+  return [proxy, executeQuery];
 };
