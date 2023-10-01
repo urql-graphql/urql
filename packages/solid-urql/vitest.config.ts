397397a0927fa1e118ae98748f338e72a6302990
nvos <personal@creoi.dev>
2023-10-01 19:16:31 +0200

    test(solid): Migrate subscription and mutation tests to use testEffect, extract query suspense tests

--- packages/solid-urql/vitest.config.ts
+++ packages/solid-urql/vitest.config.ts
@@ -1,32 +1,14 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-// 👆 do not forget to add the references above
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { mergeConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import rootConfig from '../../vitest.config';
+import { defineProject } from 'vitest/config';
 
-export default defineConfig({
-  plugins: [solidPlugin(), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  test: {
-    environment: 'jsdom',
-    transformMode: { web: [/\.[jt]sx?$/] },
-    globals: true,
-    setupFiles: [resolve(__dirname, '../../scripts/vitest/setup.js')],
-    clearMocks: true,
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/cypress/**',
-      '**/e2e-tests/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*',
-    ],
-  },
-});
+export default mergeConfig(
+  rootConfig,
+  defineProject({
+    plugins: [solidPlugin()],
+    test: {
+      globals: true,
+    },
+  })
+);
