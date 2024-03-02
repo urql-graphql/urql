e8e16bbae97ac2dc612a70f93f06b71a7371b609
Jovi De Croock <decroockjovi@gmail.com>
2024-03-02 14:03:10 +0100

    feat(core): Support Apollo Federation subscriptions multipart payloads (#3499)

---
+++ .changeset/small-cheetahs-jam.md
@@ -0,0 +1,5 @@
+---
+'@urql/core': minor
+---
+
+Support [Apollo Federation's format](https://www.apollographql.com/docs/router/executing-operations/subscription-multipart-protocol/) for subscription results in `multipart/mixed` responses (result properties essentially are namespaced on a `payload` key)
--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -223,6 +223,7 @@ export interface ExecutionResult {
    * @see {@link https://github.com/graphql/graphql-wg/blob/main/rfcs/DeferStream.md#payload-format} for the DeferStream spec
    */
   hasNext?: boolean;
+  payload?: Omit<ExecutionResult, 'payload'>;
 }
 
 /** A source of {@link OperationResult | OperationResults}, convertable to a promise, subscribable, or Wonka Source.
