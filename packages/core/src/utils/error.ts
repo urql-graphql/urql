a620d15376ebc4891ac465e05a655e0f737c048a
Jovi De Croock <decroockjovi@gmail.com>
2024-08-05 12:06:06 +0200

    fix(core): Allow for empty error messages when rehydrating (#3650)

--- packages/core/src/utils/error.ts
+++ packages/core/src/utils/error.ts
@@ -19,11 +19,11 @@ const generateErrorMessage = (
 const rehydrateGraphQlError = (error: any): GraphQLError => {
   if (
     error &&
-    error.message &&
+    typeof error.message === 'string' &&
     (error.extensions || error.name === 'GraphQLError')
   ) {
     return error;
-  } else if (typeof error === 'object' && error.message) {
+  } else if (typeof error === 'object' && typeof error.message === 'string') {
     return new GraphQLError(
       error.message,
       error.nodes,
