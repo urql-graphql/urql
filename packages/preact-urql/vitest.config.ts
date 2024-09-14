1aa4dc23783cfc7570fad58f632fedebfe2d9bab
Jovi De Croock <decroockjovi@gmail.com>
2024-09-14 13:44:19 +0200

    Fix preact alias

--- packages/preact-urql/vitest.config.ts
+++ packages/preact-urql/vitest.config.ts
@@ -1,14 +1,13 @@
 import { mergeConfig } from 'vitest/config';
+import path from 'node:path';
 import baseConfig from '../../vitest.config';
 
+console.log(path.resolve(__dirname, './node_modules/preact/hooks/dist/hooks.js'));
 export default mergeConfig(baseConfig, {
   resolve: {
     alias: {
-      'preact/hooks':
-        __dirname +
-        '/packages/preact-urql/node_modules/preact/hooks/dist/hooks.js',
-      preact:
-        __dirname + '/packages/preact-urql/node_modules/preact/dist/preact.js',
+      'preact/hooks': path.resolve(__dirname, './node_modules/preact/hooks/dist/hooks.js'),
+      preact: path.resolve(__dirname, './node_modules/preact/dist/preact.js')
     },
   },
 });
